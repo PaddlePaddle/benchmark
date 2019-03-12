9501971f7653e609fc8f10d22e2b949729e8dab5
Liu Yiqun <liuyiqun01@baidu.com>
2019-03-12 20:08:08 +0800

    Add more results and readme file.

--- OCR/paddle/configs/infer_cpu.sh
+++ OCR/paddle/configs/infer_cpu.sh
@@ -6,7 +6,7 @@ unset CUDA_VISIBLE_DEVICES
 export CUDA_VISIBLE_DEVICES=""
 
 export OCR_mode=CPU # CPU, GPU, MKLDNN
-export OCR_model_path=gpu_attention_models_512/params_01000
+export OCR_model_path=ocr_attention/ocr_attention_params
 export OCR_batch_size=1
 export OCR_skip_batch_num=10
 export OCR_iterations=2000
--- OCR/paddle/configs/infer_gpu_memory.sh
+++ OCR/paddle/configs/infer_gpu_memory.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=0
 export FLAGS_fraction_of_gpu_memory_to_use=0
 
 export OCR_mode=GPU # CPU, GPU, MKLDNN
-export OCR_model_path=gpu_attention_models_512/params_01000
+export OCR_model_path=ocr_attention/ocr_attention_params
 export OCR_batch_size=1
 export OCR_skip_batch_num=10
 export OCR_iterations=2000
