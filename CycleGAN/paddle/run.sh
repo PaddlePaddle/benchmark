869056b7464212240359c6d25325894790cb7b93
liangjinhua01 <liangjinhua01@baidudeMacBook-Pro.local>
2019-07-09 20:09:48 +0800

    add cpu & gpu utilization

--- CycleGAN/paddle/run.sh
+++ CycleGAN/paddle/run.sh
@@ -2,9 +2,9 @@
 set -xe
 
 if [ $# -lt 2 ]; then
-  echo "Usage: "
-  echo "  CUDA_VISIBLE_DEVICES=0 bash run.sh train|infer speed|mem /ssd1/ljh/logs"
-  exit
+    echo "Usage: "
+    echo "  CUDA_VISIBLE_DEVICES=0 bash run.sh train|infer speed|mem /ssd1/ljh/logs"
+    exit
 fi
 
 #打开后速度变快
@@ -26,17 +26,17 @@ batch_size=1
 log_file=${run_log_path}/${model_name}_${task}_${index}_${num_gpu_devices}
 
 train(){
-  echo "Train on ${num_gpu_devices} GPUs"
-  echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
-  python train.py | tee ${log_file} 2>&1 &
-  train_pid=$!
-  sleep 120
-  kill -9 $train_pid
+    echo "Train on ${num_gpu_devices} GPUs"
+    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+    python train.py | tee ${log_file} 2>&1 &
+    train_pid=$!
+    sleep 120
+    kill -9 $train_pid
 }
 
 infer(){
-  echo "infer on ${num_gpu_devices} GPUs"
-  echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+    echo "infer on ${num_gpu_devices} GPUs"
+    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
 #  python eval_coco_map.py \
 #    --dataset=coco2017 \
 #    --pretrained_model=../imagenet_resnet50_fusebn/ \
@@ -47,46 +47,46 @@ infer(){
 }
 
 analysis_times(){
-  skip_step=$1
-  filter_fields=$2
-  count_fields=$3
-  awk 'BEGIN{count=0}/Batch_time_cost:/{
-    step_times[count]=$'${count_fields}';
-    count+=1;
-  }END{
-    print "\n================ Benchmark Result ================"
-    print "total_step:", count
-    print "batch_size:", "'${batch_size}'"
-    if(count>1){
-      step_latency=0
-      step_latency_without_step0_avg=0
-      step_latency_without_step0_min=step_times['${skip_step}']
-      step_latency_without_step0_max=step_times['${skip_step}']
-      for(i=0;i<count;++i){
-        step_latency+=step_times[i];
-        if(i>='${skip_step}'){
-          step_latency_without_step0_avg+=step_times[i];
-          if(step_times[i]<step_latency_without_step0_min){
-            step_latency_without_step0_min=step_times[i];
-          }
-          if(step_times[i]>step_latency_without_step0_max){
-            step_latency_without_step0_max=step_times[i];
+    skip_step=$1
+    filter_fields=$2
+    count_fields=$3
+    awk 'BEGIN{count=0}/Batch_time_cost:/{
+      step_times[count]=$'${count_fields}';
+      count+=1;
+    }END{
+      print "\n================ Benchmark Result ================"
+      print "total_step:", count
+      print "batch_size:", "'${batch_size}'"
+      if(count>1){
+        step_latency=0
+        step_latency_without_step0_avg=0
+        step_latency_without_step0_min=step_times['${skip_step}']
+        step_latency_without_step0_max=step_times['${skip_step}']
+        for(i=0;i<count;++i){
+          step_latency+=step_times[i];
+          if(i>='${skip_step}'){
+            step_latency_without_step0_avg+=step_times[i];
+            if(step_times[i]<step_latency_without_step0_min){
+              step_latency_without_step0_min=step_times[i];
+            }
+            if(step_times[i]>step_latency_without_step0_max){
+              step_latency_without_step0_max=step_times[i];
+            }
           }
         }
+        step_latency/=count;
+        step_latency_without_step0_avg/=(count-'${skip_step}')
+        printf("average latency (origin result):\n")
+        printf("\tAvg: %.3f s/step\n", step_latency)
+        printf("\tFPS: %.3f examples/s\n", "'${batch_size}'"/step_latency)
+        printf("average latency (skip '${skip_step}' steps):\n")
+        printf("\tAvg: %.3f s/step\n", step_latency_without_step0_avg)
+        printf("\tMin: %.3f s/step\n", step_latency_without_step0_min)
+        printf("\tMax: %.3f s/step\n", step_latency_without_step0_max)
+        printf("\tFPS: %.3f examples/s\n", '${batch_size}'/step_latency_without_step0_avg)
+        printf("\n")
       }
-      step_latency/=count;
-      step_latency_without_step0_avg/=(count-'${skip_step}')
-      printf("average latency (origin result):\n")
-      printf("\tAvg: %.3f s/step\n", step_latency)
-      printf("\tFPS: %.3f examples/s\n", "'${batch_size}'"/step_latency)
-      printf("average latency (skip '${skip_step}' steps):\n")
-      printf("\tAvg: %.3f s/step\n", step_latency_without_step0_avg)
-      printf("\tMin: %.3f s/step\n", step_latency_without_step0_min)
-      printf("\tMax: %.3f s/step\n", step_latency_without_step0_max)
-      printf("\tFPS: %.3f examples/s\n", '${batch_size}'/step_latency_without_step0_avg)
-      printf("\n")
-    }
-  }' ${log_file}
+    }' ${log_file}
 }
 
 if [ $index = "mem" ]
@@ -102,8 +102,15 @@ then
     awk 'BEGIN {max = 0} {if(NR>1){if ($1 > max) max=$1}} END {print "Max=", max}' gpu_use.log
 else
     echo "Benchmark for $task"
-
+    job_bt=`date '+%Y%m%d%H%M%S'`
     $task
+    job_et=`date '+%Y%m%d%H%M%S'`
+    hostname=`echo $(hostname)|awk -F '.baidu.com' '{print $1}'`
+    monquery -n $hostname -i GPU_AVERAGE_UTILIZATION -s $job_bt -e $job_et -d 60 > gpu_avg_utilization
+    monquery -n $hostname -i CPU_USER -s $job_bt -e $job_et -d 60 > cpu_use
+    awk '{if(NR>1){time+=$3;count+=1}} END{if(count>0) avg=time/count; else avg=0; printf("avg_gpu_use=%.2f\n" ,avg)}' gpu_avg_utilization
+    awk '{if(NR>1){time+=$3;count+=1}} END{if(count>0) avg=time/count; else avg=0; printf("avg_cpu_use=%.2f\n" ,avg)}' cpu_use
+
     if [ ${task} = "train" ]
     then
       analysis_times 3 12 12
--- se-resnext/paddle/run.sh
+++ se-resnext/paddle/run.sh
@@ -11,9 +11,9 @@ export FLAGS_fraction_of_gpu_memory_to_use=0.98
 #export FLAGS_enable_parallel_graph=1
 
 if [ $# -lt 3 ]; then
-  echo "Usage: "
-  echo "  CUDA_VISIBLE_DEVICES=0 bash run.sh speed|mem|maxbs 32 sp|mp /ssd1/ljh/logs"
-  exit
+    echo "Usage: "
+    echo "  CUDA_VISIBLE_DEVICES=0 bash run.sh speed|mem|maxbs 32 sp|mp /ssd1/ljh/logs"
+    exit
 fi
 
 task="train"
@@ -40,107 +40,114 @@ log_file=${run_log_path}/${model_name}_${task}_${index}_${num_gpu_devices}_${run
 log_parse_file=${log_file}
 
 train(){
-  echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$cal_batch_size"
-  WORK_ROOT=$PWD
-  train_cmd=" --model=SE_ResNeXt50_32x4d \
-     --batch_size=${batch_size} \
-     --total_images=1281167 \
-     --class_dim=1000 \
-     --image_shape=3,224,224 \
-     --model_save_dir=output/ \
-     --pretrained_model=SE_ResNext50_32x4d_pretrained/ \
-     --data_dir=data/ILSVRC2012 \
-     --with_mem_opt=False \
-     --with_inplace=True \
-     --lr_strategy=cosine_decay \
-     --lr=0.1 \
-     --l2_decay=1.2e-4 \
-     --num_epochs=${num_epochs}"
+    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$cal_batch_size"
+    WORK_ROOT=$PWD
+    train_cmd=" --model=SE_ResNeXt50_32x4d \
+       --batch_size=${batch_size} \
+       --total_images=1281167 \
+       --class_dim=1000 \
+       --image_shape=3,224,224 \
+       --model_save_dir=output/ \
+       --pretrained_model=SE_ResNext50_32x4d_pretrained/ \
+       --data_dir=data/ILSVRC2012 \
+       --with_mem_opt=False \
+       --with_inplace=True \
+       --lr_strategy=cosine_decay \
+       --lr=0.1 \
+       --l2_decay=1.2e-4 \
+       --num_epochs=${num_epochs}"
 
-  case ${run_mode} in
-  sp) train_cmd="python -u train.py "${train_cmd} ;;
-  mp)
-      train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES train.py "${train_cmd}
-      log_parse_file="mylog/workerlog.0" ;;
-  *) echo "choose run_mode(sp or mp)"; exit 1;
-  esac
+    case ${run_mode} in
+    sp) train_cmd="python -u train.py "${train_cmd} ;;
+    mp)
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES train.py "${train_cmd}
+        log_parse_file="mylog/workerlog.0" ;;
+    *) echo "choose run_mode(sp or mp)"; exit 1;
+    esac
 
-  ${train_cmd} > ${log_file} 2>&1 &
-  train_pid=$!
-  sleep 600
-  kill -9 `ps -ef|grep python |awk '{print $2}'`
+    ${train_cmd} > ${log_file} 2>&1 &
+    train_pid=$!
+    sleep 600
+    kill -9 `ps -ef|grep python |awk '{print $2}'`
 
-  if [ $run_mode = "mp" -a -d mylog ]; then
-      rm ${log_file}
-      cp mylog/workerlog.0 ${log_file}
-  fi
-  cd ${WORK_ROOT}
+    if [ $run_mode = "mp" -a -d mylog ]; then
+        rm ${log_file}
+        cp mylog/workerlog.0 ${log_file}
+    fi
+    cd ${WORK_ROOT}
 }
 
 analysis_times(){
-  skip_step=$1
-  count_fields=$2
-  sed 's/batch\/sec/\ batch\/sec/' ${log_parse_file} | awk 'BEGIN{count=0}/trainbatch/{
-    step_times[count]=$'${count_fields}';
-    count+=1;
-  }END{
-    print "\n================ Benchmark Result ================"
-    print "num_epochs:", "'${num_epochs}'"
-    print "batch_size:", "'${cal_batch_size}'"
-    if(count>'${skip_step}'){
-      step_latency=0
-      step_latency_without_step0_avg=0
-      step_latency_without_step0_min=step_times['${skip_step}']
-      step_latency_without_step0_max=step_times['${skip_step}']
-      for(i=0;i<count;++i){
-        step_latency+=step_times[i];
-        if(i>='${skip_step}'){
-          step_latency_without_step0_avg+=step_times[i];
-          if(step_times[i]<step_latency_without_step0_min){
-            step_latency_without_step0_min=step_times[i];
-          }
-          if(step_times[i]>step_latency_without_step0_max){
-            step_latency_without_step0_max=step_times[i];
+    skip_step=$1
+    count_fields=$2
+    sed 's/batch\/sec/\ batch\/sec/' ${log_parse_file} | awk 'BEGIN{count=0}/trainbatch/{
+      step_times[count]=$'${count_fields}';
+      count+=1;
+    }END{
+      print "\n================ Benchmark Result ================"
+      print "num_epochs:", "'${num_epochs}'"
+      print "batch_size:", "'${cal_batch_size}'"
+      if(count>'${skip_step}'){
+        step_latency=0
+        step_latency_without_step0_avg=0
+        step_latency_without_step0_min=step_times['${skip_step}']
+        step_latency_without_step0_max=step_times['${skip_step}']
+        for(i=0;i<count;++i){
+          step_latency+=step_times[i];
+          if(i>='${skip_step}'){
+            step_latency_without_step0_avg+=step_times[i];
+            if(step_times[i]<step_latency_without_step0_min){
+              step_latency_without_step0_min=step_times[i];
+            }
+            if(step_times[i]>step_latency_without_step0_max){
+              step_latency_without_step0_max=step_times[i];
+            }
           }
         }
+        step_latency/=count;
+        step_latency_without_step0_avg/=(count-'${skip_step}')
+        printf("average latency (including data reading):\n")
+        printf("\tAvg: %.3f s/step\n", step_latency)
+        printf("\tFPS: %.3f examples/s\n", "'${cal_batch_size}'"/step_latency)
+        printf("average latency (skip '${skip_step}' steps):\n")
+        printf("\tAvg: %.3f s/step\n", step_latency_without_step0_avg)
+        printf("\tMin: %.3f s/step\n", step_latency_without_step0_min)
+        printf("\tMax: %.3f s/step\n", step_latency_without_step0_max)
+        printf("\tFPS: %.3f examples/s\n", "'${cal_batch_size}'"/step_latency_without_step0_avg)
+        printf("\n")
       }
-      step_latency/=count;
-      step_latency_without_step0_avg/=(count-'${skip_step}')
-      printf("average latency (including data reading):\n")
-      printf("\tAvg: %.3f s/step\n", step_latency)
-      printf("\tFPS: %.3f examples/s\n", "'${cal_batch_size}'"/step_latency)
-      printf("average latency (skip '${skip_step}' steps):\n")
-      printf("\tAvg: %.3f s/step\n", step_latency_without_step0_avg)
-      printf("\tMin: %.3f s/step\n", step_latency_without_step0_min)
-      printf("\tMax: %.3f s/step\n", step_latency_without_step0_max)
-      printf("\tFPS: %.3f examples/s\n", "'${cal_batch_size}'"/step_latency_without_step0_avg)
-      printf("\n")
-    }
-  }'
+    }'
 }
 
 echo "Benchmark for $index"
 
 if [ ${index} = 'mem' ]
 then
-  export FLAGS_fraction_of_gpu_memory_to_use=0.001
-  gpu_id=`echo $CUDA_VISIBLE_DEVICES |cut -c1`
-  nvidia-smi --id=$gpu_id --query-compute-apps=used_memory --format=csv -lms 100 > gpu_use.log 2>&1 &
-  gpu_memory_pid=$!
-  train
-  kill $gpu_memory_pid
-  awk 'BEGIN {max = 0} {if(NR>1){if ($1 > max) max=$1}} END {print "Max=", max}' gpu_use.log
+    export FLAGS_fraction_of_gpu_memory_to_use=0.001
+    gpu_id=`echo $CUDA_VISIBLE_DEVICES |cut -c1`
+    nvidia-smi --id=$gpu_id --query-compute-apps=used_memory --format=csv -lms 100 > gpu_use.log 2>&1 &
+    gpu_memory_pid=$!
+    train
+    kill $gpu_memory_pid
+    awk 'BEGIN {max = 0} {if(NR>1){if ($1 > max) max=$1}} END {print "Max=", max}' gpu_use.log
 elif [ ${index} = 'speed' ]
 then
-  train
-  analysis_times 2 14
+    job_bt=`date '+%Y%m%d%H%M%S'`
+    train
+    job_et=`date '+%Y%m%d%H%M%S'`
+    hostname=`echo $(hostname)|awk -F '.baidu.com' '{print $1}'`
+    monquery -n $hostname -i GPU_AVERAGE_UTILIZATION -s $job_bt -e $job_et -d 60 > gpu_avg_utilization
+    monquery -n $hostname -i CPU_USER -s $job_bt -e $job_et -d 60 > cpu_use
+    awk '{if(NR>1){time+=$3;count+=1}} END{if(count>0) avg=time/count; else avg=0; printf("avg_gpu_use=%.2f\n" ,avg)}' gpu_avg_utilization
+    awk '{if(NR>1){time+=$3;count+=1}} END{if(count>0) avg=time/count; else avg=0; printf("avg_cpu_use=%.2f\n" ,avg)}' cpu_use
+    analysis_times 2 14
 else
-  train
-  error_string="Please shrink FLAGS_fraction_of_gpu_memory_to_use or FLAGS_initial_gpu_memory_in_mb or FLAGS_reallocate_gpu_memory_in_mbenvironment variable to a lower value"
-  if [ `grep -c "${error_string}" ${log_parse_file}` -eq 0 ]; then
-    echo "maxbs is ${batch_size}"
-  else
-    echo "maxbs running error"
-  fi
+    train
+    error_string="Please shrink FLAGS_fraction_of_gpu_memory_to_use or FLAGS_initial_gpu_memory_in_mb or FLAGS_reallocate_gpu_memory_in_mbenvironment variable to a lower value"
+    if [ `grep -c "${error_string}" ${log_parse_file}` -eq 0 ]; then
+      echo "maxbs is ${batch_size}"
+    else
+      echo "maxbs running error"
+    fi
 
 fi
