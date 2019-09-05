b6aa01c716b4a69d363ba36dc898ae7dc834d5a9
wangchaochaohu <wangchao66@baidu.com>
2019-09-05 21:10:41 +0800

    test=develop upload run.sh for the mask-rcnn-fpn (#175)

---
+++ Mask-RCNN-FPN/resnet/paddle/run.sh
@@ -0,0 +1,111 @@
+#!bin/bash
+set -xe
+#python -u tools/train.py -c configs/mask_rcnn_r101_fpn_1x.yml
+if [ $# -lt 3 ]; then
+  echo "Usage: "
+  echo "  CUDA_VISIBLE_DEVICES=0 bash run.sh train speed|mem sp|mp /ssd1/ljh/logs"
+  exit
+fi
+
+task="$1"
+index="$2"
+run_mode="$3"
+run_log_path=${4:-$(pwd)}
+model_name="mask_rcnn_fpn_mask_rcnn_resnet"
+
+device=${CUDA_VISIBLE_DEVICES//,/ }
+arr=($device)
+num_gpu_devices=${#arr[*]}
+base_batch_size=2
+batch_size=`expr ${base_batch_size} \* $num_gpu_devices`
+log_file=${run_log_path}/${model_name}_${task}_${index}_${num_gpu_devices}_${run_mode}
+log_parse_file=${log_file}
+
+train(){
+  echo "Train on ${num_gpu_devices} GPUs"
+  echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+
+  train_cmd="-c configs/mask_rcnn_r101_vd_fpn_1x.yml"
+
+  case ${run_mode} in
+  sp) train_cmd="python -u tools/train.py "${train_cmd} ;;
+  mp)
+      train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES tools/train.py "${train_cmd}
+      log_parse_file="mylog/workerlog.0" ;;
+  *) echo "choose run_mode(sp or mp)"; exit 1;
+  esac
+
+  ${train_cmd} > ${log_file} 2>&1 &
+  train_pid=$!
+  sleep 800
+  kill -9 `ps -ef|grep python |awk '{print $2}'`
+
+  if [ $run_mode = "mp" -a -d mylog ]; then
+      rm ${log_file}
+      cp mylog/workerlog.0 ${log_file}
+  fi
+}
+
+
+analysis_times(){
+  skip_step=$1
+  filter_fields=$2
+  count_fields=$3
+  awk 'BEGIN{count=0}{if(NF>='${filter_fields}'){
+    step_times[count]=$'${count_fields}';
+    count+=1;}
+  }END{
+    print "\n================ Benchmark Result ================"
+    print "total_step:", count
+    print "batch_size:", "'${batch_size}'"
+    if(count>1){
+      step_latency=0
+      step_latency_without_step0_avg=0
+      step_latency_without_step0_min=step_times['${skip_step}']
+      step_latency_without_step0_max=step_times['${skip_step}']
+      for(i=0;i<count;++i){
+        step_latency+=step_times[i];
+        if(i>='${skip_step}'){
+          step_latency_without_step0_avg+=step_times[i];
+          if(step_times[i]<step_latency_without_step0_min){
+            step_latency_without_step0_min=step_times[i];
+          }
+          if(step_times[i]>step_latency_without_step0_max){
+            step_latency_without_step0_max=step_times[i];
+          }
+        }
+      }
+      step_latency/=count;
+      step_latency_without_step0_avg/=(count-'${skip_step}')
+      printf("average latency (origin result):\n")
+      printf("\tAvg: %.3f s/step\n", step_latency)
+      printf("\tFPS: %.3f images/s\n", "'${batch_size}'"/step_latency)
+      printf("average latency (skip '${skip_step}' steps):\n")
+      printf("\tAvg: %.3f s/step\n", step_latency_without_step0_avg)
+      printf("\tMin: %.3f s/step\n", step_latency_without_step0_min)
+      printf("\tMax: %.3f s/step\n", step_latency_without_step0_max)
+      printf("\tFPS: %.3f images/s\n", '${batch_size}'/step_latency_without_step0_avg)
+      printf("\n")
+    }
+  }' ${log_parse_file}
+}
+
+echo "Benchmark for $index"
+
+if [ $index = "mem" ]
+then
+  export FLAGS_fraction_of_gpu_memory_to_use=0.001
+  gpu_id=`echo $CUDA_VISIBLE_DEVICES | cut -c1`
+  nvidia-smi --id=$gpu_id --query-compute-apps=used_memory --format=csv -lms 100 > gpu_use.log 2>&1 &
+  gpu_memory_pid=$!
+  $task
+  kill $gpu_memory_pid
+  awk 'BEGIN {max = 0} {if(NR>1){if ($1 > max) max=$1}} END {print "Max=", max}' gpu_use.log
+elif [ ${index} = 'speed' ]
+then
+  $task
+  if [ ${task} = "train" ]
+  then
+      analysis_times 3 22 20
+  fi
+fi
