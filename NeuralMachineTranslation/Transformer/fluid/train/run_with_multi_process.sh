bc0c5f9fcf12257acdb129c31f65eab420b5b19e
wangchaochaohu <wangchao66@baidu.com>
2019-09-05 16:33:25 +0800

    Transformer revise (#193)
    * update transformer run.sh for the model update
    
    * fix test=develop
    
    * fix data path test=develop
    
    * fix data path test=develop
    
    * fix data path test=develop

--- NeuralMachineTranslation/Transformer/fluid/train/run_with_multi_process.sh
+++ NeuralMachineTranslation/Transformer/fluid/train/run_with_multi_process.sh
@@ -12,35 +12,13 @@ gen_data=/ssd3/transformer_1.1/gen_data
 cd ../../../../models/PaddleNLP/neural_machine_translation/transformer/
 
 # base model
-python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES train.py \
-  --src_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
-  --trg_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
-  --special_token '<s>' '<e>' '<unk>' \
-  --train_file_pattern $gen_data/train.tok.clean.bpe.32000.en-de.tiny \
-  --token_delimiter ' ' \
-  --use_token_batch True \
-  --batch_size 4096 \
-  --sort_type pool \
-  --pool_size 200000 \
-  --shuffle False \
-  --enable_ce True \
-  --shuffle_batch False \
-  --use_py_reader True \
-  --use_mem_opt True \
-  --fetch_steps 100  $@ \
-  dropout_seed 10 \
-  learning_rate 2.0 \
-  warmup_steps 8000 \
-  beta2 0.997 \
-  d_model 512 \
-  d_inner_hid 2048 \
-  n_head 8 \
-  prepostprocess_dropout 0.1 \
-  attention_dropout 0.1 \
-  relu_dropout 0.1 \
-  weight_sharing True \
-  pass_num 100 \
-  model_dir 'tmp_models' \
-  ckpt_dir 'tmp_ckpts'
+python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES main.py \
+	--do_train True \
+        --epoch 30 \
+        --src_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
+        --trg_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
+        --special_token '<s>' '<e>' '<unk>' \
+	--training_file $gen_data/wmt16_ende_data_bpe/train.tok.clean.bpe.32000.en-de.tiny \
+	--batch_size 4096
 
 cd -
