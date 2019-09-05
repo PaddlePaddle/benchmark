4d4b79316d278b8d6f7b1c39fbe169dd708ce441
wangchaochaohu <wangchao66@baidu.com>
2019-09-05 07:25:08 +0000

    fix test=develop

--- NeuralMachineTranslation/Transformer/fluid/train/run_with_multi_process.sh
+++ NeuralMachineTranslation/Transformer/fluid/train/run_with_multi_process.sh
@@ -13,12 +13,12 @@ cd ../../../../models/PaddleNLP/neural_machine_translation/transformer/
 
 # base model
 python -m paddle.distributed.launch --log_dir=./mylog --selected_gpus=$CUDA_VISIBLE_DEVICES main.py \
-         --do_train True \
+	--do_train True \
         --epoch 30 \
         --src_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
         --trg_vocab_fpath $gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
         --special_token '<s>' '<e>' '<unk>' \
 	--training_file $gen_data/wmt16_ende_data_bpe/train.tok.clean.bpe.32000.en-de \
-	--batch_size ${base_batch_size}
+	--batch_size 4096
 
 cd -
