ee728697b4dbd77e4492d9bd0aa48d3f8ff8cf05
Liu Yiqun <liuyiqun01@baidu.com>
2019-09-26 13:50:09 +0800

    Refine the script of senet.

--- static_graph/image_classification/pytorch/run_senet.sh
+++ static_graph/image_classification/pytorch/run_senet.sh
@@ -2,84 +2,74 @@
 
 set -xe
 
-echo ""
-echo "You can use xreki/maskrcnn-benchmark:lastest to test PyTorch 1.1"
-echo "Please run nvidia-docker with --shm-size information."
-echo "For example:"
-echo "  nvidia-docker run --name pytorch_test \\"
-echo "          --shm-size 16G --network=host -it --rm \\"
-echo "          -v $PWD:/work -w /work \\" 
-echo "          xreki/maskrcnn-benchmark:lastest \\"
-echo "          bash"
-echo "Then you may need install some python packages, use"
-echo "          pip install image"
-echo "          pip install scipy"
-echo ""
-
-#export CUDA_VISIBLE_DEVICES="0"
-#export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-export WORK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
-export BENCHMARK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../.." && pwd )"
-
-if [ $# -ne 2 ]; then
+if [ $# -lt 3 ]; then
   echo "Usage: "
-  echo "  CUDA_VISIBLE_DEVICES=0 bash run_senet.sh se_resnext_50|resnet_50 32"
+  echo "  CUDA_VISIBLE_DEVICES=0 bash run_senet.sh speed|mem|maxbs 32 se_resnext_50|resnet_50|resnet_101 sp|mp /ssd1/ljh/logs"
   exit
 fi
 
-# 1. task
-task=speed
+if [ "${BENCHMAKR_ROOT}" == "" ]; then
+    export BENCHMARK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../.." && pwd )"
+fi
 
-# 2. model: se_resnext_50, resnet_50, resnet_101
-model=${1:-se_resnext_50}
+function _set_params() {
+    index=$1
+    base_batch_size=$2
+    model_name=$3 # se_resnext_50, resnet_50, resnet_101
+    run_mode="sp" # Multi-processes is not supported
+    run_log_root=${5:-$(pwd)}
 
-# 3. batch_size
-base_batchsize=${2:-32}
-devices_str=${CUDA_VISIBLE_DEVICES//,/ }
-gpu_devices=($devices_str)
-num_gpu_devices=${#gpu_devices[*]}
-batch_size=`expr $base_batchsize \* $num_gpu_devices`
-num_workers=`expr 4 \* $num_gpu_devices`
+    skip_steps=2
+    keyword="Time:"
+    separator=" "
+    position=11
+    range=5
+    model_mode=3 # steps/s -> samples/s
 
-num_epochs=2
+    device=${CUDA_VISIBLE_DEVICES//,/ }
+    arr=($device)
+    num_gpu_devices=${#arr[*]}
 
-# 4. log_file
-log_file=log_senet_${model}_${task}_bs${batch_size}_${num_gpu_devices}
+    batch_size=`expr $base_batch_size \* $num_gpu_devices`
+    num_workers=`expr 4 \* $num_gpu_devices`
 
+    if [[ ${index} = "analysis" ]]; then
+        log_file=${run_log_root}/log_senet_${model_name}_speed_${num_gpu_devices}_${run_mode}
+    else
+        log_file=${run_log_root}/log_senet_${model_name}_${index}_${num_gpu_devices}_${run_mode}
+    fi
+    log_parse_file=${log_file}
+}
 
-train() {
-  echo "Train on ${num_gpu_devices} GPUs"
-  echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
-  python -c "import torch; print(torch.__version__)"
-  gpu_id=`echo $CUDA_VISIBLE_DEVICES |cut -c1`
-  nvidia-smi --id=$gpu_id --query-compute-apps=used_memory --format=csv -lms 100 > gpu_use.log 2>&1 &
-  gpu_memory_pid=$!
-  export PYTHONPATH=${WORK_ROOT}/vision
-  stdbuf -oL python ${WORK_ROOT}/train.py \
-        --network ${model} \
+function _set_env() {
+    echo "nothing ..."
+}
+
+function _train() {
+    echo "current CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=$num_gpu_devices, batch_size=$batch_size"
+    WORK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+    num_epochs=2
+
+    python -c "import torch; print(torch.__version__)"
+    export PYTHONPATH=${BENCHMARK_ROOT}/third_party/pytorch/vision
+
+    echo "${model_name}, batch_size: ${batch_size}"
+    cd ./SENet/
+    stdbuf -oL python train.py \
+        --network ${model_name} \
         --data-dir ImageData/ \
         --batch-size ${batch_size} \
         --num-workers ${num_workers} \
         --num-epochs ${num_epochs} \
         --gpus ${CUDA_VISIBLE_DEVICES} > ${log_file} 2>&1 &
-  train_pid=$!
-  sleep 600
-  kill -9 `ps -ef|grep 'python */SENet/train.py --network'|awk '{print $2}'`
-#  kill -9 $train_pid
+
+    train_pid=$!
+    sleep 300
+    kill -9 `ps -ef|grep python |awk '{print $2}'`
+    cd ${WORK_ROOT}
 }
 
-analysis() {
-  sed 's/batch\/sec/\ batch\/sec/' ${log_file} > tmp.txt
-  python ${BENCHMARK_ROOT}/scripts/analysis.py \
-    --filename tmp.txt \
-    --keyword "Time:" \
-    --separator " " \
-    --batch_size ${batch_size} \
-    --skip_steps 10 \
-    --mode 1
-  rm tmp.txt
-}
-
-train
-analysis
-awk 'BEGIN {max = 0} {if(NR>1){if ($1 > max) max=$1}} END {print "Max=", max}' gpu_use.log
+source ${BENCHMARK_ROOT}/scripts/run_model.sh
+_set_params $@
+_set_env
+_run
