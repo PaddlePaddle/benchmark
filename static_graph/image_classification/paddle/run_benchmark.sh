c3891c7a4971b0af799c7461e742023abd127d08
root <root@yq01-gpu-255-125-19-00.epc.baidu.com>
2019-11-28 03:40:11 +0000

    update run_benchmark

--- static_graph/image_classification/paddle/run_benchmark.sh
+++ static_graph/image_classification/paddle/run_benchmark.sh
@@ -62,7 +62,6 @@ function _train(){
            --batch_size=${batch_size} \
            --total_images=1281167 \
            --class_dim=1000 \
-           --image_shape=3,224,224 \
            --model_save_dir=output/ \
            --lr_strategy=piecewise_decay \
            --num_epochs=${num_epochs} \
