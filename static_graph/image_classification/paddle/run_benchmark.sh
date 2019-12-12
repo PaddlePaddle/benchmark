acd1d75fb5d8eaa7ac5e0108aef1f3cee916be76
liangjinhua01 <liangjinhua01@baidu.com>
2019-12-12 15:45:28 +0800

    modify run_benchmark.sh

--- static_graph/image_classification/paddle/run_benchmark.sh
+++ static_graph/image_classification/paddle/run_benchmark.sh
@@ -30,6 +30,7 @@ function _set_params(){
     arr=($device)
     num_gpu_devices=${#arr[*]}
 
+    # 多进程batch_size需要依据模型来修改
     batch_size=`expr ${base_batch_size} \* ${num_gpu_devices}`
     log_file=${run_log_path}/${model_name}_${index}_${num_gpu_devices}_${run_mode}
     log_with_profiler=${profiler_path}/${model_name}_${index}_${num_gpu_devices}_${run_mode}
