2b39f337a78a91737a9addb1115f0d7f8b78a7fa
Divano <zhengtianyu@baidu.com>
2020-03-10 16:37:41 +0800

    add seq2seq case (#324)
    * add seq2seq
    
    * add run benchmark
    
    * add mobilenet

---
+++ dygraph/mobilenet_v1/run_benchmark.sh
@@ -0,0 +1,1 @@
+python3.6 train.py      --batch_size=256        --total_images=1281167    --class_dim=1000 --image_shape=3,224,224 --model_save_dir=output.v1.sing/ --lr_strategy=piecewise_decay --lr=0.1   --data_dir=./data/ILSVRC2012  --l2_decay=3e-5  --model=MobileNetV1 
