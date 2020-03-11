1f993d210d025dfdfc9caa58af6107ddd360a033
Liu Yiqun <liuyiqun01@baidu.com>
2020-03-11 12:37:10 +0000

    Add the test of elementwise_mul.

---
+++ api/paddle/elementwise_mul.py
@@ -0,0 +1,40 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from main import parse_args, test_speed_main
+import paddle.fluid as fluid
+
+import sys
+sys.path.append("..")
+from common import paddle_api_benchmark as paddle_api
+
+class elementwise_mul(paddle_api.PaddleAPIBenchmarkBase):
+    def build_program(self, backward=False):
+        with fluid.program_guard(self.main_program, self.startup_program):
+            x = fluid.data(
+                name='x', shape=[50, 128, 1000], dtype='float32', lod_level=0)
+            y = fluid.data(
+                name='y', shape=[128, 1000], dtype='float32', lod_level=0)
+            x.stop_gradient = False
+            y.stop_gradient = False
+            result = fluid.layers.elementwise_mul(x, y)
+
+            self.feed_vars = [x, y]
+            self.fetch_vars = [result]
+            if backward:
+                self.append_gradients(result, [x, y])
+
+
+if __name__ == '__main__':
+    test_speed_main(elementwise_mul())
--- api/paddle/run.sh
+++ api/paddle/run.sh
@@ -3,11 +3,13 @@
 export CUDA_VISIBLE_DEVICES="1"
 #export GLOG_v=4
 
-python abs.py \
+name=${1:-"abs"}
+
+nvprof python ${name}.py \
       --run_with_executor True \
       --check_output False \
       --profiler "none" \
-      --backward False \
+      --backward True \
       --use_gpu True \
-      --repeat 1000 \
+      --repeat 100 \
       --log_level 1
