b8d5deb9ac72a4f0760b4d64b2848379e74732f1
Liu Yiqun <liuyiqun01@baidu.com>
2020-03-13 07:50:24 +0000

    Add test case of conv2d_transpose.

--- api/paddle/run.sh
+++ api/paddle/run.sh
@@ -2,14 +2,16 @@
 
 export CUDA_VISIBLE_DEVICES="1"
 #export GLOG_v=4
+#export LD_LIBRARY_PATH=/work/cudnn-7.6/lib64:${LD_LIBRARY_PATH}
 
 name=${1:-"abs"}
 
 python ${name}.py \
+      --dtype "float32" \
       --run_with_executor True \
       --check_output False \
       --profiler "none" \
-      --backward True \
+      --backward False \
       --use_gpu True \
       --repeat 100 \
-      --log_level 1
+      --log_level 0
