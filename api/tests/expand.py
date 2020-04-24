fd77420156a143c88ccad09ebbf8798cab805166
Liu Yiqun <liuyiqun01@baidu.com>
2020-04-24 07:36:11 +0000

    Update expand.

--- api/tests/expand.py
+++ api/tests/expand.py
@@ -18,53 +18,45 @@ import sys
 sys.path.append("..")
 from common import paddle_api_benchmark as paddle_api
 from common import tensorflow_api_benchmark as tensorflow_api
-
-
-class ExpandConfig(object):
-    def __init__(self, x_shape, expand_times):
-        self.x_shape = x_shape
-        self.expand_times = expand_times
-
-
-config = ExpandConfig(x_shape=[32, 807, 1], expand_times=[4, 1, 807])
-
-#config = ExpandConfig(x_shape=[32, 807, 807],
-#                      expand_times=[4, 1, 1])
+from common import api_param
 
 
 class PDExpand(paddle_api.PaddleAPIBenchmarkBase):
-    def build_program(self, backward=False, dtype=None):
+    def build_program(self, config):
         import paddle.fluid as fluid
 
         self.name = "expand"
         with fluid.program_guard(self.main_program, self.startup_program):
             x = fluid.data(
-                name='x', shape=config.x_shape, dtype='float32', lod_level=0)
+                name='x',
+                shape=config.x_shape,
+                dtype=config.x_dtype,
+                lod_level=0)
             x.stop_gradient = False
             result = fluid.layers.expand(x=x, expand_times=config.expand_times)
 
             self.feed_vars = [x]
             self.fetch_vars = [result]
-            if backward:
+            if config.backward:
                 self.append_gradients(result, [x])
 
 
 class TFExpand(tensorflow_api.TensorflowAPIBenchmarkBase):
-    def build_graph(self, backward=False, dtype=None):
+    def build_graph(self, config):
         import tensorflow as tf
 
         self.name = "expand"
         self.allow_growth = True
 
-        data = tf.placeholder(
-            name='data', shape=config.x_shape, dtype=tf.float32)
-        result = tf.tile(input=data, multiples=config.expand_times)
+        x = tf.placeholder(
+            name='x', shape=config.x_shape, dtype=tf.as_dtype(config.x_dtype))
+        result = tf.tile(input=x, multiples=config.expand_times)
 
-        self.feed_list = [data]
+        self.feed_list = [x]
         self.fetch_list = [result]
-        if backward:
-            self.append_gradients(result, [data])
+        if config.backward:
+            self.append_gradients(result, [x])
 
 
 if __name__ == '__main__':
-    test_main(PDExpand(), TFExpand(), feed_spec=None)
+    test_main(PDExpand(), TFExpand(), config=api_param.APIConfig("expand", ""))
