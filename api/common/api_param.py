880a6ade63456ed730db6f5c6f689dd6336dddfa
Liu Yiqun <liuyiqun01@baidu.com>
2020-04-26 07:49:32 +0000

    Add the support of list of Variable and update concat.

--- api/common/api_param.py
+++ api/common/api_param.py
@@ -15,28 +15,56 @@
 import json
 
 
+def parse_list(value_str, sub_dtype="int"):
+    if sub_dtype in ["int", "int64"]:
+        value_str = value_str.replace("L", "").replace("[", "").replace(
+            "]", "").split(',')
+        return map(int, value_str)
+    else:
+        # TODO: check and support list of other data type.
+        raise ValueError("Do not support parsing list of non-int data type.")
+
+
 class BaseParamInfo(object):
     def __init__(self, name, type, value):
-        self.name = name
-        self.type = type
-        self.value = value
+        self.name = self._encode_item(name)
+        self.type = self._encode_item(type)
+        self.value = self._translate_value(self._encode_item(value))
 
-    def convert_params_to_str(self):
-        param = self.name + '--' + self.type + '| ' + self.value + '\n '
-        return param
+    def _encode_item(self, item):
+        return item.encode("utf-8") if isinstance(item, str) else item
+
+    def to_string(self):
+        return self.name + '--' + self.type + '| ' + str(self.value) + '\n '
+
+    def _translate_value(self, value):
+        if self.type in ["float", "float32", "float64"]:
+            return float(value)
+        elif self.type in ["int", "int32", "int64"]:
+            return int(value)
+        elif self.type == "bool":
+            return bool(value)
+        elif self.type == "string":
+            return None if value == "None" else value
+        elif self.type == "list":
+            return parse_list(value, sub_dtype="int")
 
 
-class VarParamInfo(object):
+class VarParamInfo(BaseParamInfo):
     def __init__(self, name, type, dtype, shape, lod_level=0):
-        self.name = name
-        self.type = type
-        self.dtype = dtype
-        self.shape = shape
-        self.lod_level = lod_level
+        self.name = self._encode_item(name)
+        self.type = self._encode_item(type)
+        self.dtype = self._encode_item(dtype)
+        if isinstance(shape, str):
+            self.shape = parse_list(self._encode_item(shape))
+        else:
+            assert isinstance(shape, list)
+            self.shape = shape
+        self.lod_level = self._encode_item(lod_level)
 
-    def convert_params_to_str(self):
-        param = self.name + '--' + self.type + '| ' + self.dtype + '| shape:' + self.shape + '\n '
-        return param
+    def to_string(self):
+        return self.name + '--' + self.type + '| ' + str(
+            self.dtype) + '| shape:' + str(self.shape) + '\n '
 
 
 class APIConfig(object):
@@ -47,6 +75,32 @@ class APIConfig(object):
         self.params_list = []
         self.backward = False
         self.feed_spec = None
+
+    def init_from_json(self, filename, config_id=0):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            self.name = data[config_id]["op"]
+            self.params = data[config_id]["param_info"]
+
+        self._parse_params()
+        for param in self.params_list:
+            setattr(self, param.name, param.value)
+        for var in self.variable_list:
+            setattr(self, var.name + '_shape', var.shape)
+            setattr(self, var.name + '_dtype', var.dtype)
+        return self
+
+    def to_tensorflow(self):
+        return self
+
+    def to_string(self):
+        self._parse_params()
+        params = ""
+        for info in self.variable_list:
+            params = params + info.to_string()
+        for info in self.params_list:
+            params = params + info.to_string()
+        return params
 
     def __str__(self):
         debug_str = ('API params of <%s> {\n') % (self.name)
@@ -59,82 +113,24 @@ class APIConfig(object):
         debug_str = debug_str + '}'
         return debug_str
 
-    def init_from_json(self, filename, config_id=0):
-        with open(filename, 'r') as f:
-            data = json.load(f)
-            self.name = data[config_id]["op"]
-            self.params = data[config_id]["param_info"]
-
-        self._parse_params()
-        for params in self.params_list:
-            self._translate_param(
-                name=params.name.encode('utf-8'),
-                type=params.type.encode('utf-8'),
-                value=params.value.encode('utf-8'))
-        for input_p in self.variable_list:
-            self._translate_variable(
-                name=input_p.name.encode('utf-8'),
-                dtype=input_p.dtype.encode('utf-8'),
-                shape=input_p.shape.encode('utf-8'),
-                lod_level=input_p.lod_level)
-        return self
-
-    def to_tensorflow(self):
-        return self
-
-    def _convert_params_to_str(self):
-        params = ""
-        for name, value in self.params.items():
-            if self._is_variable(value):
-                info = VarParamInfo(name, value["type"], value["dtype"],
-                                    value["shape"])
-            else:
-                info = BaseParamInfo(name, value["type"], value["value"])
-            params = params + info.convert_params_to_str()
-        return params
-
     def _parse_params(self):
         for name, value in self.params.items():
-            if self._is_variable(value):
+            assert value.get("type", None) is not None
+            if value["type"] == "Variable":
                 info = VarParamInfo(name, value["type"], value["dtype"],
                                     value["shape"])
+                self.variable_list.append(info)
+            elif value["type"] == "list<Variable>":
+                dtype_list = []
+                shape_list = []
+                for key, var in value.items():
+                    if key != "type":
+                        assert var["type"] == "Variable"
+                        dtype_list.append(var["dtype"])
+                        shape_list.append(parse_list(var["shape"]))
+                info = VarParamInfo(name, value["type"], dtype_list,
+                                    shape_list)
                 self.variable_list.append(info)
             else:
                 info = BaseParamInfo(name, value["type"], value["value"])
                 self.params_list.append(info)
-
-    def _parse_list(self, value_str, dtype):
-        value_str = value_str.replace("L", "").replace("[", "").replace(
-            "]", "").split(',')
-        # TODO: check and support list of other data type.
-        if dtype == "int":
-            return map(int, value_str)
-        else:
-            raise ValueError(
-                "Do not support parsing list of non-int data type.")
-
-    def _is_variable(self, value):
-        if value.get("type", None) is not None and value["type"] == "Variable":
-            return True
-        return False
-
-    def _translate_param(self, name, type, value):
-        value_t = None
-        if type in ["float", "float32", "float64"]:
-            value_t = float(value)
-        elif type in ["int", "int32", "int64"]:
-            value_t = int(value)
-        elif type == "bool":
-            value_t = bool(value)
-        elif type == "string":
-            if value == "None":
-                value_t = None
-            else:
-                value_t = value
-        elif type == "list":
-            value_t = self._parse_list(value, dtype="int")
-        setattr(self, name, value_t)
-
-    def _translate_variable(self, name, dtype, shape, lod_level):
-        setattr(self, name + '_shape', self._parse_list(shape, dtype="int"))
-        setattr(self, name + '_dtype', dtype)
