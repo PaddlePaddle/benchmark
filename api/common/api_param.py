00f300fab2763fe8e5a7ff4c19770df787878ce2
Liu Yiqun <liuyiqun01@baidu.com>
2020-04-29 03:03:32 +0000

    Change the support of atol.

--- api/common/api_param.py
+++ api/common/api_param.py
@@ -100,6 +100,11 @@ class APIConfig(object):
             data = json.load(f)
             self.name = data[config_id]["op"]
             self.params = data[config_id]["param_info"]
+            if data[config_id].get("atol", None) is not None:
+                if isinstance(data[config_id]["atol"], str):
+                    self.atol = float(data[config_id]["atol"])
+                elif isinstance(data[config_id]["atol"], float):
+                    self.atol = data[config_id]["atol"]
 
         self._parse_params()
         for param in self.params_list:
