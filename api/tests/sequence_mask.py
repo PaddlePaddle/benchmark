373a46a00df19d20c610d09dcbf65929cd2e3480
GaoWei8 <53294385+GaoWei8@users.noreply.github.com>
2020-05-19 19:29:21 +0800

    add API test (#416)
    * add api test

---
+++ api/tests/sequence_mask.py
@@ -0,0 +1,61 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+class SequenceMaskConfig(APIConfig):
+    def __init__(self):
+        super(SequenceMaskConfig, self).__init__('sequence_mask')
+
+    def to_tensorflow(self):
+        tf_config = self
+        tf_config.dtype = tf.dtypes.as_dtype(self.dtype)
+        return tf_config
+
+
+class PDSequenceMask(PaddleAPIBenchmarkBase):
+    def build_program(self, config):
+        with fluid.program_guard(self.main_program, self.startup_program):
+            data = fluid.data(
+                name='data',
+                shape=config.x_shape,
+                dtype=config.x_dtype,
+                lod_level=0)
+            data.stop_gradient = False
+            result = fluid.layers.sequence_mask(
+                x=data, maxlen=config.maxlen, dtype=config.dtype)
+
+            self.feed_vars = [data]
+            self.fetch_vars = [result]
+            if config.backward:
+                self.append_gradients(result, [data])
+
+
+class TFSequenceMask(TensorflowAPIBenchmarkBase):
+    def build_graph(self, config):
+        data = self.placeholder(
+            name='data', shape=config.x_shape, dtype=config.x_dtype)
+        print(config.dtype)
+        result = tf.sequence_mask(
+            lengths=data, maxlen=config.maxlen, dtype=config.dtype)
+
+        self.feed_list = [data]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [data])
+
+
+if __name__ == '__main__':
+    test_main(PDSequenceMask(), TFSequenceMask(), config=SequenceMaskConfig())
