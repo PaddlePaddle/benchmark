7c03897baf1e0dceca4a44004d53f231e9a4c4c5
Yiqun Liu <liuyiqun01@baidu.com>
2020-05-21 09:36:03 +0800

    Fix the error in elementwise's broadcast json, and polish the module import codes (#422)
    * Add the config of elementwise backward and fix axis.
    
    * Polish the codes of importing tf functions and add activation, elementwise to CI.

--- api/tests/activation.py
+++ api/tests/activation.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from common_import import *
 
 
@@ -19,11 +20,12 @@ class ActivationConfig(APIConfig):
         super(ActivationConfig, self).__init__('activation')
         self.api = 'cos'
         self.api_list = {
-            'abs': 'abs',
             'cos': 'cos',
             'exp': 'exp',
             'floor': 'floor',
-            'sigmoid': 'sigmoid'
+            'square': 'square',
+            'tanh': 'tanh',
+            'sigmoid': 'sigmoid',
         }
 
     def to_tensorflow(self):
