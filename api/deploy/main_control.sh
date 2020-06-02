7a37317f0b86c0dd37d5c82d76723b904c9cc8c6
GaoWei8 <catherin.gao@foxmail.com>
2020-06-02 12:06:02 +0000

    polish codes

--- api/deploy/main_control.sh
+++ api/deploy/main_control.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
 
 python register.py
 
-export PYTHONPATH=/work/benchmark/api/tests:${PYTHONPATH}
+export PYTHONPATH=/home/gaowei/padding/tf_com/benchmark/api/tests:${PYTHONPATH}
 py_dir="../tests"
 api_info_file="auto_run_info.txt"
 json_dir=$1
@@ -18,16 +18,25 @@ path=$(cd $(dirname $0);pwd)
 
 device=("cpu" "gpu")
 device_set=(false true)
+direction=("forward" "backward")
+direction_set=(false true)
 framwork=("paddle" "tensorflow")
 feature=("speed" "accuracy")
+api_num=0
+no_backwards=()
 
 for line in `cat $api_info_file`
 do
+    api_num=$[$api_num+1]
     api_name=$(echo $line| cut -d',' -f1)
     api=$(echo $line| cut -d',' -f2)
     json_file=$(echo $line| cut -d',' -f3)
     backward=$(echo $line| cut -d',' -f4)
-    
+    if [ ${backward} = False ]; then  
+        length=${#no_backwards[@]}
+        length=$[$length+1]
+        no_backwards[${length}]=${api_name}  
+    fi
     if [ "$json_file" != "None" ]
     then
         casesNum=$(grep "op" $path/$json_dir/$json_file |wc -l)
@@ -35,40 +44,60 @@ do
         casesNum=1
     fi
 
-    echo "api_name: "${api_name}", api: "${api}", json_file: "${json_file}", backward: "${backward}", num_json: "${casesNum}
+    if [ "$json_file" != "None" ]; then
+        json_file_name=$path/$json_dir/${json_file}
+    else
+        json_file_name=None
+    fi
+
     for((i=0;i<casesNum;i++));
     do
         for j in "${!device[@]}";
         do 
             for k in "${!framwork[@]}";
             do 
-                for n in "${!feature[@]}";
-                do 
+                for m in "${!direction[@]}"; 
+                do
+                    if [ ${direction[$m]} = "backward" ] && [ ${backward} = False ]; then
+                        continue
+                    fi
                     if [ "${device[$j]}" = "gpu" ]; then
-                        repeat=1000
+                        repeat=1
                     else
-                        repeat=100
+                        repeat=1
                     fi
-                    if [ "$json_file" != "None" ]; then 
-                        json_file_name=$path/$json_dir/${json_file}
-                    else
-                        json_file_name=None
-                    fi
-                    logfile=$res_dir/${api_name}"-"${framwork[$k]}"_"${device[$j]}"_"${feature[$n]}"_"${i}".txt"
+                    logfile=$res_dir/${api_name}"-"${framwork[$k]}"_"${device[$j]}"_speed_""${direction[$m]}""_"${i}".txt"
+                    echo "api_name: "${api_name}", api: "${api}", "${direction[$m]}", json_file: "${json_file}", json_id: "${i}
                     echo ${logfile}
 
                     python -m launch $path/$py_dir/${api}.py \
                       --api_name ${api_name} \
-                      --task ${feature[$n]} \
                       --framework ${framwork[$k]} \
                       --json_file ${json_file_name} \
                       --config_id $i \
-                      --backward ${backward} \
+                      --backward ${direction_set[$m]} \
                       --use_gpu ${device_set[$j]} \
                       --repeat $repeat \
                       --log_level 0 > $logfile 2>&1
-                done
+               done
             done
-        done
+
+            logfile=$res_dir/${api_name}"-paddle_"${device[$j]}"_accuracy_forward_"${i}".txt"
+            echo "api_name: "${api_name}", api: "${api}", forward, json_file: "${json_file}", json_id: "${i}
+            echo ${logfile}
+
+            python -m launch $path/$py_dir/${api}.py \
+              --api_name ${api_name} \
+              --task accuracy \
+              --framework paddle \
+              --json_file ${json_file_name} \
+              --config_id $i \
+              --backward ${direction_set[$m]} \
+              --use_gpu ${device_set[$j]} \
+              --repeat $repeat \
+              --log_level 0 > $logfile 2>&1
+       done
     done
 done
+echo "The number of whole API: "${api_num}
+echo "APIs which has no backwards: "${no_backwards[@]}
