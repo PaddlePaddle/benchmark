5f1b5668d68a74317677f0733709686387fdc870
GaoWei8 <catherin.gao@foxmail.com>
2020-06-02 08:11:16 +0000

    polish codes

--- api/deploy/main_control.sh
+++ api/deploy/main_control.sh
@@ -2,10 +2,13 @@
 
 export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
 
+python register.py
+
+export PYTHONPATH=/work/benchmark/api/tests:${PYTHONPATH}
 py_dir="../tests"
-auto_file=$1
-json_dir=$2
-res_dir=$3
+api_info_file="auto_run_info.txt"
+json_dir=$1
+res_dir=$2
 if [ ! -d $res_dir ]
 then
     mkdir $res_dir
@@ -18,7 +21,7 @@ device_set=(false true)
 framwork=("paddle" "tensorflow")
 feature=("speed" "accuracy")
 
-for line in `cat $auto_file`
+for line in `cat $api_info_file`
 do
     api_name=$(echo $line| cut -d',' -f1)
     api=$(echo $line| cut -d',' -f2)
@@ -32,6 +35,7 @@ do
         casesNum=1
     fi
 
+    echo "api_name: "${api_name}", api: "${api}", json_file: "${json_file}", backward: "${backward}", num_json: "${casesNum}
     for((i=0;i<casesNum;i++));
     do
         for j in "${!device[@]}";
@@ -40,28 +44,24 @@ do
             do 
                 for n in "${!feature[@]}";
                 do 
-                    if [ "${device[$j]}" = "gpu" ]
-                    then
+                    if [ "${device[$j]}" = "gpu" ]; then
                         repeat=1000
                     else
                         repeat=100
                     fi
-                    if [ "$json_file" != "None" ]
-                    then 
-                        jfile_name=$path/$json_dir/${json_file}
+                    if [ "$json_file" != "None" ]; then 
+                        json_file_name=$path/$json_dir/${json_file}
                     else
-                        jfile_name=None
+                        json_file_name=None
                     fi
                     logfile=$res_dir/${api_name}"-"${framwork[$k]}"_"${device[$j]}"_"${feature[$n]}"_"${i}".txt"
-                    if [ -e $logfile ]
-                    then
-                        echo "" >  $logfile
-                    fi
-                    python $path/$py_dir/${api}.py \
+                    echo ${logfile}
+
+                    python -m launch $path/$py_dir/${api}.py \
                       --api_name ${api_name} \
                       --task ${feature[$n]} \
                       --framework ${framwork[$k]} \
-                      --json_file ${jfile_name} \
+                      --json_file ${json_file_name} \
                       --config_id $i \
                       --backward ${backward} \
                       --use_gpu ${device_set[$j]} \
