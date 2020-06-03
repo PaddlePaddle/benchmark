2c4339ce6eb3417ac40d7d6d0e30857fc9664ae1
Liu Yiqun <liuyiqun01@baidu.com>
2020-06-03 06:22:17 +0000

    Improve the codes of op frequency summary.

--- api/json/process_json.py
+++ api/json/process_json.py
@@ -15,6 +15,8 @@
 import json
 import os
 import argparse
+import warnings
+import collections
 
 import sys
 sys.path.append("..")
@@ -26,22 +28,26 @@ params_list = []
 result_json = []
 op_dicts_sum = {}
 op_frequncy_each = {}
-op_frequncy_file = 'op_frequency.txt'
 
 
 def get_index(lst=None, item=''):
     return [i for i in range(len(lst)) if lst[i] == item]
 
 
-def parse():
+def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         '--json_path',
         type=str,
         default=None,
-        help='The json file name or direction')
+        help='The json file name or directory')
     parser.add_argument(
         '--output_dir',
+        type=str,
+        default=None,
+        help='The output directory of json files')
+    parser.add_argument(
+        '--op_frequency_path',
         type=str,
         default=None,
         help='The direction of output json file')
@@ -68,6 +74,9 @@ def remove_duplicates(args):
                 data = json.load(f)
                 for i in range(0, len(data)):
                     op = data[i]["op"]
+                    if op != "depthwise_conv2d" and op in special_op_list.ALIAS_OP_MAP.keys(
+                    ):
+                        op = special_op_list.ALIAS_OP_MAP[op]
                     param = data[i]["param_info"]
                     if check_frequency(op, param):
                         op_file_dict[op] = op_file_dict.get(op, 0) + 1
@@ -92,65 +101,76 @@ def remove_duplicates(args):
             op_frequncy_each[file] = op_file_dict
 
 
-def fwrite_json(args):
-    dir = os.path.join(os.getcwd(), args.output_dir)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+def write_op_configs(args):
+    # Clear the output directory.
+    output_dir = os.path.abspath(args.output_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     else:
-        file = os.listdir(dir)
-        for fi in file:
-            if os.path.getsize(os.path.join(dir, fi)):
-                os.remove(os.path.join(dir, fi))
+        files = os.listdir(output_dir)
+        if len(files) > 0:
+            warnings.warn(
+                "All %d files in output directory (%s) will be cleared and rewriten."
+                % (len(files), output_dir))
+        for filename in files:
+            absolute_path = os.path.join(output_dir, filename)
+            if os.path.getsize(absolute_path):
+                os.remove(absolute_path)
+
+    all_op_configs = {}
     for i in range(len(op_list)):
-        op = (op_list[i] + '.json').encode("utf-8")
-        with open(os.path.join(dir, op), 'a') as fw:
-            fw.writelines(
-                json.dumps(
-                    result_json[i], sort_keys=True, indent=4) + ',\n')
-        fw.close()
-    file = os.listdir(dir)
-    print('The json files will be rewrite: ')
-    for i in file:
-        with open(os.path.join(dir, i), 'r+') as f:
-            print('The json file after processing: ' + os.path.join(dir, i))
-            content = f.read()
-            f.seek(0, 0)
-            f.write('[\n' + content)
-            f.seek(-2, 2)
-            f.truncate()
-            f.seek(0, 2)
-            f.truncate()
-            f.write('\n]\n')
-        f.close()
-        with open(os.path.join(dir, i), 'r') as fs:
-            data = json.load(fs)
-        fs.close()
-        print('The number of params: ' + str(len(data)))
+        op_type = op_list[i]
+        if all_op_configs.get(op_type, None) is None:
+            all_op_configs[op_type] = []
+        all_op_configs[op_type].append(result_json[i])
+
+    print("Update configs for %d operators." % len(all_op_configs))
+    for op_type, config_list in sorted(all_op_configs.items()):
+        op_json_path = os.path.join(output_dir, op_type + ".json")
+        print("-- Write %4d configs to %s" % (len(config_list), op_json_path))
+        with open(op_json_path, 'w') as f:
+            f.writelines(json.dumps(config_list, sort_keys=True, indent=4))
+    print("")
 
 
-def write_dict(args):
-    dir = os.path.join(os.getcwd(), args.output_dir)
-    with open(os.path.join(dir, op_frequncy_file), 'w') as fre:
-        for file, op_dicts in op_frequncy_each.items():
-            if file is not None:
-                fre.writelines(file + ' frequency: \n')
-            op_dicts_sorted = sorted(
-                op_dicts.items(), key=lambda d: d[1], reverse=True)
-            for op in op_dicts_sorted:
-                fre.writelines(str(op[0]) + ' : ' + str(op[1]) + '\n')
-            fre.writelines('\n\n')
-        fre.writelines('Summary frequency: \n')
-        op_dicts_sum_sorted = sorted(
-            op_dicts_sum.items(), key=lambda d: d[1], reverse=True)
-        for op in op_dicts_sum_sorted:
-            fre.writelines(str(op[0]) + ' : ' + str(op[1]) + '\n')
-        fre.writelines('\n')
-    fre.close()
-    print('The op frequency file: ' + os.path.join(dir, op_frequncy_file))
+def write_op_frequency(args):
+    def _translate_to_string(title, frequency_dict):
+        frequency_str = ""
+        frequency_str += "======================================================\n"
+        frequency_str += "  " + title + "\n"
+        frequency_str += "======================================================\n"
+        frequency_dict_sorted = sorted(
+            frequency_dict.items(), key=lambda d: d[1], reverse=True)
+        for i in range(len(frequency_dict_sorted)):
+            item = frequency_dict_sorted[i]
+            frequency_str += "%s %s    %d\n" % (str(i + 1).ljust(4),
+                                                item[0].ljust(36), item[1])
+        frequency_str += "\n"
+        return frequency_str
+
+    if args.op_frequency_path is not None:
+        op_frequency_path = args.op_frequency_path
+    else:
+        output_dir = os.path.abspath(args.output_dir)
+        op_frequency_path = os.path.join(output_dir, 'op_frequency.txt')
+
+    with open(op_frequency_path, 'w') as f:
+        for filename, op_frequency_dict in op_frequncy_each.items():
+            f.writelines(_translate_to_string(filename, op_frequency_dict))
+
+        frequency_summary_title = "Summary of %d models" % len(
+            op_frequncy_each)
+        frequency_str_summary = _translate_to_string(frequency_summary_title,
+                                                     op_dicts_sum)
+        print(frequency_str_summary)
+        f.writelines(frequency_str_summary)
+
+    print("-- Write frequency results of %d models to %s." %
+          (len(op_frequncy_each), op_frequency_path))
 
 
 if __name__ == '__main__':
-    args = parse()
+    args = parse_args()
     remove_duplicates(args)
-    fwrite_json(args)
-    write_dict(args)
+    write_op_configs(args)
+    write_op_frequency(args)
