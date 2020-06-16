e1f4c9998f35c4b80308165146e1f8b33db0c2ff
Yiqun Liu <liuyiqun01@baidu.com>
2020-06-16 17:35:08 +0800

    Update arguments of dygraph's tesing script files. (#464)

--- dynamic_graph/resnet/paddle/run_benchmark.sh
+++ dynamic_graph/resnet/paddle/run_benchmark.sh
@@ -1,9 +1,10 @@
 #!bin/bash
+
 set -xe
 if [[ $# -lt 1 ]]; then
     echo "running job dict is {1: speed, 2:mem, 3:profiler, 6:max_batch_size}"
     echo "Usage: "
-    echo "  CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh 1|2|3 sp|mp 600(max_iter)"
+    echo "  CUDA_VISIBLE_DEVICES=0 bash $0 1|2|3 600(max_iter)"
     exit
 fi
 
@@ -13,7 +14,7 @@ function _set_params(){
     model_name="resnet"
 
     run_mode="sp" # Don't support mp
-    max_iter=${3}
+    max_iter=${2}
     if [[ ${index} -eq 3 ]]; then is_profiler=1; else is_profiler=0; fi
  
     run_log_path=${TRAIN_LOG_DIR:-$(pwd)}
@@ -22,20 +23,14 @@ function _set_params(){
     mission_name="图像分类"
     direction_id=0
     skip_steps=5
-    keyword="batch cost:"
+    keyword="batch_cost:"
     separator=" "
-    position=14
-    model_mode=0 # s/step -> steps/s
+    position=11
+    model_mode=0 # s/step -> samples/s
 
     device=${CUDA_VISIBLE_DEVICES//,/ }
     arr=($device)
     num_gpu_devices=${#arr[*]}
-
-    if [[ ${run_mode} = "sp" ]]; then
-        batch_size=`expr $base_batch_size \* $num_gpu_devices`
-    else
-        batch_size=$base_batch_size
-    fi
 
     log_file=${run_log_path}/dynamic_${model_name}_${index}_${num_gpu_devices}_${run_mode}
     log_with_profiler=${profiler_path}/dynamic_${model_name}_3_${num_gpu_devices}_${run_mode}
@@ -44,22 +39,13 @@ function _set_params(){
     log_parse_file=${log_file}
 }
 
-function _set_env(){
-    #开启gc
-    echo "nothing"
-}
-
-echo ${CUDA_VISIBLE_DEVICES}
-
-
 function _train(){
     train_cmd="--max_iter ${max_iter} \
-              --batch_size=${batch_size}
-              "
-#              --data_dir=./ILSVRC2012_Pytorch/dataset_100/  
-#              --model=ResNet50
-#              --is_profiler=${is_profiler} \
-#              --profiler_path=${profiler_path} \
+               --batch_size=${base_batch_size} \
+               --class_dim=1000 \
+               --use_imagenet_data \
+               --data_dir=./data/ILSVRC2012 \
+               "
     if [ ${num_gpu_devices} -eq 1 ]; then
         train_cmd="python -u train.py "${train_cmd}
     else
@@ -69,7 +55,6 @@ function _train(){
     fi
     
     ${train_cmd} > ${log_file} 2>&1
-    kill -9 `ps -ef|grep python |awk '{print $2}'`
     if [ ${num_gpu_devices} != 1  -a -d mylog ]; then
         rm ${log_file}
         cp mylog/workerlog.0 ${log_file}
@@ -79,5 +64,4 @@ function _train(){
 
 source ${BENCHMARK_ROOT}/scripts/run_model.sh
 _set_params $@
-_set_env
 _run
