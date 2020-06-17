1c576e6df0faccf92223b7c1867c190e26a3ce77
heya02 <heya02@baidu.com>
2020-06-17 12:22:28 +0000

    add dynamic transformer models multi gpu mode

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -109,7 +109,7 @@ dy_transformer(){
     sed -i '/set\ -xe/d' run_benchmark.sh
     echo "index is speed, 1gpu begin"
     CUDA_VISIBLE_DEVICES=5 bash run_benchmark.sh 1 sp 3000 | tee ${log_path}/dynamic_${FUNCNAME}_speed_1gpus 2>&1
-#    sleep 60
-#    echo "index is speed, 8gpus begin"
-#    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 3000 | tee ${log_path}/dynamic_${FUNCNAME}_speed_8gpus 2>&1
+    sleep 60
+    echo "index is speed, 8gpus begin, mp"
+    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 mp 3000 | tee ${log_path}/dynamic_${FUNCNAME}_speed_8gpus 2>&1
 }
