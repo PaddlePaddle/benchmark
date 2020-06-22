2ea81e469b6e19e2bda1d004d28e255121ff46d9
Liu Yiqun <liuyiqun01@baidu.com>
2020-06-22 11:36:20 +0000

    Correct the value of backward for series ops.

--- api/run_op_benchmark.sh
+++ api/run_op_benchmark.sh
@@ -11,4 +11,4 @@ fi
 
 config_dir=${OP_BENCHMARK_ROOT}/tests/configs
 log_path=${OP_BENCHMARK_ROOT}/logs/log_${timestamp}.txt
-bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${config_dir} ${output_dir} > ${log_path} 2>&1
+bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${config_dir} ${output_dir} > ${log_path} 2>&1 &
