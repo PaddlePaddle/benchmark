f6410e374ac87756de7254971165493a9fa3303f
Liu Yiqun <liuyiqun01@baidu.com>
2020-07-06 01:23:59 +0000

    Support python3.

--- api/common/api_param.py
+++ api/common/api_param.py
@@ -71,9 +71,18 @@ def parse_list(value_str, sub_dtype="int"):
 def parse_tuple(value_str, sub_dtype="int"):
     value_str = parse_string(value_str)
     if sub_dtype in ["int", "int64"]:
-        value_str = value_str.replace("L", "").replace("(", "").replace(
-            ")", "").split(',')
-        return map(int, value_str)
+        try:
+            if value_str != "()":
+                value_str_list = value_str.replace("L", "").replace(
+                    "(", "").replace(")", "").split(',')
+                value_list = []
+                for item in value_str_list:
+                    value_list.append(int(item))
+                return value_list
+            else:
+                return []
+        except Exception as e:
+            assert False, "Parse {} failed: {}".format(value_str, e)
     else:
         # TODO: check and support list of other data type.
         raise ValueError("Do not support parsing list of non-int data type.")
