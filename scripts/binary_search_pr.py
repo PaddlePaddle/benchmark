295c3e927a24c927f655d75a93a2fe26129ef4b0
xiege01 <xiege01@baidu.com>
2020-09-17 21:32:59 +0800

    add binary search pr tool

--- scripts/binary_search_pr.py
+++ scripts/binary_search_pr.py
@@ -1,13 +1,8 @@
 import argparse
 import os
 import sys
+import subprocess
 import numpy as np
-
-base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
-sys.path.append(base_path)
-print sys.path
-import models.benchmark_server.helper as helper
-from benchmark_server import benchmark_models as bm
 
 
 def parse_args():
@@ -17,16 +12,16 @@ def parse_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--start_day',
+        '--start_commit',
         type=str,
-        default='2020-01-01',
-        help='start day of searching, the correct pr is found in this day'
+        default='8ec4af278d59d58ed9acc4657c7b6126fcfd8992',
+        help='the start pr of searching, it is a correct pr.'
     )
     parser.add_argument(
-        '--end_day',
+        '--end_commit',
         type=str,
-        default='2020-09-15',
-        help='end day of searching, the wrong pr is found in this day'
+        default='3755564ae14b96a07412b0035bc22cdea52d43ac',
+        help='the end pr of searching, it is a pr with problem'
     )
     parser.add_argument(
         '--is_perf',
@@ -37,26 +32,20 @@ def parse_args():
     parser.add_argument(
         '--command',
         type=str,
-        default='sh /ssd3/xiege/run_benchmark.sh',
-        help='path of the running script'
+        default='sh run_benchmark.sh',
+        help='command of running the script'
     )
     parser.add_argument(
-        '--model_name',
-        type=str,
-        default='bert',
-        help='model_name,only needed when is_perf=True'
+        '--stardard_value',
+        type=float,
+        default=0.0,
+        help='the stardard value,only needed when is_perf=True'
     )
     parser.add_argument(
-        '--run_machine_type',
-        type=str,
-        default='ONE_GPU',
-        help='ONE_GPU|FOUR_GPU|MULTI_GPU|MULTI_GPU_MULTI_PROCESS,only needed when is_perf=True'
-    )
-    parser.add_argument(
-        '--graph_type',
-        type=str,
-        default='static_graph',
-        help='static_graph|dynamic_graph,only needed when is_perf=True'
+        '--threshold',
+        type=float,
+        default=0.05,
+        help='the threshold of alarming, default is 0.05'
     )
     args = parser.parse_args()
     return args
@@ -67,34 +56,24 @@ def compile(commit_id):
     os.system(reset_command)
     command = 'rm -rf build && mkdir build'
     os.system(command)
+    build_path = os.path.join(paddle_path, 'build')
+    os.chdir(build_path)
     print('commit {} build start'.format(commit_id))
     cmake_command = 'cmake .. -DPY_VERSION=3.7 -DWITH_GPU=ON -DWITH_DISTRIBUTE=OFF -DWITH_TESTING=OFF -DWITH_INFERENCE_API_TEST=OFF -DON_INFER=OFF -DCMAKE_BUILD_TYPE=Release'
-    build_command = 'make - j$(nproc)'
+    build_command = 'make -j12'
     os.system(cmake_command)
     os.system(build_command)
     print('commit {} build done'.format(commit_id))
-    install_command = 'pip install python/dist/paddlepaddle_gpu-0.0.0-cp37-cp37m-linux_x86_64.whl'
+    install_command = 'pip install -U python/dist/paddlepaddle_gpu-0.0.0-cp37-cp37m-linux_x86_64.whl'
     os.system(install_command)
     print('commit {} install done'.format(commit_id))
 
 
-def get_standard_value(model, env):
-    if args.graph_type == 'static_graph':
-        job_type = 2
-    else:
-        job_type = 3
-    benchmark_results = bm.ViewJobResult.objects.filter(model_name=args.model_name, report_index_id=1,
-                                                        job_type=job_type, cuda_version=10.1, cudnn_version=7,
-                                                        device_type='v100', model_implement_type=args.graph_type,
-                                                        frame_name="paddlepaddle", run_machine_type=args.graph_type,
-                                                        benchmark=1).order_by('-version')
-    benchmark_results = [float(i.report_result) for i in benchmark_results[:1]] if benchmark_results else []
-    benchmark_result = round(np.array(benchmark_results).mean(), 4)
-    return benchmark_result
-
-
 def check_success(commit_id):
+    os.chdir(paddle_path)
     compile(commit_id)
+    os.chdir(base_path)
+    print('base_path:{}'.format(base_path))
     if not args.is_perf:
         cmd = args.command
         if os.system(cmd) == 0:
@@ -102,28 +81,40 @@ def check_success(commit_id):
         else:
             return False
     else:
-        cmd = args.command + '| tee run.log'
-        os.system(cmd)
-        cmd = 'cat run.log | tr "," "\n" | grep FINAL_RESULT | tee result.log'
-        os.system(cmd)
-        with open('result.log', 'r') as f:
+        cmd = args.command
+        log = subprocess.getstatusoutput(cmd)
+        print('log:{}'.format(log[1]))
+        f = open('log.txt', 'w')
+        f.writelines(log[1])
+        f.close()
+        cmd = 'cat log.txt | tr "," "\n" | grep FINAL_RESULT'
+        log_result = subprocess.getstatusoutput(cmd)
+        f = open('log_result.txt', 'w')
+        f.writelines(log_result[1])
+        f.close()
+        with open('log_result.txt', 'r') as f:
             sub_str = f.split(':')
             result = float(sub_str[1].strip('\n'))
-            standard_value = get_standard_value(args.model, args.env)
-            if abs((result - standard_value) / standard_value) <= 0.5:
+            standard_value = args.standard_value
+            if abs((result - standard_value) / standard_value) <= args.threshold:
                 return True
             else:
                 return False
 
 
 def get_commits(start, end):
-    cmd = 'git log --pretty=oneline --after="{}" --before="{}" | tee git.log'.format(start, end)
-    os.system(cmd)
+    print('start:{}'.format(start))
+    print('end:{}'.format(end))
+    cmd = 'git log {}..{} --pretty=oneline'.format(start, end)
+    log = subprocess.getstatusoutput(cmd)
+    print(log[1])
     commit_list = []
-    with open("git.log", 'r') as f:
-        for line in f:
-            commit = line.split()[0]
-            commit_list.append(commit)
+    candidate_commit = log[1].split('\n')
+    print(candidate_commit)
+    for commit in candidate_commit:
+        commit = commit.split(' ')[0]
+        print('commit:{}'.format(commit))
+        commit_list.append(commit)
     return commit_list
 
 
@@ -149,8 +140,12 @@ def binary_search(commits):
 
 if __name__ == '__main__':
     args = parse_args()
-    cmd = 'git clone http://github.com/paddlepaddle/paddle.git && cd paddle'
+    base_path = os.getcwd()
+    paddle_path = os.path.join(base_path, 'paddle')
+    cmd = 'rm -rf paddle && git clone http://github.com/paddlepaddle/paddle.git'
     os.system(cmd)
-    commits = get_commits(start=args.start_day + ' ' + '0:00:00', end=args.end_day + ' ' + '23:59:59')
+    os.chdir(paddle_path)
+    commits = get_commits(start=args.start_commit, end=args.end_commit)
+    print('the candidate commits is {}'.format(commits))
     final_commit = binary_search(commits)
     print('the pr with problem is {}'.format(final_commit))
