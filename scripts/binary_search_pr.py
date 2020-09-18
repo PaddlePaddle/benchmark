91d21b5e71dd0ab744ffcac5a28ea3db22183972
xiegegege <xiege01@baidu.com>
2020-09-18 11:04:16 +0800

    add binary search pr tool

--- scripts/binary_search_pr.py
+++ scripts/binary_search_pr.py
@@ -20,8 +20,8 @@ def parse_args():
     parser.add_argument(
         '--end_commit',
         type=str,
-        default='3755564ae14b96a07412b0035bc22cdea52d43ac',
-        help='the end pr of searching, it is a pr with problem'
+        default='4adac0e30978f3a28d10adf30f319e96ee3e03ed',
+        help='the end pr of searching, it is a pr with problem.'
     )
     parser.add_argument(
         '--is_perf',
@@ -32,14 +32,14 @@ def parse_args():
     parser.add_argument(
         '--command',
         type=str,
-        default='sh run_benchmark.sh',
+        default='CUDA_VISIBLE_DEVICES=7 bash run_benchmark.sh 1 sp 300',
         help='command of running the script'
     )
     parser.add_argument(
-        '--stardard_value',
+        '--standard_value',
         type=float,
-        default=0.0,
-        help='the stardard value,only needed when is_perf=True'
+        default=65.906,
+        help='the standard value, only needed when is_perf=True'
     )
     parser.add_argument(
         '--threshold',
@@ -52,6 +52,8 @@ def parse_args():
 
 
 def compile(commit_id):
+    uninstall_command = 'pip uninstall paddlepaddle-gpu -y'
+    os.system(uninstall_command)
     reset_command = 'git reset --hard %s' % commit_id
     os.system(reset_command)
     command = 'rm -rf build && mkdir build'
@@ -62,16 +64,21 @@ def compile(commit_id):
     cmake_command = 'cmake .. -DPY_VERSION=3.7 -DWITH_GPU=ON -DWITH_DISTRIBUTE=OFF -DWITH_TESTING=OFF -DWITH_INFERENCE_API_TEST=OFF -DON_INFER=OFF -DCMAKE_BUILD_TYPE=Release'
     build_command = 'make -j12'
     os.system(cmake_command)
-    os.system(build_command)
-    print('commit {} build done'.format(commit_id))
+    build_res = os.system(build_command)
+    if build_res == 0:
+        print('commit {} build done'.format(commit_id))
+    else:
+        print('commit {} build failed'.format(commit_id))
+        return -1
     install_command = 'pip install -U python/dist/paddlepaddle_gpu-0.0.0-cp37-cp37m-linux_x86_64.whl'
     os.system(install_command)
     print('commit {} install done'.format(commit_id))
+    return 0
 
 
 def check_success(commit_id):
-    os.chdir(paddle_path)
-    compile(commit_id)
+    # os.chdir(paddle_path)
+    # compile(commit_id)
     os.chdir(base_path)
     print('base_path:{}'.format(base_path))
     if not args.is_perf:
@@ -93,13 +100,16 @@ def check_success(commit_id):
         f.writelines(log_result[1])
         f.close()
         with open('log_result.txt', 'r') as f:
-            sub_str = f.split(':')
-            result = float(sub_str[1].strip('\n'))
-            standard_value = args.standard_value
-            if abs((result - standard_value) / standard_value) <= args.threshold:
-                return True
-            else:
-                return False
+            for line in f:
+                sub_str = line.split(':')
+                result = float(sub_str[1].strip('\n'))
+                print('result is {}'.format(result))
+                standard_value = args.standard_value
+                print('standard_value is {}'.format(standard_value))
+                if abs((result - standard_value) / standard_value) <= args.threshold:
+                    return True
+                else:
+                    return False
 
 
 def get_commits(start, end):
@@ -123,18 +133,19 @@ def binary_search(commits):
         print('only two candidate commits left in binary_search, the final commit is {}'.format(commits[0]))
         return commits[0]
     left, right = 0, len(commits) - 1
-    while left <= right:
+    if left <= right:
         mid = left + (right - left) // 2
         commit = commits[mid]
         if check_success(commit):
             print('the commit {} is success'.format(commit))
-            right = mid
-            selected_commits = commits[:, mid + 1]
+            # right = mid
+            print('mid value:{}'.format(mid))
+            selected_commits = commits[:mid + 1]
             binary_search(selected_commits)
         else:
             print('the commit {} is failed'.format(commit))
-            left = mid
-            selected_commits = commits[mid, :]
+            # left = mid
+            selected_commits = commits[mid:]
             binary_search(selected_commits)
 
 
@@ -149,3 +160,6 @@ if __name__ == '__main__':
     print('the candidate commits is {}'.format(commits))
     final_commit = binary_search(commits)
     print('the pr with problem is {}'.format(final_commit))
+    f = open('final_commit.txt', 'w')
+    f.writelines('the final commit is:{}'.format(final_commit))
+    f.close()
