6342ca9df5094d3f658d1adc60b887fa0c124fb8
wangchaochaohu <wangchao66@baidu.com>
2020-09-21 12:35:21 +0000

    refine

--- api/tests_v2/linspace.py
+++ api/tests_v2/linspace.py
@@ -15,38 +15,21 @@
 from common_import import *
 
 
-class LinspaceConfig(APIConfig):
-    def __init__(self):
-        super(LinspaceConfig, self).__init__("linspace")
-        self.feed_spec = [
-                {"range": [0, 100] }, #start
-                {"range": [200, 300] }  #stop
-                ]
-
-
 class PDLinspace(PaddleAPIBenchmarkBase):
     def build_program(self, config):
-        start = self.variable(
-            name="start", shape=[1], dtype=config.start_dtype)
-        stop = self.variable(
-            name="stop", shape=[1], dtype=config.stop_dtype)
-        result = paddle.linspace(start=start, stop=stop, num=config.num, dtype=config.dtype)
+        result = paddle.linspace(start=config.start, stop=config.stop, num=config.num, dtype=config.dtype)
 
-        self.feed_vars = [start, stop]
+        self.feed_vars = []
         self.fetch_vars = [result]
 
 
 class TFLinspace(TensorflowAPIBenchmarkBase):
     def build_graph(self, config):
-        start = self.variable(
-            name="start", shape=[1], dtype=config.start_dtype)
-        stop = self.variable(
-            name="stop", shape=[1], dtype=config.stop_dtype)
-        result = tf.linspace(start=start, stop=stop, num=config.num)
+        result = tf.linspace(start=config.start, stop=config.stop, num=config.num)
 
-        self.feed_list = [start, stop]
+        self.feed_list = []
         self.fetch_list = [result]
 
 
 if __name__ == '__main__':
-    test_main(PDLinspace(), TFLinspace(), config=LinspaceConfig())
+    test_main(PDLinspace(), TFLinspace(), config=APIConfig('linspace'))
