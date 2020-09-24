506343cba37e058583e52df4084652fcdb0442c6
shippingwang <shipeng1108@163.com>
2020-09-24 03:30:34 +0000

    fix

--- api/tests_v2/pixel_shuffle.py
+++ api/tests_v2/pixel_shuffle.py
@@ -18,7 +18,10 @@ from common_import import *
 class PDPixelShuffle(PaddleAPIBenchmarkBase):
     def build_program(self, config):
         x = self.variable(name='x', shape=config.x_shape, dtype=config.x_dtype)
-        out = paddle.nn.functional.pixel_shuffle(x=x, upscale_factor=self.upscale_factor, data_format=self.data_format)
+        out = paddle.nn.functional.pixel_shuffle(
+            x=x,
+            upscale_factor=self.upscale_factor,
+            data_format=self.data_format)
 
         self.feed_vars = [x]
         self.fetch_vars = [out]
@@ -29,7 +32,10 @@ class PDPixelShuffle(PaddleAPIBenchmarkBase):
 class TFPixelShuffle(TensorflowAPIBenchmarkBase):
     def build_graph(self, config):
         x = self.variable(name='x', shape=config.x_shape, dtype=config.x_dtype)
-        out = tf.nn.depth_to_space(input=x, block_size=self.upscale_factor, data_format=self.data_format)
+        out = tf.nn.depth_to_space(
+            input=x,
+            block_size=self.upscale_factor,
+            data_format=self.data_format)
 
         self.feed_list = [x]
         self.fetch_list = [out]
@@ -38,4 +44,5 @@ class TFPixelShuffle(TensorflowAPIBenchmarkBase):
 
 
 if __name__ == '__main__':
-    test_main(PDPixelShuffle(), TFPixelShuffle(), config=APIConfig("pixel_shuffle"))
+    test_main(
+        PDPixelShuffle(), TFPixelShuffle(), config=APIConfig("pixel_shuffle"))
