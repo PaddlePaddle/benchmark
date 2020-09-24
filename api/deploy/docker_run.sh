5bee76d8151539e89be91f9886b5e330e635d879
xiegegege <46314656+xiegegege@users.noreply.github.com>
2020-09-24 15:06:19 +0800

    adjust website link (#622)

--- api/deploy/docker_run.sh
+++ api/deploy/docker_run.sh
@@ -226,8 +226,6 @@ function run(){
             -v /usr/bin/monquery:/usr/bin/monquery \
             -e "BENCHMARK_WEBSITE1=${BENCHMARK_WEBSITE1}" \
             -e "BENCHMARK_WEBSITE2=${BENCHMARK_WEBSITE2}" \
-            -e "BENCHMARK_WEBSITE3=${BENCHMARK_WEBSITE3}" \
-            -e "BENCHMARK_WEBSITE4=${BENCHMARK_WEBSITE4}" \
             -e "http_proxy=${HTTP_PROXY}" \
             -e "https_proxy=${HTTP_PROXY}" \
             -e "PADDLE_VERSION=${PADDLE_VERSION}" \
