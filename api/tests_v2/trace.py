f26f7cf4993007ed796e83228056dab328f2837a
yaoxuefeng6 <yaoxuefeng@baidu.com>
2020-09-27 13:00:24 +0800

    add roll trace flatten

---
+++ api/tests_v2/trace.py
@@ -0,0 +1,35 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+class PDTrace(PaddleAPIBenchmarkBase):
+    def build_program(self, config):
+        data = self.variable(
+            name='data', shape=config.x_shape, dtype=config.x_dtype)
+        result = paddle.trace(x=data, offset=config.offset, axis1=config.axis1, axis2=config.axis2)
+
+        self.feed_vars = [data]
+        self.fetch_vars = [result]
+
+
+class TFTrace(TensorflowAPIBenchmarkBase):
+    def build_graph(self, config):
+        data = self.variable(
+            name='data', shape=config.x_shape, dtype=config.x_dtype)
+        result = tf.linalg.trace(x=data)
+
+        self.feed_list = [data]
+        self.fetch_list = [result]
