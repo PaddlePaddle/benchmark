545cf73c2ee09c0292e6b8182d1a0fa26ee9bca1
GaoWei8 <53294385+GaoWei8@users.noreply.github.com>
2020-10-09 18:01:48 +0800

    Move 1.8 tests to 2.0 tests  (#654)

---
+++ api/tests_v2/reduce.py
@@ -0,0 +1,60 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+class ReduceConfig(APIConfig):
+    def __init__(self):
+        super(ReduceConfig, self).__init__('reduce')
+        self.feed_spec = {"range": [-1, 1]}
+        self.api_name = 'reduce_mean'
+        self.api_list = {
+            'mean': 'reduce_mean',
+            'sum': 'reduce_sum',
+            'prod': 'reduce_prod'
+        }
+
+
+class PDReduce(PaddleAPIBenchmarkBase):
+    def build_program(self, config):
+        data = self.variable(
+            name='x', shape=config.x_shape, dtype=config.x_dtype)
+        result = self.layers(
+            config.api_name, x=data, axis=config.axis, keepdim=config.keepdim)
+
+        self.feed_vars = [data]
+        self.fetch_vars = [result]
+        if config.backward:
+            self.append_gradients(result, [data])
+
+
+class TFReduce(TensorflowAPIBenchmarkBase):
+    def build_graph(self, config):
+        data = self.variable(
+            name='x', shape=config.x_shape, dtype=config.x_dtype)
+        result = self.layers(
+            config.api_name,
+            input_tensor=data,
+            axis=config.axis,
+            keepdims=config.keepdim)
+
+        self.feed_list = [data]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [data])
+
+
+if __name__ == '__main__':
+    test_main(PDReduce(), TFReduce(), config=ReduceConfig())
