734b059a7bb147fd950e2284448375347d84b866
贾晓 <jiaxiao243@126.com>
2020-10-10 16:53:42 +0800

    update scripts (#661)

--- api/deploy/docker_run.sh
+++ api/deploy/docker_run.sh
@@ -211,8 +211,8 @@ function run(){
              pip uninstall paddlepaddle-gpu -y;
              pip install ${all_path}/images/${IMAGE_NAME};
              cd ${benchmark_work_path}/baidu/paddle/benchmark/api;
-             bash deploy/main_control.sh tests/configs ${logs_dir} ${CUDA_VISIBLE_DEVICES} gpu;
-             bash deploy/main_control.sh tests/configs ${logs_dir} ${CPU_VISIBLE_DEVICES} cpu;
+             bash deploy/main_control.sh tests tests/configs ${logs_dir} ${CUDA_VISIBLE_DEVICES} gpu;
+             bash deploy/main_control.sh tests tests/configs ${logs_dir} ${CPU_VISIBLE_DEVICES} cpu;
              unset http_proxy https_proxy;
              python deploy/post_log.py --server_path ${LOG_SERVER} --file_path ${logs_dir}
              ln -s ${all_path}/env/bin/python /usr/local/bin/mypython;
