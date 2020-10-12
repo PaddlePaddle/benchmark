fde4c50107f0a6d9d9e9ce5779cd11ce9fce7d28
Yiqun Liu <liuyiqun01@baidu.com>
2020-10-12 13:19:00 +0800

    Polish the deploying script (#664)
    * Update the script file to collect the testing information for v2.
    
    * Update the testing shell scripts.
    
    * Remove main and launch from special_module_list.
    
    * Exhance the summary to print the compare ratio to excel.

--- api/run_op_benchmark.sh
+++ api/run_op_benchmark.sh
@@ -2,17 +2,27 @@
 
 OP_BENCHMARK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
-gpu_ids=${1:-"0"}
+test_module_name=${1:-"tests"}
+gpu_ids=${2:-"0"}
 
 timestamp=`date '+%Y%m%d-%H%M%S'`
-output_dir=${OP_BENCHMARK_ROOT}/logs/${timestamp}
-if [ ! -d ${OP_BENCHMARK_ROOT}/logs ]; then
-    mkdir -p ${OP_BENCHMARK_ROOT}/logs
+if [ ${test_module_name} = "tests" ]; then
+    log_dir_name="logs"
+elif [ ${test_module_name} = "tests_v2" ]; then
+    log_dir_name="logs_v2"
+else
+    echo "Please set test_module_name to \"tests\" or \"tests_v2\""
+    exit
+fi
+output_dir=${OP_BENCHMARK_ROOT}/${log_dir_name}/${timestamp}
+if [ ! -d ${OP_BENCHMARK_ROOT}/${log_dir_name} ]; then
+    mkdir -p ${OP_BENCHMARK_ROOT}/${log_dir_name}
 fi
 if [ ! -d ${output_dir} ]; then
     mkdir -p ${output_dir}
 fi
 
-config_dir=${OP_BENCHMARK_ROOT}/tests/configs
-log_path=${OP_BENCHMARK_ROOT}/logs/log_${timestamp}.txt
-bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${config_dir} ${output_dir} ${gpu_ids} "both" "both" > ${log_path} 2>&1 &
+tests_dir=${OP_BENCHMARK_ROOT}/${test_module_name}
+config_dir=${OP_BENCHMARK_ROOT}/${test_module_name}/configs
+log_path=${OP_BENCHMARK_ROOT}/${log_dir_name}/log_${timestamp}.txt
+bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${tests_dir} ${config_dir} ${output_dir} ${gpu_ids} "both" "both" > ${log_path} 2>&1 &
