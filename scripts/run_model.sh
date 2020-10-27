09130ff3727a774f3c7a8ef7144d22bc4cce13d2
heya02 <heya02@baidu.com>
2020-10-27 15:34:32 +0000

    add model commit, test=document_fix

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -8,8 +8,8 @@ function _collect_occupancy() {
         hostname=`echo $(hostname)|awk -F '.baidu.com' '{print $1}'`
         gpu_log_file=${log_file}_gpu
         cpu_log_file=${log_file}_cpu
-        monquery -n ${hostname} -i GPU_AVERAGE_UTILIZATION -s $job_bt -e $job_et -d 20 > ${gpu_log_file}
-        monquery -n ${hostname} -i CPU_USER -s $job_bt -e $job_et -d 20 > ${cpu_log_file}
+        monquery -n ${hostname} -i GPU_AVERAGE_UTILIZATION -s $job_bt -e $job_et -d 60 > ${gpu_log_file}
+        monquery -n ${hostname} -i CPU_USER -s $job_bt -e $job_et -d 60 > ${cpu_log_file}
         cpu_num=$(cat /proc/cpuinfo | grep processor | wc -l)
         gpu_num=$(nvidia-smi -L|wc -l)
 
@@ -49,7 +49,7 @@ function _run(){
         echo "Do nothing"
     fi
 
-    python3 ${BENCHMARK_ROOT}/scripts/analysis.py \
+    python ${BENCHMARK_ROOT}/scripts/analysis.py \
             --filename "${log_file}" \
             --log_with_profiler ${log_with_profiler:-"not found!"} \
             --profiler_path ${profiler_path:-"not found!"} \
