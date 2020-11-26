f332101063a2141402dbaf30c814e91f551449f7
heya02 <heya02@baidu.com>
2020-11-26 06:25:26 +0000

    swicth_clas, test=document_fix

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cur_model_list=(dy_wavenet dy_senta dy_yolov3 dy_mask_rcnn dy_slowfast dy_tsn dy_tsm dy_gan dy_seg dy_seq2seq dy_resnet dy_ptb_lm dy_transformer dy_mobilenet dy_resnet152)
+cur_model_list=(dy_wavenet dy_senta dy_yolov3 dy_mask_rcnn dy_slowfast dy_tsn dy_tsm dy_gan dy_seg dy_seq2seq dy_resnet dy_ptb_lm dy_transformer dy_mobilenet)
 
 
 # MobileNet
@@ -59,34 +59,32 @@ dy_seq2seq(){
 }
 
 # resnet
-dy_resnet(){
-    cur_model_path=${BENCHMARK_ROOT}/models/dygraph/resnet
-    cd ${cur_model_path}
-
-    # Prepare data
-    mkdir -p data
-    ln -s ${data_path}/ILSVRC2012  ${cur_model_path}/data                         # 准备数据集,需要保证benchmark任务极其21 上对应目录下存在该数据集！
-
-    # Running ...
-    rm -f ./run_benchmark.sh
-    cp ${BENCHMARK_ROOT}/dynamic_graph/resnet/paddle/run_benchmark.sh ./
-    sed -i '/set\ -xe/d' run_benchmark.sh
-    model_list=(ResNet50_bs32 ResNet50_bs128)
-    run_batchsize=32
-    for model_item in ${model_list[@]}
-    do
-        if [ ${model_item} = "ResNet50_bs128" ]; then
-            run_batchsize=128
-        fi
-        echo "index is speed, 1gpu begin"
-        CUDA_VISIBLE_DEVICES=5 bash run_benchmark.sh 1 ${run_batchsize} ${model_item} sp 800 | tee ${log_path}/dynamic_${model_item}_speed_1gpus 2>&1
-        sleep 60
-        echo "index is speed, 8gpus begin"
-        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 ${run_batchsize} ${model_item} mp 500 | tee ${log_path}/dynamic_${model_item}_speed_8gpus 2>&1
-    done
-    ######
-    
-}
+#dy_resnet(){
+#    cur_model_path=${BENCHMARK_ROOT}/models/dygraph/resnet
+#    cd ${cur_model_path}
+#
+#    # Prepare data
+#    mkdir -p data
+#    ln -s ${data_path}/ILSVRC2012  ${cur_model_path}/data                         # 准备数据集,需要保证benchmark任务极其21 上对应目录下存在该数据集！
+#
+#    # Running ...
+#    rm -f ./run_benchmark.sh
+#    cp ${BENCHMARK_ROOT}/dynamic_graph/resnet/paddle/run_benchmark.sh ./
+#    sed -i '/set\ -xe/d' run_benchmark.sh
+#    model_list=(ResNet50_bs32 ResNet50_bs128)
+#    run_batchsize=32
+#    for model_item in ${model_list[@]}
+#    do
+#        if [ ${model_item} = "ResNet50_bs128" ]; then
+#            run_batchsize=128
+#        fi
+#        echo "index is speed, 1gpu begin"
+#        CUDA_VISIBLE_DEVICES=5 bash run_benchmark.sh 1 ${run_batchsize} ${model_item} sp 800 | tee ${log_path}/dynamic_${model_item}_speed_1gpus 2>&1
+#        sleep 60
+#        echo "index is speed, 8gpus begin"
+#        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 ${run_batchsize} ${model_item} mp 500 | tee ${log_path}/dynamic_${model_item}_speed_8gpus 2>&1
+#    done
+#}
 
 # ptb
 dy_ptb_lm(){
@@ -383,7 +381,7 @@ dy_senta(){
     done
 }
 
-dy_resnet152(){
+dy_resnet(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleClas
     cd ${cur_model_path}
     git checkout dygraph
