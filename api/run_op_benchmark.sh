a416c5dc703859f5df047810665598691e17c597
Yiqun Liu <liuyiqun01@baidu.com>
2020-12-11 17:20:03 +0800

    Polish the script to run and summary all tests and add gelu, transpose. (#788)
    * Enhance unsqueeze_short to avoid unsqueeze [N] to [1, N].
    
    * Make the check of compare_framework earlier.
    
    * Add gelu and transpose.
    
    * Update the script to run all tests.
    
    * Add configs of bert-base.
    
    * Add script of cross_entropy, but it cannot run correctly now.
    
    * Add ones_like.

--- api/run_op_benchmark.sh
+++ api/run_op_benchmark.sh
@@ -2,27 +2,37 @@
 
 OP_BENCHMARK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
-test_module_name=${1:-"tests"}
+test_module_name=${1:-"tests"}  # "tests", "tests_v2", "dynamic_tests_v2"
 gpu_ids=${2:-"0"}
 
-timestamp=`date '+%Y%m%d-%H%M%S'`
-if [ ${test_module_name} = "tests" ]; then
-    log_dir_name="logs"
-elif [ ${test_module_name} = "tests_v2" ]; then
-    log_dir_name="logs_v2"
-else
-    echo "Please set test_module_name to \"tests\" or \"tests_v2\""
+if [ ${test_module_name} != "tests" ] && [ ${test_module_name} != "tests_v2" ] && [ ${test_module_name} != "dynamic_tests_v2" ]; then
+    echo "Please set test_module_name to \"tests\", \"tests_v2\" or \"dynamic_tests_v2\"!"
     exit
 fi
-output_dir=${OP_BENCHMARK_ROOT}/${log_dir_name}/${timestamp}
-if [ ! -d ${OP_BENCHMARK_ROOT}/${log_dir_name} ]; then
-    mkdir -p ${OP_BENCHMARK_ROOT}/${log_dir_name}
+
+OUTPUT_ROOT=${OP_BENCHMARK_ROOT}/logs
+if [ ! -d ${OUTPUT_ROOT} ]; then
+    mkdir -p ${OUTPUT_ROOT}
 fi
+
+timestamp=`date '+%Y%m%d-%H%M%S'`
+output_dir=${OUTPUT_ROOT}/${test_module_name}/${timestamp}
 if [ ! -d ${output_dir} ]; then
     mkdir -p ${output_dir}
 fi
 
+if [ ${test_module_name} = "tests" ]; then
+    config_dir=${OP_BENCHMARK_ROOT}/tests/configs
+else
+    config_dir=${OP_BENCHMARK_ROOT}/tests_v2/configs
+fi
+
+if [ "${test_module_name}" == "dynamic_tests_v2" ]; then
+    testing_mode="dynamic"
+else
+    testing_mode="static"
+fi
+
 tests_dir=${OP_BENCHMARK_ROOT}/${test_module_name}
-config_dir=${OP_BENCHMARK_ROOT}/${test_module_name}/configs
-log_path=${OP_BENCHMARK_ROOT}/${log_dir_name}/log_${timestamp}.txt
-bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${tests_dir} ${config_dir} ${output_dir} ${gpu_ids} "both" "both" > ${log_path} 2>&1 &
+log_path=${OUTPUT_ROOT}/log_${test_module_name}_${timestamp}.txt
+bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${tests_dir} ${config_dir} ${output_dir} ${gpu_ids} "both" "both" "none" "both" "${testing_mode}" > ${log_path} 2>&1 &
