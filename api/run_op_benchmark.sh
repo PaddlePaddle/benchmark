1bc8b5ab716dcaac5610588ac85e12e7f3954458
Liu Yiqun <liuyiqun01@baidu.com>
2021-01-13 03:47:17 +0000

    Enable to benchmarking to testing mode one time.

--- api/run_op_benchmark.sh
+++ api/run_op_benchmark.sh
@@ -2,8 +2,13 @@
 
 OP_BENCHMARK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
-test_module_name=${1:-"tests"}  # "tests", "tests_v2", "dynamic_tests_v2"
-gpu_ids=${2:-"0"}
+OUTPUT_ROOT=${OP_BENCHMARK_ROOT}/logs
+if [ ! -d ${OUTPUT_ROOT} ]; then
+    mkdir -p ${OUTPUT_ROOT}
+fi
+
+test_module_names=${1:-"dynamic_tests_v2,tests_v2"}  # "tests", "tests_v2", "dynamic_tests_v2"
+gpu_ids=${2:-"2,3"}
 
 install_package() {
     local package_name=$1
@@ -25,36 +30,38 @@ install_package() {
     fi
 }
 
-if [ ${test_module_name} != "tests" ] && [ ${test_module_name} != "tests_v2" ] && [ ${test_module_name} != "dynamic_tests_v2" ]; then
-    echo "Please set test_module_name to \"tests\", \"tests_v2\" or \"dynamic_tests_v2\"!"
-    exit
-fi
-
-OUTPUT_ROOT=${OP_BENCHMARK_ROOT}/logs
-if [ ! -d ${OUTPUT_ROOT} ]; then
-    mkdir -p ${OUTPUT_ROOT}
-fi
-
 timestamp=`date '+%Y%m%d-%H%M%S'`
-output_dir=${OUTPUT_ROOT}/${test_module_name}/${timestamp}
-if [ ! -d ${output_dir} ]; then
-    mkdir -p ${output_dir}
-fi
 
-if [ ${test_module_name} = "tests" ]; then
-    config_dir=${OP_BENCHMARK_ROOT}/tests/configs
-else
-    config_dir=${OP_BENCHMARK_ROOT}/tests_v2/configs
-fi
+test_module_array=(${test_module_names//,/ })
+num_test_modules=${#test_module_array[*]}
+for test_module_name in ${test_module_array[*]}; do
+    echo "-- Benchmarking module: ${test_module_name}"
 
-if [ "${test_module_name}" == "dynamic_tests_v2" ]; then
-    testing_mode="dynamic"
-    install_package "torch" "1.7.1"
-else
-    testing_mode="static"
-    install_package "tensorflow" "2.3.1"
-fi
-
-tests_dir=${OP_BENCHMARK_ROOT}/${test_module_name}
-log_path=${OUTPUT_ROOT}/log_${test_module_name}_${timestamp}.txt
-bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${tests_dir} ${config_dir} ${output_dir} ${gpu_ids} "both" "both" "none" "both" "${testing_mode}" > ${log_path} 2>&1 &
+    if [ ${test_module_name} != "tests" ] && [ ${test_module_name} != "tests_v2" ] && [ ${test_module_name} != "dynamic_tests_v2" ]; then
+        echo "Please set test_module_name (${test_module_name}) to \"tests\", \"tests_v2\" or \"dynamic_tests_v2\"!"
+        exit
+    fi
+    
+    output_dir=${OUTPUT_ROOT}/${test_module_name}/${timestamp}
+    if [ ! -d ${output_dir} ]; then
+        mkdir -p ${output_dir}
+    fi
+    
+    if [ ${test_module_name} = "tests" ]; then
+        config_dir=${OP_BENCHMARK_ROOT}/tests/configs
+    else
+        config_dir=${OP_BENCHMARK_ROOT}/tests_v2/configs
+    fi
+    
+    if [ "${test_module_name}" == "dynamic_tests_v2" ]; then
+        testing_mode="dynamic"
+        install_package "torch" "1.7.1"
+    else
+        testing_mode="static"
+        install_package "tensorflow" "2.3.1"
+    fi
+    
+    tests_dir=${OP_BENCHMARK_ROOT}/${test_module_name}
+    log_path=${OUTPUT_ROOT}/log_${test_module_name}_${timestamp}.txt
+    bash ${OP_BENCHMARK_ROOT}/deploy/main_control.sh ${tests_dir} ${config_dir} ${output_dir} ${gpu_ids} "both" "both" "none" "both" "${testing_mode}" > ${log_path} 2>&1
+done
