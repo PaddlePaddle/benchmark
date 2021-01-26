fd90a67ff3475fa556ae936e99c0b15c07cea03e
heya02 <heya02@baidu.com>
2021-01-26 07:52:50 +0000

    update_fp16_scripts, test=document_fix

--- static_graph/image_classification/resnet50_fp/paddle/run_benchmark_fp16.sh
+++ static_graph/image_classification/resnet50_fp/paddle/run_benchmark_fp16.sh
@@ -76,7 +76,7 @@ function _train(){
     sp) train_cmd="python -u tools/static/train.py -o is_distributed=False "${train_cmd} ;;
     mp)
         rm -rf ./mylog
-        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=$CUDA_VISIBLE_DEVICES tools/static/train.py "${train_cmd}
+        train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=$CUDA_VISIBLE_DEVICES tools/static/train.py -o is_distributed=True "${train_cmd}
         log_parse_file="mylog/workerlog.0" ;;
     *) echo "choose run_mode(sp or mp)"; exit 1;
     esac
