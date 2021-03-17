96f6fe8732893c69c9d8bb2cdfaee24e4a7463ea
JiaXiao243 <jiaxiao243@126.com>
2021-03-17 14:22:20 +0000

    update  histogram

--- api/dynamic_tests_v2/histogram.py
+++ api/dynamic_tests_v2/histogram.py
@@ -15,6 +15,17 @@
 from common_import import *
 
 
+class HistogramConfig(APIConfig):
+    def __init__(self):
+        super(HistogramConfig, self).__init__("histogram")
+
+    def init_from_json(self, filename, config_id=0, unknown_dim=16):
+        super(HistogramConfig, self).init_from_json(filename, config_id,
+                                                    unknown_dim)
+        if self.input_dtype == "int32":
+            self.input_dtype = "float32"
+
+
 class PDHistogram(PaddleDynamicAPIBenchmarkBase):
     def build_graph(self, config):
         x = self.variable(
@@ -30,7 +41,7 @@ class TorchHistogram(PytorchAPIBenchmarkBase):
     def build_graph(self, config):
         x = self.variable(
             name='x', shape=config.input_shape, dtype=config.input_dtype)
-        result = torch.histc(input=x.float(), bins=100, min=0, max=0)
+        result = torch.histc(input=x, bins=100, min=0, max=0)
 
         self.feed_list = [x]
         self.fetch_list = [result]
@@ -40,4 +51,4 @@ if __name__ == '__main__':
     test_main(
         pd_dy_obj=PDHistogram(),
         torch_obj=TorchHistogram(),
-        config=APIConfig("histogram"))
+        config=HistogramConfig())
