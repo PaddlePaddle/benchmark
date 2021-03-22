383de814184958cfd5dc2f730937223f74e03fe2
xiegegege <46314656+xiegegege@users.noreply.github.com>
2021-03-22 15:03:22 +0800

    double check running result (#952)
    * double check running result
    
    * double check running result, test=document_fix

--- scripts/benchmark_ci/run_models.sh
+++ scripts/benchmark_ci/run_models.sh
@@ -1,30 +1,43 @@
 #run resnet
-resnet(){
+ResNet50_bs32_dygraph(){
+    cur_model_path=${BENCHMARK_ROOT}/PaddleClas
+    cd ${cur_model_path}
+    # Prepare data
+    ln -s ${data_path}/imagenet100_data/ ${cur_model_path}/dataset
+    # Copy run_benchmark.sh and running ...
+    rm -rf ./run_benchmark_dygraph.sh
+    cp ${BENCHMARK_ROOT}/dynamic_graph/resnet/paddle/run_benchmark_resnet.sh ./run_benchmark_dygraph.sh
+    sed -i '/set\ -xe/d' run_benchmark_dygraph.sh
+
+    #running models cases
+    model_name=ResNet50_bs32_dygraph
+    run_batchsize=32
+    echo "index is speed, 1gpu, begin, ResNet50_bs32_dygraph"
+    CUDA_VISIBLE_DEVICES=0 bash run_benchmark_dygraph.sh 1 ${run_batchsize} ${model_name} sp 1 | tee ${BENCHMARK_ROOT}/logs/dynamic/${model_name}_speed_1gpus 2>&1
+    cat dynamic_${model_name}_1_1_sp
+    sleep 60
+}
+
+ResNet50_bs32(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleClas
     cd ${cur_model_path}
     # Prepare data
     ln -s ${data_path}/imagenet100_data/ ${cur_model_path}/dataset
     # Copy run_benchmark.sh and running ...
     rm -rf ./run_benchmark_static.sh
-    rm -rf ./run_benchmark_dygraph.sh
     cp ${BENCHMARK_ROOT}/static_graph/image_classification/paddle/run_benchmark_resnet.sh ./run_benchmark_static.sh
-    cp ${BENCHMARK_ROOT}/dynamic_graph/resnet/paddle/run_benchmark_resnet.sh ./run_benchmark_dygraph.sh
     sed -i '/set\ -xe/d' run_benchmark_static.sh
-    sed -i '/set\ -xe/d' run_benchmark_dygraph.sh
-
     #running models cases
     model_name=ResNet50_bs32
     run_batchsize=32
     echo "index is speed, 1gpu, begin, ResNet50_bs32_static"
     CUDA_VISIBLE_DEVICES=0 bash run_benchmark_static.sh 1 ${run_batchsize} ${model_name} sp 1 | tee ${BENCHMARK_ROOT}/logs/static/${model_name}_speed_1gpus 2>&1
-    sleep 60
-    echo "index is speed, 1gpu, begin, ResNet50_bs32_dygraph"
-    CUDA_VISIBLE_DEVICES=0 bash run_benchmark_dygraph.sh 1 ${run_batchsize} ${model_name} sp 1 | tee ${BENCHMARK_ROOT}/logs/dynamic/${model_name}_speed_1gpus 2>&1
+    cat ${model_name}_1_1_sp
     sleep 60
 }
 
 #run bert_base_fp32
-bert(){
+bert_base_seqlen128_fp32_bs32(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleNLP/benchmark/bert
     cd ${cur_model_path}
     ln -s ${data_path}/Bert/wikicorpus_en_seqlen128 ${cur_model_path}/wikicorpus_en_seqlen128
@@ -36,6 +49,7 @@ bert(){
     model_name=bert_base_seqlen128_fp32_bs32
     echo "index is speed, 1gpu, begin, bert_base_fp32"
     CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh 1 base fp32 sp 500 | tee ${BENCHMARK_ROOT}/logs/static/${model_name}_speed_1gpus 2>&1
+    cat ${model_name}_1_1_sp
     sleep 60    
 }
 
@@ -56,6 +70,7 @@ mobilenetV1(){
     CUDA_VISIBLE_DEVICES=0 bash run_benchmark_mobilenet.sh 1  sp 1  ${model_name} | tee ${BENCHMARK_ROOT}/logs/dynamic/${model_name}_speed_1gpus 2>&1
     sleep 60
 }
-resnet
-bert
+#ResNet50_bs32_dygraph
+#ResNet50_bs32
+#bert_base_seqlen128_fp32_bs32
 #mobilenetV1
