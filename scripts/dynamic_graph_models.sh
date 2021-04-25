81a5479482b13fe0c91e3cd2d8cec1ff5fbb935c
xiegegege <xiege01@baidu.com>
2021-04-25 17:18:13 +0800

    fix data path for dy_transformer

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -109,8 +109,8 @@ dy_transformer(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleNLP/benchmark/transformer/dygraph
     cd ${cur_model_path}
     # prepare data
-    mkdir -p ~/.paddlenlp/datasets/machine_translation
-    ln -s ${data_path}/dygraph_data/transformer/WMT14ende ~/.paddlenlp/datasets/machine_translation/ 
+    mkdir -p ~/.paddlenlp/datasets
+    ln -s ${data_path}/dygraph_data/transformer/WMT14ende ~/.paddlenlp/datasets/ 
     rm -f ./run_benchmark.sh
     cp ${BENCHMARK_ROOT}/dynamic_graph/transformer/paddle/run_benchmark.sh ./
     sed -i '/set\ -xe/d' run_benchmark.sh
