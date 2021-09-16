df96237caf230de00879c6364c9dcaead89aea02
mmglove <gmm_email@163.com>
2021-09-16 14:16:04 +0800

    update_class_repo, static, from 98db91b to 85e2407e466a04abb38c69ae5b6300974dc7d917,test=document_fix

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -66,7 +66,7 @@ dy_bert(){
 dy_mobilenet(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleClas
     cd ${cur_model_path}
-    git checkout -b develop_mobilenet 98db91b2118deb0f6f1c0bf90708c1bc34687f8d
+    git checkout -b develop_resnet 98db91b2118deb0f6f1c0bf90708c1bc34687f8d
     pip install -r requirements.txt
 
     # Prepare data
@@ -434,7 +434,7 @@ dy_senta(){
 dy_resnet(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleClas
     cd ${cur_model_path}
-    git checkout -b develop_resnet 17a06dafdb92515d6e3f86b7e532979b46b195b5
+    git checkout -b develop_resnet 98db91b2118deb0f6f1c0bf90708c1bc34687f8d
     pip install -r requirements.txt
    
     ln -s ${data_path}/dygraph_data/imagenet100_data/ ${cur_model_path}/dataset
--- scripts/static_graph_models.sh
+++ scripts/static_graph_models.sh
@@ -48,7 +48,7 @@ seg_model(){
 image_classification(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleClas
     cd ${cur_model_path}
-    git checkout -b static_17a 17a06dafdb92515d6e3f86b7e532979b46b195b5
+#    git checkout -b static_17a 17a06dafdb92515d6e3f86b7e532979b46b195b5
     pip install -r requirements.txt
     # Prepare data
     ln -s ${data_path}/dygraph_data/imagenet100_data/ ${cur_model_path}/dataset/
