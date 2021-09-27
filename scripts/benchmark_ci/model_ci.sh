502bdd01864621ff9b9271607abd0673488eca25
xiegegege <46314656+xiegegege@users.noreply.github.com>
2021-09-27 21:06:28 +0800

    fix bug in model_benchmark_ci (#1098)
    * fix bug in model_benchmark_ci,test=document_fix
    
    * fix bug in model_benchmark_ci,test=document_fix

--- scripts/benchmark_ci/model_ci.sh
+++ scripts/benchmark_ci/model_ci.sh
@@ -13,7 +13,7 @@ pip install paddlenlp
 #run models
 cd ${BENCHMARK_ROOT}/scripts/benchmark_ci
 #model_list='ResNet50_bs32_dygraph ResNet50_bs32 bert_base_seqlen128_fp32_bs32 transformer_base_bs4096_amp_fp16 yolov3_bs8 TSM_bs16 deeplabv3_bs4 CycleGAN_bs1 mask_rcnn_bs1 PPOCR_mobile_2_bs8 seq2seq_bs128'
-#test models
+#test 
 model_list='ResNet50_bs32_dygraph ResNet50_bs32 deeplabv3_bs4 yolov3_bs8 bert_base_seqlen128_fp32_bs32 CycleGAN_bs1'
 source run_models.sh
 for model in ${model_list}
@@ -29,8 +29,8 @@ fi
 if [ -f "errorcode.txt" ];then rm -rf errorcode.txt
 fi
 echo success >>log.txt
-python analysis.py --log_path=${BENCHMARK_ROOT}/logs/static --standard_path=${BENCHMARK_ROOT}/scripts/benchmark_ci/standard_value/static --threshold=0.05 --loss_threshold=0.3 --paddle_dev=False
-python analysis.py --log_path=${BENCHMARK_ROOT}/logs/dynamic --standard_path=${BENCHMARK_ROOT}/scripts/benchmark_ci/standard_value/dynamic --threshold=0.05 --loss_threshold=0.3 --paddle_dev=False
+python analysis.py --log_path=${BENCHMARK_ROOT}/logs/static --standard_path=${BENCHMARK_ROOT}/scripts/benchmark_ci/standard_value/static --threshold=0.05 --loss_threshold=0.5 --paddle_dev=False
+python analysis.py --log_path=${BENCHMARK_ROOT}/logs/dynamic --standard_path=${BENCHMARK_ROOT}/scripts/benchmark_ci/standard_value/dynamic --threshold=0.05 --loss_threshold=0.5 --paddle_dev=False
 #if the fluctuations is larger than threshold, then rerun in paddle develop for result judging to avoid fluctuations caused by xiaolvyun machines.
 if [ -f "rerun_model.txt" ];then
     echo -e "rerun model in paddle develop start!"
