7ae879286edb15316e13fa5633082aed091bc571
Hui Zhang <zhtclz@foxmail.com>
2021-09-27 08:45:35 +0000

    speech conformer wenet

---
+++ OtherFrame/Speech/wenet/PrepareEnv.sh
@@ -0,0 +1,38 @@
+#if [ -f Miniconda3-latest-Linux-x86_64.sh];then
+#    wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#    bash Miniconda3-latest-Linux-x86_64.sh
+#fi
+#
+#. /workspace/miniconda3/bin/activate
+
+#conda create -n wenet python=3.8
+#conda activate wenet
+
+
+test -d venv || virtualenv -p python3.8 venv
+source venv/bin/activate
+
+pushd models/wenet
+
+pip install -r requirements.txt
+#conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c conda-forge
+pip install torch==1.9.1+cu102 torchvision==0.10.1+cu102 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+popd
+
+
+cp scripts/executor.py models/wenet/models/wenet/wenet/utils/
+
+
+
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
+#apt-get install software-properties-common -y
+#add-apt-repository ppa:rmescandon/yq
+#apt update
+#apt install yq -y
+
+
+VERSION=v4.2.0
+BINARY=yq_linux_amd64
+wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq &&\
+        chmod +x /usr/bin/yq
---
+++ OtherFrame/Speech/wenet/run_PyTorch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CUR_DIR=${PWD}
+
+# 1 安装该模型需要的依赖 (如需开启优化策略请注明)
+source venv/bin/activate
+
+# 2 拷贝该模型需要数据、预训练模型
+pushd models/wenet/examples/aishell/s0/
+
+mkdir -p exp/log
+. path.sh
+
+bash run.sh --data $PWD --stop_stage 3
+
+# 3 批量运行（如不方便批量，1，2需放到单个模型中）
+
+model_mode_list=(conformer)
+fp_item_list=(fp32)
+bs_item_list=(32 64 96)
+for model_mode in ${model_mode_list[@]}; do
+      for fp_item in ${fp_item_list[@]}; do
+          for bs_item in ${bs_item_list[@]}
+            do
+            echo "index is speed, 1gpus, begin, ${model_name}"
+            run_mode=sp
+            CUDA_VISIBLE_DEVICES=0 bash ${CUR_DIR}/run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}     #  (5min)
+            sleep 60
+            echo "index is speed, 8gpus, run_mode is multi_process, begin, ${model_name}"
+            run_mode=mp
+            CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash ${CUR_DIR}/run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}
+            sleep 60
+            done
+      done
+done
+
+popd # aishell/s1
+
