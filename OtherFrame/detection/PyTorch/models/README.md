1fe405fde1b52d1802d2253f123b737a95b5828c
ghostxsl <451323469@qq.com>
2021-10-08 17:22:44 +0800

    add detection benchmark

---
+++ OtherFrame/detection/PyTorch/models/README.md
@@ -0,0 +1,7 @@
+# Detection PyTorch 性能复现
+- Faster RCNN
+- FCOS
+- Deformable DETR
+- GFL
+
+以上模型复现使用[mmdetection](https://github.com/open-mmlab/mmdetection)
---
+++ OtherFrame/detection/PyTorch/run_PyTorch.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+ImageName="registry.baidubce.com/paddlepaddle/paddle:2.1.2-gpu-cuda10.2-cudnn7";
+docker pull ${ImageName}
+export BENCHMARK_ROOT=/workspace # 对应实际地址 benchmark/OtherFrameworks/video/PyTorch
+
+run_cmd="cd ${BENCHMARK_ROOT}
+        bash PrepareEnv.sh
+        cd ${BENCHMARK_ROOT}/models/mmdetection;
+        cp ${BENCHMARK_ROOT}/scripts/run_benchmark.sh ./;
+        cp ${BENCHMARK_ROOT}/scripts/analysis_log.py ./;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 1 fp32 1 faster_rcnn;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 1 fp32 1 faster_rcnn;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 8 fp32 1 faster_rcnn;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 8 fp32 1 faster_rcnn;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 2 fp32 1 fcos;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 2 fp32 1 fcos;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 8 fp32 1 fcos;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 8 fp32 1 fcos;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 2 fp32 1 deformable_detr;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 2 fp32 1 deformable_detr;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 2 fp32 1 gfl;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 2 fp32 1 gfl;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh sp 8 fp32 1 gfl;
+        sleep 60;
+        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh mp 8 fp32 1 gfl;
+        sleep 60;
+        "
+
+nvidia-docker run --name test_torch_detection -it  \
+    --net=host \
+    --shm-size=64g \
+    -v $PWD:/workspace \
+    ${ImageName}  /bin/bash -c "${run_cmd}"
+
+nvidia-docker stop test_torch_detection
+nvidia-docker rm test_torch_detection
