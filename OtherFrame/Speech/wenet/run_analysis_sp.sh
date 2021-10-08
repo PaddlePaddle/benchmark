df8eba6b420745bfa2efc85ae10cd3cda37c546b
huangyuxin <hyxin2014@126.com>
2021-10-08 11:26:20 +0000

    Add analysis

---
+++ OtherFrame/Speech/wenet/run_analysis_sp.sh
@@ -0,0 +1,14 @@
+cp models/wenet/examples/aishell/s0/conformer_sp_bs16_fp32_1 .
+
+python analysis.py \
+    --filename "conformer_sp_bs16_fp32_1" \
+    --keyword "avg_ips:" \
+    --base_batch_size 16 \
+    --model_name "Conformer" \
+    --mission_name "one gpu" \
+    --run_mode "sp" \
+    --ips_unit "sent./sec" \
+    --gpu_num 1 \
+    --use_num 60 \
+    --separator " " \
+
