df983472c98e7210e45a0cefb940f7ef83144ba1
huangyuxin <hyxin2014@126.com>
2021-10-11 12:28:18 +0000

    add some small changes

--- OtherFrame/Speech/wenet/run_PyTorch.sh
+++ OtherFrame/Speech/wenet/run_PyTorch.sh
@@ -11,21 +11,23 @@ pushd models/wenet/examples/aishell/s0/
 mkdir -p exp/log
 . path.sh
 
-bash run.sh --data $PWD --stop_stage 3
+#bash run.sh --data $PWD --stop_stage 3
 
 # 3 批量运行（如不方便批量，1，2需放到单个模型中）
 
 model_mode_list=(conformer)
 fp_item_list=(fp32)
-bs_item_list=(32 64 96)
+bs_item_list=(30)
 for model_mode in ${model_mode_list[@]}; do
       for fp_item in ${fp_item_list[@]}; do
           for bs_item in ${bs_item_list[@]}
             do
+            rm exp -rf
             echo "index is speed, 1gpus, begin, ${model_name}"
             run_mode=sp
-            CUDA_VISIBLE_DEVICES=0 bash ${CUR_DIR}/run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}     #  (5min)
+            CUDA_VISIBLE_DEVICES=6 bash ${CUR_DIR}/run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}     #  (5min)
             sleep 60
+            rm exp -rf
             echo "index is speed, 8gpus, run_mode is multi_process, begin, ${model_name}"
             run_mode=mp
             CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash ${CUR_DIR}/run_benchmark.sh ${run_mode} ${bs_item} ${fp_item} 500 ${model_mode}
