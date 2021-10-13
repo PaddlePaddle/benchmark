19b9b945fa39dc4a336185845b9969dd9b310694
Double_V <liuvv0203@163.com>
2021-10-13 16:31:24 +0800

    python3.7 to python

--- dynamic_graph/OCR/tensorflow/run_benchmark.sh
+++ dynamic_graph/OCR/tensorflow/run_benchmark.sh
@@ -12,12 +12,12 @@ batch_list=(8 16)
 for batch in ${batch_list[@]}; do
 
     if [ ${mode} = "sp" ]; then
-        export CUDA_VISIBLE_DEVICES=0
-        train_cmd="python3.7 multigpu_train.py --gpu_list="0" --input_size=640 --batch_size_per_gpu=${batch} --checkpoint_path='' --text_scale=512 --training_data_path=./icdar2015/ --geometry=RBOX --learning_rate=0.0001 --num_readers=24 --pretrained_model_path=./resnet_v1_50.ckpt --max_steps=200"
+        #export CUDA_VISIBLE_DEVICES=0
+        train_cmd="python multigpu_train.py --gpu_list="0" --input_size=640 --batch_size_per_gpu=${batch} --checkpoint_path='' --text_scale=512 --training_data_path=./icdar2015/ --geometry=RBOX --learning_rate=0.0001 --num_readers=24 --pretrained_model_path=./resnet_v1_50.ckpt --max_steps=200"
         num_gpu_devices=1
     else
-        export CUDA_VISIBLE_DEVICES=0,1,2,3
-        train_cmd="python3.7 multigpu_train.py --gpu_list="0,1,2,3" --input_size=640 --batch_size_per_gpu=${batch} --checkpoint_path='' --text_scale=512 --training_data_path=./icdar2015/ --geometry=RBOX --learning_rate=0.0001 --num_readers=24 --pretrained_model_path=./resnet_v1_50.ckpt --max_steps=100"
+        #export CUDA_VISIBLE_DEVICES=0,1,2,3
+        train_cmd="python multigpu_train.py --gpu_list="0,1,2,3,4,5,6,7" --input_size=640 --batch_size_per_gpu=${batch} --checkpoint_path='' --text_scale=512 --training_data_path=./icdar2015/ --geometry=RBOX --learning_rate=0.0001 --num_readers=24 --pretrained_model_path=./resnet_v1_50.ckpt --max_steps=100"
         num_gpu_devices=8
     fi
  
@@ -36,12 +36,12 @@ for batch in ${batch_list[@]}; do
         echo -e "${model_name}, SUCCESS"
         export job_fail_flag=0
     fi
-    kill -9 `ps -ef|grep 'python3.7'|awk '{print $2}'`
+    kill -9 `ps -ef|grep 'python'|awk '{print $2}'`
 
     if [ $mode = "mp" -a -d mylog ]; then
         rm ${log_file}
         cp mylog/workerlog.0 ${log_file}
     fi
-    analysis_cmd="python3.7 benchmark/analysis.py --filename ${log_file}  --mission_name ${model_name} --run_mode ${mode} --direction_id 0 --keyword 'examples/second' --base_batch_size ${batch} --skip_steps 1 --gpu_num ${num_gpu_devices} --index 1  --model_mode=-1  --ips_unit='examples/second'  --position=10"
+    analysis_cmd="python benchmark/analysis.py --filename ${log_file}  --mission_name ${model_name} --run_mode ${mode} --direction_id 0 --keyword 'examples/second' --base_batch_size ${batch} --skip_steps 1 --gpu_num ${num_gpu_devices} --index 1  --model_mode=-1  --ips_unit='examples/second'  --position=10"
     eval $analysis_cmd
 done
