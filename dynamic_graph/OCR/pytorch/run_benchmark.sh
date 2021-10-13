6dc8cc744be5647835a189e9c127c418d5f2b6e5
Double_V <liuvv0203@163.com>
2021-10-13 16:29:48 +0800

    python3.7 to python

--- dynamic_graph/OCR/pytorch/run_benchmark.sh
+++ dynamic_graph/OCR/pytorch/run_benchmark.sh
@@ -12,12 +12,12 @@ batch_list=(8 16)
 for batch in ${batch_list[@]}; do
 
     if [ ${mode} = "sp" ]; then
-        export CUDA_VISIBLE_DEVICES=0
-        train_cmd="python3.7 tools/train.py --config_file config/icdar2015_resnet18_FPN_DBhead_polyLR_bs${batch}.yaml"
+        #export CUDA_VISIBLE_DEVICES=0
+        train_cmd="python tools/train.py --config_file config/icdar2015_resnet18_FPN_DBhead_polyLR_bs${batch}.yaml"
         num_gpu_devices=1
     else
-        export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-        train_cmd="python3.7 -m torch.distributed.launch --nproc_per_node=8  tools/train.py  --config_file config/icdar2015_resnet18_FPN_DBhead_polyLR_bs${batch}.yaml"
+        #export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+        train_cmd="python -m torch.distributed.launch --nproc_per_node=8  tools/train.py  --config_file config/icdar2015_resnet18_FPN_DBhead_polyLR_bs${batch}.yaml"
         num_gpu_devices=8
     fi
  
@@ -36,13 +36,14 @@ for batch in ${batch_list[@]}; do
         echo -e "${model_name}, SUCCESS"
         export job_fail_flag=0
     fi
-    kill -9 `ps -ef|grep 'python3.7'|awk '{print $2}'`
+    kill -9 `ps -ef|grep 'python'|awk '{print $2}'`
 
     if [ $mode = "mp" -a -d mylog ]; then
         rm ${log_file}
         cp mylog/workerlog.0 ${log_file}
     fi
-    analysis_cmd="python3.7 benchmark/analysis.py --filename ${log_file}  --mission_name ${model_name} --run_mode ${mode} --direction_id 0 --keyword 'speed:' --base_batch_size ${batch} --skip_steps 1 --gpu_num ${num_gpu_devices}  --index 1  --model_mode=-1  --ips_unit=samples/sec"
+    
+    analysis_cmd="python benchmark/analysis.py --filename ${log_file}  --mission_name ${model_name} --run_mode ${mode} --direction_id 0 --keyword 'speed:' --base_batch_size ${batch} --skip_steps 1 --gpu_num ${num_gpu_devices}  --index 1  --model_mode=-1  --ips_unit=samples/sec"
     eval $analysis_cmd
 done
 
