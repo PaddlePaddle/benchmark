9aec10e661b767e79dadf2fc505f3b79cf43d948
zhoujun <572459439@qq.com>
2021-10-29 02:33:58 +0800

    Update preData.sh

--- OtherFrame/ocr/PyTorch/scripts/PSENet/preData.sh
+++ OtherFrame/ocr/PyTorch/scripts/PSENet/preData.sh
@@ -1,4 +1,4 @@
 # 下载数据集并解压
 cd /workspace/models/PSENet
-
-wget -c  -p ./tain_data/  https://paddleocr.bj.bcebos.com/dygraph_v2.0/test/icdar2015.tar && cd train_data  && tar xf icdar2015.tar && cd ../
+rm -rf train_data
+wget -P ./train_data/ -N https://paddleocr.bj.bcebos.com/dygraph_v2.0/test/icdar2015.tar && cd train_data  && tar xf icdar2015.tar && cd ../
