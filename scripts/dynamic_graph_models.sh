40f8791bf4c282fdbfbc75fd2d995d6db9bca77b
mmglove <gmm_email@163.com>
2021-11-08 19:35:44 +0800

    add_pwgan_1108,test=document_fix

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 
-cur_model_list=(dy_bert dy_lac dy_transformer dy_wavenet dy_senta dy_mask_rcnn dy_yolov3 dy_slowfast dy_tsn dy_tsm dy_gan dy_seg dy_seq2seq dy_resnet dy_ptb_medium dy_mobilenet dy_ppocr_mobile_2 dy_bmn dy_faster_rcnn_fpn dy_gpt dy_seg_repo)
+cur_model_list=(dy_bert dy_lac dy_transformer dy_wavenet dy_senta dy_mask_rcnn dy_yolov3 dy_slowfast dy_tsn dy_tsm dy_gan dy_seg dy_seq2seq dy_resnet dy_ptb_medium dy_mobilenet dy_ppocr_mobile_2 dy_bmn dy_faster_rcnn_fpn dy_gpt dy_seg_repo dy_speech_repo_pwgan)
 #if  [ ${RUN_PROFILER} = "PROFILER" ]; then
 #    log_path=${PROFILER_LOG_DIR:-$(pwd)}  #  benchmark系统指定该参数,如果需要跑profile时,log_path指向存profile的目录
 #fi
@@ -29,6 +29,12 @@ dy_seg_repo(){
     bash benchmark/run_all.sh
 }
 
+dy_speech_repo_pwgan(){
+    echo "dy_speech_repo_pwgan"
+    cur_model_path=${BENCHMARK_ROOT}/DeepSpeech/
+    cd ${cur_model_path}/tests/benchmark/pwgan/
+    bash run_all.sh
+}
 #run_bert
 dy_bert(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleNLP/examples/language_model/bert/
