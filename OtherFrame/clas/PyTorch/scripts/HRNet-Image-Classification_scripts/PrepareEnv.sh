40bae9fee987f8cb6a4446a875249381b2bde069
hysunflower <nothing_upc@163.com>
2021-12-15 09:15:25 +0000

    update scripts, test=document_fix

--- OtherFrame/clas/PyTorch/scripts/HRNet-Image-Classification_scripts/PrepareEnv.sh
+++ OtherFrame/clas/PyTorch/scripts/HRNet-Image-Classification_scripts/PrepareEnv.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # install env
-pip install torch torchvision
-if [ $? -eq 1 ]; then
+if [ ${RUN_PLAT} == "local" ]; then
     pip install ${all_path}/other_frame_whls/torch-1.10.0-cp37-cp37m-manylinux1_x86_64.whl
     pip install  ${all_path}/other_frame_whls/torchvision-0.11.1-cp37-cp37m-manylinux1_x86_64.whl
+else
+    pip install torch torchvision
 fi
 sed -i "s/opencv-python==3.4.1.15/opencv-python==4.4.0.46/g" requirements.txt
 sed -i "s/shapely==1.6.4/shapely/g" requirements.txt
--- OtherFrame/clas/PyTorch/scripts/Twins_scripts/PrepareEnv.sh
+++ OtherFrame/clas/PyTorch/scripts/Twins_scripts/PrepareEnv.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # install env
-pip install torch torchvision
-if [ $? -eq 1 ]; then
+if [ ${RUN_PLAT} == "local" ]; then
     pip install ${all_path}/other_frame_whls/torch-1.10.0-cp37-cp37m-manylinux1_x86_64.whl
     pip install  ${all_path}/other_frame_whls/torchvision-0.11.1-cp37-cp37m-manylinux1_x86_64.whl
+else
+    pip install torch torchvision
 fi
 pip install timm==0.4.5
