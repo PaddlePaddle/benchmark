db0a4718afe37cc34a813c563a65a99008022e20
hysunflower <nothing_upc@163.com>
2021-12-24 09:08:05 +0000

    update_other_scripts, test=document_fix

--- OtherFrame/Speech/PyTorch/PWGAN/run_PyTorch.sh
+++ OtherFrame/Speech/PyTorch/PWGAN/run_PyTorch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#http_proxy=http://172.19.57.45:3128
-
 ImageName="registry.baidubce.com/paddlepaddle/paddle:2.1.2-gpu-cuda10.2-cudnn7";
 docker pull ${ImageName}
 export BENCHMARK_ROOT=/workspace 
@@ -12,11 +10,11 @@ run_cmd="cd /workspace
         cp ${BENCHMARK_ROOT}/scripts/Parakeet/PWGAN/preData.sh ./;
         cp ${BENCHMARK_ROOT}/scripts/Parakeet/PWGAN/run_benchmark.sh ./;
         cp ${BENCHMARK_ROOT}/scripts/Parakeet/PWGAN/analysis_log.py ./;
+        bash preData.sh
         sed -i '/set\ -xe/d' run_benchmark.sh
         CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh Parakeet_pwgan sp fp32 6 100 3;
         CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh Parakeet_pwgan mp fp32 6 100 3;
         "
-        #bash preData.sh
 #        CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh Parakeet_pwgan sp fp32 26 100 3;  # 模型名改成小写 和benchmark paddle 一致 否则数据无法前端正常显示
 #        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh Parakeet_pwgan mp fp32 26 100 3;
 
