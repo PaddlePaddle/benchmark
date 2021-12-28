2c60189c268659d55fead5502b3373acc754674f
xiegegege <xiege01@baidu.com>
2021-12-28 18:47:20 +0800

    fix model_benchmark_ci,test=document_fix

--- scripts/benchmark_ci/model_ci.sh
+++ scripts/benchmark_ci/model_ci.sh
@@ -14,7 +14,7 @@ pip install paddlenlp
 cd ${BENCHMARK_ROOT}/scripts/benchmark_ci
 #model_list='ResNet50_bs32_dygraph ResNet50_bs32 bert_base_seqlen128_fp32_bs32 transformer_base_bs4096_amp_fp16 yolov3_bs8 TSM_bs16 deeplabv3_bs4 CycleGAN_bs1 mask_rcnn_bs1 PPOCR_mobile_2_bs8 seq2seq_bs128'
 #test 
-model_list='ResNet50_bs32_dygraph deeplabv3_bs4 yolov3_bs8 bert_base_seqlen128_fp32_bs32'
+model_list='ResNet50_bs32 ResNet50_bs32_dygraph deeplabv3_bs4 yolov3_bs8 bert_base_seqlen128_fp32_bs32'
 source run_models.sh
 for model in ${model_list}
 do
