7b53ef6ed9a809cfbdbcfcd84dffca9c0814f53c
JamesLim-sy <santamonic@sina.com>
2021-12-29 01:51:43 +0000

    delete useless configs

--- api/dynamic_tests_v2/run.sh
+++ api/dynamic_tests_v2/run.sh
@@ -16,7 +16,7 @@ config_id=${2:-"0"}
 task=${3:-"accuracy"} # "accuracy" or "speed"
 
 testing_mode="dynamic" # "static" or "dynamic"
-framework="pytorch"  # "paddle" or "tensorflow" or "pytorch"
+framework="paddle"  # "paddle" or "tensorflow" or "pytorch"
 filename="${OP_BENCHMARK_ROOT}/tests_v2/configs/${name}.json"
 if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
   use_gpu=False
@@ -33,7 +33,7 @@ run_args="--task ${task} \
           --profiler none \
           --backward True \
           --use_gpu ${use_gpu} \
-          --repeat 1 \
+          --repeat 10000 \
           --allow_adaptive_repeat False \
           --log_level 0"
 
