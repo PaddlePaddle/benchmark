6df0ba747d0291cec82ba7018fef423366e28f26
mmglove <gmm_email@163.com>
2021-12-31 15:23:25 +0800

    fix conformer

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -58,7 +58,7 @@ dy_speech_repo_conformer(){
     cp ${data_path}/dygraph_data/conformer/aishell.py ${cur_model_path}/dataset/aishell/
     pip install loguru
     bash prepare.sh
-    bash prepare.sh             # 第一轮数据处理会报错
+    echo "bash run.sh --stage 0 --stop_stage 0" >> bash prepare.sh             # 第一轮数据处理会报错
     bash run.sh
     rm -rf ${BENCHMARK_ROOT}/PaddleSpeech/dataset/aishell    # 避免数据集占用docker内过多空间,在执行最后一个模型后删掉
 }
@@ -67,6 +67,7 @@ dy_video_TimeSformer(){
     echo "dy_video_TimeSformer"
     cur_model_path=${BENCHMARK_ROOT}/PaddleVideo/
     cd ${cur_model_path}/benchmark/TimeSformer/
+    pip install scikit-image==0.18.1
     bash run_all.sh local
     rm -rf ${BENCHMARK_ROOT}/PaddleVideo/    # 避免数据集占用docker内过多空间,在执行最后一个模型后删掉
 }
@@ -249,7 +250,8 @@ dy_tsn(){
     cur_model_path=${BENCHMARK_ROOT}/PaddleVideo
     cd ${cur_model_path}
 
-    pip install wget av
+    pip install wget av 
+    pip install scikit-image==0.18.1
     # Prepare pretrained modles
     rm -rf ResNet50_pretrain.pdparams
     ln -s ${prepare_path}/tsn/ResNet50_pretrain.pdparams ${cur_model_path}/
@@ -339,6 +341,7 @@ dy_slowfast(){
     pip install tqdm
     pip install decord
     pip install pandas av
+    pip install scikit-image==0.18.1
     # Prepare data
     rm -rf data
     ln -s ${data_path}/dygraph_data/slowfast/data/ ${cur_model_path}/
@@ -451,6 +454,7 @@ dy_tsm(){
     cd ${cur_model_path}
 
     pip install wget av decord
+    pip install scikit-image==0.18.1
     # Prepare pretrained modles
     ln -s ${prepare_path}/tsm/ResNet50_pretrain.pdparams ${cur_model_path}/
     # Prepare data
@@ -616,7 +620,7 @@ dy_ppocr_mobile_2() {
 dy_bmn() {
     cur_model_path=${BENCHMARK_ROOT}/PaddleVideo
     cd ${cur_model_path}
-
+    pip install scikit-image==0.18.1
     package_check_list=(tqdm PyYAML numpy decord pandas av)
     for package in ${package_check_list[@]}; do
         if python -c "import ${package}" >/dev/null 2>&1; then
