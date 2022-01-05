114a81a9783e9196dac8c70e147d9d4713cc4beb
hysunflower <52739577+hysunflower@users.noreply.github.com>
2022-01-05 18:56:35 +0800

    【暂不合入  八卡报错】add resent dynamic fp16, test=document_fix (#1236)
    * add resent dynamic fp16, test=document_fix
    
    * add resent dynamic fp16, test=document_fix
    
    * add resent dynamic fp16, test=document_fix

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -536,13 +536,10 @@ dy_resnet(){
     rm -f ./run_benchmark.sh
     cp ${BENCHMARK_ROOT}/dynamic_graph/resnet/paddle/run_benchmark_resnet.sh ./
     sed -i '/set\ -xe/d' run_benchmark_resnet.sh
-    batch_size=32
-    model_list=(ResNet152_bs32 ResNet50_bs32 ResNet50_bs128)
+    model_list=(ResNet152_bs32 ResNet50_bs32 ResNet50_bs128 ResNet50_amp_fp16_bs128 ResNet50_amp_fp16_bs256) #ResNet50_pure_fp16_bs128
     for model_name in ${model_list[@]}
     do
-        if [ ${model_name} == "ResNet50_bs128" ]; then
-            batch_size=128
-        fi
+	batch_size=${model_name#*bs}
         echo "model is ${model_name}, index is speed, 1gpu begin"
         CUDA_VISIBLE_DEVICES=0 bash run_benchmark_resnet.sh 1 ${batch_size} ${model_name} sp 1 | tee ${log_path}/dynamic_${model_name}_speed_1gpus 2>&1
         sleep 60
