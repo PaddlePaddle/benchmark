b019008a4a0ce0ece1e7258b5e6ebea7342e2cfe
hysunflower <52739577+hysunflower@users.noreply.github.com>
2022-01-12 16:51:29 +0800

    add pse scripts, test=document_fix (#1253)
    * add pse scripts, test=document_fix
    
    * add pse scripts, test=document_fix
    
    * add ocr db models, test=document_fix
    
    * add ocr db models, test=document_fix
    
    * add ocr db models, test=document_fix

--- OtherFrame/ocr/PyTorch/PrepareEnv.sh
+++ OtherFrame/ocr/PyTorch/PrepareEnv.sh
@@ -5,19 +5,9 @@ echo "*******prepare benchmark***********"
 
 ################################# 创建一些log目录,如:
 export BENCHMARK_ROOT=/workspace
-log_date=`date "+%Y.%m%d.%H%M%S"`
-frame=pytorch1.7.1
-cuda_version=10.2
-save_log_dir=${BENCHMARK_ROOT}/logs/${frame}_${log_date}_${cuda_version}/
-
-if [[ -d ${save_log_dir} ]]; then
-    rm -rf ${save_log_dir}
-fi
 # this for update the log_path coding mat
-export TRAIN_LOG_DIR=${save_log_dir}/train_log
-mkdir -p ${TRAIN_LOG_DIR}
 
-log_path=${TRAIN_LOG_DIR}
+log_path=${TRAIN_LOG_DIR:-"pwd"}
 
 ################################# 安装框架 如:
 pip3.7 install -U pip -i https://mirror.baidu.com/pypi/simple
@@ -26,7 +16,8 @@ pip3.7 install pqi -i https://mirror.baidu.com/pypi/simple
 pqi add baidu https://mirror.baidu.com/pypi/simple
 pqi use baidu
 
+#pip3.7 install torch==1.10.0+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 pip3.7 install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-
+pip3.7 install anyconfig
 echo "*******prepare benchmark end***********"
 
