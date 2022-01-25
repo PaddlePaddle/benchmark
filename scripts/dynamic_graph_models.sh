b5c51376be8179ca4d85195cc5d8b1657688e0c4
hysunflower <nothing_upc@163.com>
2022-01-25 09:11:35 +0000

    add seg fp16, test=document_fix

--- scripts/dynamic_graph_models.sh
+++ scripts/dynamic_graph_models.sh
@@ -319,19 +319,23 @@ dy_seg(){
     sed -i '/set\ -xe/d' run_benchmark.sh
 
     model_list=(deeplabv3 HRnet)
+    fp_mode_list=(fp32 fp16)
     for model_item in ${model_list[@]}
     do
-        if [ ${model_item} = "HRnet" ]; then
-            bs_item=8
-        elif [ ${model_item} = "deeplabv3" ]; then
-            bs_item=4
-        fi
-        echo "index is speed, ${model_item} 1gpu begin"
-        CUDA_VISIBLE_DEVICES=5 bash run_benchmark.sh 1 ${bs_item} sp ${model_item} 200 | tee ${log_path}/dynamic_seg_${model_item}_bs${bs_item}_speed_1gpus 2>&1
-        sleep 10
-        echo "index is speed, ${model_item} 8gpu begin"
-        CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 ${bs_item} mp ${model_item} 200 | tee ${log_path}/dynamic_seg_${model_item}_bs${bs_item}_speed_8gpus 2>&1
-        sleep 10
+        for fp_mode in ${fp_mode_list[@]}
+        do
+            if [ ${model_item} = "HRnet" ]; then
+                bs_item=8
+            elif [ ${model_item} = "deeplabv3" ]; then
+                bs_item=4
+            fi
+            echo "index is speed, ${model_item} 1gpu begin"
+            CUDA_VISIBLE_DEVICES=5 bash run_benchmark.sh 1 ${bs_item} sp ${model_item} 200 False ${fp_mode} | tee ${log_path}/dynamic_seg_${model_item}_bs${bs_item}_${fp_mode}_speed_1gpus 2>&1
+            sleep 10
+            echo "index is speed, ${model_item} 8gpu begin"
+            CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh 1 ${bs_item} mp ${model_item} 200 False ${fp_mode} | tee ${log_path}/dynamic_seg_${model_item}_bs${bs_item}_${fp_mode}_speed_8gpus 2>&1
+            sleep 10
+        done
     done
 }
 
