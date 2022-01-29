807e8e52fd110e797b9158141fa08bb7fac5ceeb
shangliang Xu <ghostxsl@users.noreply.github.com>
2022-01-29 19:26:05 +0800

    [torch detection] upgrade detection benchmark (#1280)

---
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/higherhrnet/N1C8/higherhrnet_bs20_fp32_MultiP_DP.sh
@@ -0,0 +1,12 @@
+model_item=higherhrnet
+bs_item=20
+fp_item=fp32
+run_process_type=MultiP
+run_mode=DP
+device_num=N1C8
+max_epochs=1
+num_workers=2
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
