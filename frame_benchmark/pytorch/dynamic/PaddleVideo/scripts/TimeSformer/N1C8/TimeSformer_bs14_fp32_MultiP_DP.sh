b181a6c9fe1de3b9b24498258d295ccb7b60cc31
HydrogenSulfate <490868991@qq.com>
2022-01-29 18:53:45 +0800

    add TimeSformer benchmark  (#1276)
    * update benchmark in pytorch's code
    
    * update code
    
    * update code
    
    * update torch's version
    
    * Update TimeSformer_bs14_fp32_MultiP_DP.sh
    
    delete 'benchmark/' in pre path

---
+++ frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/TimeSformer/N1C8/TimeSformer_bs14_fp32_MultiP_DP.sh
@@ -0,0 +1,9 @@
+model_item=TimeSformer
+bs_item=14
+fp_item=fp32
+run_process_type=MultiP
+run_mode=DP
+device_num=N1C8
+
+bash prepare.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} 2>&1;
