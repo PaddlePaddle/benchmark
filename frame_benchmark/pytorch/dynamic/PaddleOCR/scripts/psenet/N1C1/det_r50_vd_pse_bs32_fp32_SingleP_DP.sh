49214d07d2f73f8510263d6660da147a2fbacb2a
xiaoting <31891223+tink2123@users.noreply.github.com>
2022-02-08 19:17:18 +0800

    Pse benchmark (#1279)
    * add pse
    
    * polish pse shell
    
    * rename shell
    
    * polish analysis dict
    
    * fix speed_unit
    
    * add models_path
    
    Co-authored-by: gmm <38800877+mmglove@users.noreply.github.com>

---
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/psenet/N1C1/det_r50_vd_pse_bs32_fp32_SingleP_DP.sh
@@ -0,0 +1,13 @@
+model_item=det_r50_vd_pse
+#max_token
+bs_item=32
+fp_item=fp32
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C1
+max_iter=10
+num_workers=1
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_iter} ${num_workers} 2>&1;
