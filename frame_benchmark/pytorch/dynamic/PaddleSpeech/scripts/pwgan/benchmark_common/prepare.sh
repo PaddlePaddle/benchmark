f2ee6681807236e9164c670d74fef06de16c4849
TianYuan <white-sky@qq.com>
2022-02-09 04:29:21 +0000

    add pwg_benchmark

---
+++ frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/benchmark_common/prepare.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# 执行路径在竞品模型库的根目录下
+
+stage=3
+stop_stage=100
+
+echo "*******prepare benchmark start ***********"
+
+
+################################# 配置python
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    test -d venv || virtualenv -p python3.7 venv
+    source venv/bin/activate
+fi
+
+################################# 安装框架 如:
+
+# 安装 pyrtorch
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    pip install -U pip
+    echo `pip --version`
+    echo "install pytorch..."
+    pip install torch==1.9.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+    echo "pytorch installed"
+fi
+# 拉取模型代码并安装
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo "install pwgan..."
+    pip install -e .
+    echo "pwgan installed"
+    echo "install apex..."
+    # If you want to use distributed training, please run following command to install apex.
+    git clone https://github.com/NVIDIA/apex.git
+    pushd apex
+    git checkout 3303b3e
+    pip install -v --disable-pip-version-check --no-cache-dir ./
+    pushd ../
+    echo "apex installed"
+
+    # install nkf
+    apt-get install nkf -y
+    apt-get install sox -y
+    apt-get install jq -y
+
+fi
+
+################################# 准备训练数据 如:
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+    pushd egs/csmsc/voc1
+    bash run.sh --stage -1 --stop-stage 1
+    popd
+fi
+echo "*******prepare benchmark end***********"
---
+++ frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/benchmark_common/test_pwgan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 拷贝到 benmark 同级目录下执行
+# 创建docker：registry.baidu.com/paddle-benchmark/paddlecloud-base-image:paddlecloud-ubuntu18.04-gcc8.2-cuda11.2-cudnn8
+export ROOT_DIR=$PWD  # 设置个全局变量
+cd ${ROOT_DIR}
+#clone 2 处的代码
+#git clone https://github.com/mmglove/benchmark.git -b dev_0112 --recursive    # 重构后的benchmark 路径，改动太多，暂未合入
+#git clone ssh://guomengmeng01@icode.baidu.com:8235/baidu/paddle/benchmark-frame
+shell_name=pwgan_bs6_fp32_SingleP_DP.sh
+# shell_name=pwgan_bs26_fp32_SingleP_DP.sh
+# shell_name=pwgan_bs6_fp32_MultiP_DP.sh
+# shell_name=pwgan_bs26_fp32_MultiP_DP.sh
+model_path=${ROOT_DIR}/benchmark/frame_benchmark/pytorch/dynamic/PaddleSpeech/models/ParallelWaveGAN
+script_path=${ROOT_DIR}/benchmark/frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/N1C1/${shell_name}
+script_benchmark_common=${ROOT_DIR}/benchmark/frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/benchmark_common/*
+cp ${script_path} ${model_path}
+cp ${script_benchmark_common} ${model_path} -r
+cd ${model_path}
+bash prepare.sh
+bash ${shell_name}
