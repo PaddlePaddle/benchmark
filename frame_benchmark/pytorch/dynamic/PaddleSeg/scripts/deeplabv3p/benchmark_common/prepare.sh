9651540a3820bc3b1f65973a3d086655080972ea
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2022-02-14 19:44:09 +0800

    Add seg models: DeepLabV3p, HRNet (#1297)
    * add yml
    
    * add hrnet,deeplab

---
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/deeplabv3p/benchmark_common/prepare.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# 执行路径在模型库的根目录下
+################################# 安装框架 如:
+echo "*******prepare benchmark start ***********"
+pip install -U pip
+echo `pip --version`
+pip install torch==1.9.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install torchvision==0.10.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install mmcv-full==1.3.13 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt  -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -e .
+pip list
+
+wget https://paddleseg.bj.bcebos.com/benchmark/mmseg/mmseg_benchmark_configs.tar.gz
+tar -zxf mmseg_benchmark_configs.tar.gz
+################################# 准备训练数据 如:
+mkdir -p data
+wget https://paddleseg.bj.bcebos.com/dataset/cityscapes_30imgs.tar.gz \
+    -O data/cityscapes_30imgs.tar.gz
+tar -zxf data/cityscapes_30imgs.tar.gz -C data/
+mv data/cityscapes_30imgs data/cityscapes
+echo "*******prepare benchmark end***********"
+
+
+
+
