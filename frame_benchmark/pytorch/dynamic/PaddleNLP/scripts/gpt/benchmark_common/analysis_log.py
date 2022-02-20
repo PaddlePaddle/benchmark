8d72477695300bd01438282c8d24a6d6d8bd3246
Zhong Hui <zhonghui.net@gmail.com>
2022-02-20 21:44:19 +0800

    add megatron gpt

---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/gpt/benchmark_common/analysis_log.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+
+import re
+import sys
+import json
+import os
+
+
+def analyze(model_item, log_file, res_log_file, device_num):
+    bs_pat = re.compile(r"global_batch_size ............................... (.*)")
+    time_pat = re.compile(r"elapsed time per iteration \(ms\): (.*) \| learning")
+    gpu_ids_pat = re.compile(r"world_size ......................................(.*)")
+    fp_pat = re.compile(r"fp16 ............................................(.*)")
+
+    logs = open(log_file).readlines()
+    logs = ";".join(logs)
+    bs_res = bs_pat.findall(logs)
+    time_res = time_pat.findall(logs)
+    gpu_ids_res = gpu_ids_pat.findall(logs)
+    fp_res = fp_pat.findall(logs)
+    # print(gpu_ids_res)
+    # print(bs_res)
+    # print(time_res)
+    # print(fp_res)
+
+    run_mode = ""
+    gpu_num = 0
+    ips = 0
+    fp_item = "fp32"
+    bs = 0
+    run_process_type = "SingleP" 
+
+    if gpu_ids_res == [] or time_res == []:
+        ips = 0
+    else:
+        # delete first 20 steps
+        iter_times = [float(x) for x in  time_res[20:-1]]
+        gpu_num = int(gpu_ids_res[-1])
+        run_mode = "SP" if gpu_num == 1 else "MP"
+        run_process_type = "SingleP" if gpu_num == 1 else "MultiP"
+        if fp_res[-1].strip().lower() == "true":
+            fp_item = "fp16" 
+        bs = int(bs_res[-1])
+        ips = bs * 1000 * 1024 /( sum(iter_times)/len(iter_times) )
+
+    model_name = model_item+"_"+"bs"+str(bs)+"_"+fp_item+"_"+run_mode
+    info = {
+                "model_branch": os.getenv('model_branch'),
+                "model_commit": os.getenv('model_commit'),
+                "model_name": model_name,
+                "batch_size": bs,
+                "fp_item": fp_item,
+                "run_process_type": run_process_type,
+                "run_mode": run_mode,
+                "convergence_value": 0,
+                "convergence_key": "",
+                "ips": ips,
+                "speed_unit": "tokens/s",
+                "device_num": device_num,
+                "model_run_time": os.getenv('model_run_time'),
+                "frame_commit": "",
+                "frame_version": os.getenv('frame_version'),
+    }
+    json_info = json.dumps(info)
+    print(json_info)
+    with open(res_log_file, "w") as of:
+        of.write(json_info)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage:" + sys.argv[0] + " model_item path/to/log/file path/to/res/log/file")
+        sys.exit()
+
+    model_item = sys.argv[1]
+    log_file = sys.argv[2]
+    res_log_file = sys.argv[3]
+    device_num = sys.argv[4]
+    analyze(model_item, log_file, res_log_file, device_num)
---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/gpt/benchmark_common/prepare.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -xe
+echo "*******prepare benchmark***********"
+################################# 创建一些log目录,如:
+# export BENCHMARK_ROOT=/workspace   # 起容器的时候映射的目录  benchmark/OtherFrameworks/PyTorch/
+run_env=$ROOT_DIR/run_env
+log_date=`date "+%Y.%m%d.%H%M%S"`
+
+################################# 配置python, 如:
+rm -rf $run_env
+mkdir $run_env
+echo `which python3.7`
+ln -s $(which python3.7)m-config  $run_env/python3-config
+#ln -s /usr/local/python3.7.0/lib/python3.7m-config /usr/local/bin/python3-config
+ln -s $(which python3.7) $run_env/python
+ln -s $(which pip) $run_env/pip
+
+export PATH=$run_env:${PATH}
+
+#pip install -U pip
+echo `pip --version`
+echo `python3-config --help`
+
+#pip install -U pip
+# echo `pip --version`
+# python ${BENCHMARK_ROOT}/paddlecloud/file_upload_download.py \
+#     --remote-path frame_benchmark/pytorch_req/pytorch_191/ \
+#     --local-path ./  \
+#     --mode download
+# ls
+pip install torch==1.9.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install regex pybind11 Ninja 
+cd ../apex
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+cd -
+echo "*******prepare benchmark end***********"
+
+
+# 下载训练数据
+
+if [ -d data ]
+then
+  rm -rf data
+fi
+
+mkdir data && cd data
+wget https://paddlenlp.bj.bcebos.com/models/transformers/gpt2/dataset/my-gpt2_text_document.idx
+wget https://paddlenlp.bj.bcebos.com/models/transformers/gpt2/dataset/my-gpt2_text_document.bin
+cd -
+
+if [ -d token_files ]
+then
+  rm -rf token_files
+fi
+
+mkdir token_files && cd token_files
+wget http://paddlenlp.bj.bcebos.com/models/transformers/gpt/gpt-en-vocab.json
+wget http://paddlenlp.bj.bcebos.com/models/transformers/gpt/gpt-en-merges.txt
+cd -
+
+echo "*******prepare benchmark end***********"
