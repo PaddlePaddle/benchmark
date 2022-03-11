67fe7c22d4daa7342ec8b5dbb552917664c55001
LiuChiachi <709153940@qq.com>
2022-03-11 11:46:17 +0000

    add torch bert

---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_large/N1C1/bert_large_bs4_fp32_SingleP_DP.sh
@@ -0,0 +1,12 @@
+model_item=bert_large
+bs_item=4
+fp_item=fp16
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C1
+max_iter=100
+num_workers=1
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_iter} ${num_workers} 2>&1;
---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_large/benchmark_common/PrepareEnv.sh
@@ -0,0 +1,35 @@
+echo "*******prepare benchmark start ***********"
+
+cd PyTorch/LanguageModeling/BERT
+
+ln -s  $PWD /workspace/bert
+
+mkdir /workspace/bert/data
+
+export http_proxy=http://172.19.57.45:3128
+export https_proxy=http://172.19.57.45:3128
+export ftp_proxy=http://172.19.57.45:3128/
+export no_proxy=localhost,bj.bcebos.com,su.bcebos.com
+
+export BERT_PREP_WORKING_DIR=/workspace/bert/data
+
+bash data/create_datasets_from_start.sh
+
+
+
+cd /workspace/bert
+
+pip install --no-cache-dir \
+ tqdm boto3 requests six ipdb h5py nltk progressbar onnxruntime tokenizers>=0.7\
+ git+https://github.com/NVIDIA/dllogger wget
+
+apt-get install -y iputils-ping
+
+# Install lddl
+conda install -y jemalloc
+pip install /workspace/bert/lddl
+python -m nltk.downloader punkt
+
+RUN pip install lamb_amp_opt/
+
+echo "*******prepare benchmark end***********"
