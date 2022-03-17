8d07cf447acb5b13ba161072021fad9e03c431e3
LiuChiachi <709153940@qq.com>
2022-03-17 09:21:43 +0000

    upload data to bos

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_large_seqlen512/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_large_seqlen512/benchmark_common/PrepareEnv.sh
@@ -12,10 +12,6 @@ export BERT_PREP_WORKING_DIR=/workspace/bert/data
 
 apt-get update && apt-get install -y pbzip2 pv bzip2 cabextract
 
-cd /workspace
-git clone https://github.com/attardi/wikiextractor.git && cd wikiextractor && git checkout 6408a430fc504a38b04d37ce5e7fc740191dee16 && cd ..
-git clone https://github.com/soskek/bookcorpus.git
-
 apt-get install libb64-0d
 
 cd /workspace/bert
@@ -29,8 +25,8 @@ apt-get install -y iputils-ping
 # bash data/create_datasets_from_start.sh
 cd /workspace/bert/data/
 # hdf5_lower_case_1_seq_len_512_max_pred_80_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5/wikicorpus_en
-wget https://bj.bcebos.com/paddlenlp/data/bert_512_wiki_demo_data.zip
-unzip bert_512_wiki_demo_data.zip
+wget https://bj.bcebos.com/paddlenlp/data/hdf5_lower_case_1_seq_len_512_max_pred_80_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5.zip
+unzip hdf5_lower_case_1_seq_len_512_max_pred_80_masked_lm_prob_0.15_random_seed_12345_dupe_factor_5.zip
 
 cd ${ROOT_DIR}
 
