dfe9d2ab99b98a4fd5e0849670843d0c1a97e824
0x45f <23097963+0x45f@users.noreply.github.com>
2022-03-22 20:16:15 +0800

    Refine warpctc for benchmark (#1412)
    * Refine warpctc for benchmark test=document_fix
    
    * refine var name
    
    * just for test=document_fix

--- api/dynamic_tests_v2/warpctc.py
+++ api/dynamic_tests_v2/warpctc.py
@@ -16,20 +16,23 @@ from common_import import *
 
 class PaddleWarpctc(PaddleDynamicAPIBenchmarkBase):
     def build_graph(self, config):
-        logits_length = self.variable(name='logits_length', shape=config.logits_length_shape, 
-                                      dtype=config.logits_length_dtype, 
+        log_probs = self.variable(name='log_probs', shape=config.log_probs_shape, dtype=config.log_probs_dtype)
+        labels = self.variable(name='labels', shape=config.labels_shape, dtype=config.labels_dtype)
+        input_lengths = self.variable(name='input_lengths', shape=config.input_lengths_shape, 
+                                      dtype=config.input_lengths_dtype, 
                                       value=np.array([config.max_seq_length]*config.batch_size).astype("int64"))
-        label_length = self.variable(name='label_length', shape=config.label_length_shape, 
-                                     dtype=config.label_length_dtype, 
+        label_lengths = self.variable(name='label_lengths', shape=config.label_lengths_shape, 
+                                     dtype=config.label_lengths_dtype, 
                                      value=np.array([config.max_label_length]*config.batch_size).astype("int64"))
-        logits = self.variable(name='logits', shape=config.logits_shape, dtype=config.logits_dtype)
-        label = self.variable(name='label', shape=config.label_shape, dtype=config.label_dtype)
-        result = paddle.fluid.layers.warpctc(input=logits, label=label, input_length=logits_length, 
-                                             label_length=label_length, blank=config.blank, norm_by_times=config.norm_by_times)
-        self.feed_list = [logits_length, label_length, logits, label]
+
+        result = paddle.nn.functional.ctc_loss(log_probs=log_probs, labels=labels, input_lengths=input_lengths, 
+                                    label_lengths=label_lengths, blank=config.blank, 
+                                    reduction=config.reduction, norm_by_times=config.norm_by_times)
+        
+        self.feed_list = [log_probs, labels, input_lengths, label_lengths]
         self.fetch_list = [result]
         if config.backward:
-            self.append_gradients(result, [logits])
+            self.append_gradients(result, [log_probs])
 
 if __name__ == '__main__':
     test_main(
