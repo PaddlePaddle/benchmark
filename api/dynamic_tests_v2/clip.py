5f18a1224bea7e6bde634b8e2d914b95c335ec32
wuyefeilin <30919197+wuyefeilin@users.noreply.github.com>
2022-04-07 20:47:48 +0800

    add clip, erf and yolov3_loss test, test=document_fix (#1436)
    * add clip, erf and yolov3_loss test
    
    * update clip.py and clip.json
    
    * rm yolov3_loss test
    
    * update clip.json
    
    * update clip.json, test=document_fix

---
+++ api/dynamic_tests_v2/clip.py
@@ -0,0 +1,37 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+class ClipConfig(APIConfig):
+    def __init__(self):
+        super(ClipConfig, self).__init__("clip")
+        self.feed_spec = {"range": [-1, 1]}
+        self.run_torch = False
+
+
+class PaddleClip(PaddleDynamicAPIBenchmarkBase):
+    def build_graph(self, config):
+        x = self.variable(name="x", shape=config.x_shape, dtype=config.x_dtype)
+        result = paddle.clip(x=x, min=config.min, max=config.max)
+
+        self.feed_list = [x]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [x])
+
+
+if __name__ == '__main__':
+    test_main(pd_dy_obj=PaddleClip(), config=ClipConfig())
