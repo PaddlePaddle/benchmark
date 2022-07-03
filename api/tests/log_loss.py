242ed84129d8ddc63b40ce213c895e24ac6ecc06
Yiqun Liu <Xreki@users.noreply.github.com>
2022-07-03 20:07:17 +0800

    Move the rest test scripts to unified. (#1470)

---
+++ api/tests/log_loss.py
@@ -0,0 +1,38 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+@benchmark_registry.register("log_loss")
+class LogLossConfig(APIConfig):
+    def __init__(self):
+        super(LogLossConfig, self).__init__("log_loss")
+        self.run_torch = False
+        self.feed_spec = [{"range": [-1, 1]}, {"range": [-1, 1]}]
+
+
+@benchmark_registry.register("log_loss")
+class PaddleLogLoss(PaddleOpBenchmarkBase):
+    def build_graph(self, config):
+        input = self.variable(
+            name='input', shape=config.input_shape, dtype=config.input_dtype)
+        label = self.variable(
+            name='label', shape=config.label_shape, dtype=config.label_dtype)
+        result = paddle.nn.functional.log_loss(
+            input=input, label=label, epsilon=config.epsilon)
+        self.feed_list = [input, label]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [input])
---
+++ api/tests/take_along_axis.py
@@ -0,0 +1,41 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+
+@benchmark_registry.register("take_along_axis")
+class TakeAlongAxisConfig(APIConfig):
+    def __init__(self):
+        super(TakeAlongAxisConfig, self).__init__("take_along_axis")
+        self.feed_spec = [{"range": [-1, 1]}, {"range": [0, 100]}]
+        self.run_torch = False
+
+
+@benchmark_registry.register("take_along_axis")
+class PaddleTakeAlongAxis(PaddleOpBenchmarkBase):
+    def build_graph(self, config):
+        arr = self.variable(
+            name="arr", shape=config.arr_shape, dtype=config.arr_dtype)
+        indices = self.variable(
+            name="indices",
+            shape=config.indices_shape,
+            dtype=config.indices_dtype)
+        result = paddle.take_along_axis(
+            arr=arr, indices=indices, axis=config.axis)
+
+        self.feed_list = [arr, indices]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [arr])
