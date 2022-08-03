3eb1f513504faafaf6aa04b1fd790568ab88cb5f
yuehuayingxueluo <867460659@qq.com>
2022-08-03 19:48:30 +0800

    Update prepare.sh
    change prepare.sh

--- frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/deeplabv3p_resnet50/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/deeplabv3p_resnet50/benchmark_common/prepare.sh
@@ -10,17 +10,17 @@ CUDA_VERSION=`nvcc -run detect_cuda.cu | awk -F. '{print $1}'`
 
 WHEEL_URL_PREFIX="https://paddle-wheel.bj.bcebos.com/benchmark"
 apt-get install -y axel
-#if [[ $CUDA_VERSION -ge 11 ]]; then
-#  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
-#  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
-#  pip install torch-1.9.1+cu111-cp37-cp37m-linux_x86_64.whl
-#  pip install torchvision-0.10.1+cu111-cp37-cp37m-linux_x86_64.whl
-#else
-#  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
-#  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
-#  pip install torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl
-#  pip install torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl
-#fi
+if [[ $CUDA_VERSION -ge 11 ]]; then
+  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  pip install torch-1.9.1+cu111-cp37-cp37m-linux_x86_64.whl
+  pip install torchvision-0.10.1+cu111-cp37-cp37m-linux_x86_64.whl
+else
+  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
+  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
+  pip install torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl
+  pip install torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl
+fi
 
 # pip install torch==1.9.1 -i https://mirrors.ustc.edu.cn/pypi/web/simple
 # pip install torchvision==0.10.1+cu111 -i https://pypi.tuna.tsinghua.edu.cn/simple
