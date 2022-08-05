c71748079ca8ed222cd2cce77deef946078211e9
yuehuayingxueluo <867460659@qq.com>
2022-08-05 14:29:38 +0000

    add three PaddleSeg models' scripts

--- frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/fastscnn/benchmark_common/run_benchmark.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/fastscnn/benchmark_common/run_benchmark.sh
@@ -60,7 +60,7 @@ function _train(){
     if [ ${device_num} = "N1C1" ]; then
         train_cmd="python tools/train.py ${train_config} ${train_options}"
     else
-        train_cmd="./tools/dist_train.sh ${train_config} 8 ${train_options}"
+        train_cmd="./tools/dist_train.sh ${train_config} 8 ${device_num:1:1} ${train_options}"
     fi
 
     echo "=============="
@@ -95,4 +95,3 @@ _train
 job_et=`date '+%Y%m%d%H%M%S'`
 export model_run_time=$((${job_et}-${job_bt}))
 _analysis_log
-
