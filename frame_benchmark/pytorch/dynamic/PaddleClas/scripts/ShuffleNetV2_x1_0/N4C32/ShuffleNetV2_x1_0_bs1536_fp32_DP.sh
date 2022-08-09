d35a4952c6f623f149872a4924db2005e2c42eae
Mine <mofengshenjieII@163.com>
2022-08-09 03:18:26 +0000

    Add distributed training scripts for ResNet50_bs128_fp32, ShuffleNetV2_x1_0_bs1536_fp32, MobileNetV3_large_x1_0_bs640_fp32 and SwinTransformer_tiny_patch4_window7_224_bs104_fp32. Now these benchmark can be tested on multi nodes. And fix bug of HRNet. When multi-process create the same folder, it may cause conflict.

---
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/ShuffleNetV2_x1_0/N4C32/ShuffleNetV2_x1_0_bs1536_fp32_DP.sh
@@ -0,0 +1,18 @@
+model_item=ShuffleNetV2_x1_0
+bs_item=1500
+fp_item=fp32
+run_process_type=MultiP
+run_mode=DP
+device_num=N4C32
+max_epoch=1
+num_workers=4
+
+node_num=${PADDLE_TRAINERS_NUM}
+node_rank=${PADDLE_TRAINER_ID}
+master_addr=${POD_0_IP}
+master_port=14233
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epoch} ${num_workers} ${node_num} ${node_rank} ${master_addr} ${master_port} 2>&1;
+
---
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/SwinTransformer_tiny_patch4_window7_224/N4C32/SwinTransformer_tiny_patch4_window7_224_bs104_fp32_DP.sh
@@ -0,0 +1,17 @@
+model_item=SwinTransformer_tiny_patch4_window7_224
+bs_item=104
+fp_item=fp32
+run_process_type=MultiP
+run_mode=DP
+device_num=N4C32
+max_epoch=5
+num_workers=4
+
+node_num=${PADDLE_TRAINERS_NUM}
+node_rank=${PADDLE_TRAINER_ID}
+master_addr=${POD_0_IP}
+master_port=14233
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epoch} ${num_workers} ${node_num} ${node_rank} ${master_addr} ${master_port} 2>&1;
