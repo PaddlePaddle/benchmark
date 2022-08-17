71c4cff7c678ecd5daafdc723d9e12e5af62f609
yuehuayingxueluo <867460659@qq.com>
2022-08-17 16:44:50 +0800

    Update prepare.sh
    fix importlib-metadata and libsndfile1-dev

--- frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/conformer/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/conformer/benchmark_common/prepare.sh
@@ -25,6 +25,8 @@ pip install torch-1.9.1+cu111-cp37-cp37m-linux_x86_64.whl
 pip install torchvision-0.10.1+cu111-cp37-cp37m-linux_x86_64.whl
 wget https://paddle-wheel.bj.bcebos.com/benchmark/torchaudio-0.9.1-cp37-cp37m-manylinux1_x86_64.whl
 pip install torchaudio-0.9.1-cp37-cp37m-manylinux1_x86_64.whl
+pip install importlib-metadata -i https://pypi.tuna.tsinghua.edu.cn/simple
+apt-get install libsndfile1-dev -y
 # pip install torch==1.9.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 #conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c conda-forge
