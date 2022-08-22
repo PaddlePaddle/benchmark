21d00f44bf29dfe73703b25e7e82c34e16c7fb5e
yuehuayingxueluo <867460659@qq.com>
2022-08-22 06:37:47 +0000

    update PaddleViode's TimeSformer model

--- frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/TimeSformer/benchmark_common/run_benchmark.sh
+++ frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/TimeSformer/benchmark_common/run_benchmark.sh
@@ -42,11 +42,16 @@ function _analysis_log(){
 }
 function _train(){
     batch_size=${base_batch_size}  # 如果模型跑多卡单进程时,请在_train函数中计算出多卡需要的bs
+
+    PORT=${PORT:-29500}
+
     echo "current ${model_name} CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES, gpus=${device_num}, batch_size=${batch_size}"
     accu_steps=8  # TimeSformer梯度累加8次
     train_config="--cfg configs/Kinetics/TimeSformer_divST_8x32_224.yaml"
     batch_size_all_gpu=`expr ${batch_size} \* ${num_gpu_devices}`  # 所有卡的单次bs之和
     batch_size_all_gpu_acc=`expr ${batch_size_all_gpu} \* ${accu_steps}`  # 所有卡的accu次bs之和
+    echo "batch_size_all_gpu = $batch_size_all_gpu"
+    echo "batch_size_all_gpu_acc = $batch_size_all_gpu_acc"
     # TRAIN.BATCH_SIZE是全部卡的bs之和
     train_options="TEST.ENABLE False \
                    TRAIN.AUTO_RESUME False \
@@ -57,12 +62,30 @@ function _train(){
                    GLOBAL_BATCH_SIZE ${batch_size_all_gpu_acc} \
                    TIMESFORMER.PRETRAINED_MODEL ./jx_vit_base_p16_224-80ecf9dd.pth \
                    SOLVER.MAX_EPOCH ${max_epoch}"
-    case ${run_process_type} in
-    SingleP) train_cmd="python tools/run_net.py ${train_config} ${train_options}" ;;
-    MultiP)
-        train_cmd="python tools/run_net.py ${train_config} ${train_options}" ;;
-    *) echo "choose run_mode(SingleP or MultiP)"; exit 1;
-    esac
+
+    nodes="${device_num:1:1}"
+
+    if [[ nodes -gt 1 ]];then
+        init_method="env://"
+        export MASTER_ADDR="$POD_0_IP"
+        export MASTER_PORT="$PORT"
+        export NNODES="$nodes"
+        export NODE_RANK="$PADDLE_TRAINER_ID"
+
+        echo "MASTER_ADDR="$POD_0_IP""
+        echo "MASTER_PORT="$PORT""
+        echo "NNODES="$nodes""
+        echo "NODE_RANK="$PADDLE_TRAINER_ID""
+        train_cmd="python tools/run_net.py --shard_id $NODE_RANK --num_shards $NNODES --init_method $init_method ${train_config} ${train_options}"
+    else
+        case ${run_process_type} in
+        SingleP) train_cmd="python tools/run_net.py ${train_config} ${train_options}" ;;
+        MultiP)
+            train_cmd="python tools/run_net.py ${train_config} ${train_options}" ;;
+        *) echo "choose run_mode(SingleP or MultiP)"; exit 1;
+        esac
+    fi
+
 #   以下为通用执行命令，无特殊可不用修改
     timeout 15m ${train_cmd} > ${log_file} 2>&1
     if [ $? -ne 0 ];then
