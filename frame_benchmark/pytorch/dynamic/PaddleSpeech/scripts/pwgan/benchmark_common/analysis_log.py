5c046138a89900143cb0b1b3af8cf38fffabd5cc
yuehuayingxueluo <867460659@qq.com>
2022-08-23 10:31:49 +0800

    Update analysis_log.py
    fix run_mode

--- frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/benchmark_common/analysis_log.py
+++ frame_benchmark/pytorch/dynamic/PaddleSpeech/scripts/pwgan/benchmark_common/analysis_log.py
@@ -11,7 +11,7 @@ import os
 def analyze(model_name, log_file, res_log_file, device_num):
     log_file_name = log_file.split("/")[-1]
     # model_name: pwgan_bs6_fp32_SingleP_DP
-    model_item, bs, fp_item, run_process_type, run_mode = model_name.split("_")
+    model_item, bs, fp_item, run_process_type, run_mode = "DP"
     gpu_num = 0
     ips = 0
     fail_flag = 0
