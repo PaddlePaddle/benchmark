2656dfef235d8e46692e59ebad8300c2dd18cbea
yuehuayingxueluo <867460659@qq.com>
2022-08-24 11:30:38 +0000

    fix fl_r50_fpn_1x_coco_bs8_fp32_DP.sh

--- frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/gfl_r50_fpn_1x_coco/N4C32/gfl_r50_fpn_1x_coco_bs8_fp32_DP.sh
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/gfl_r50_fpn_1x_coco/N4C32/gfl_r50_fpn_1x_coco_bs8_fp32_DP.sh
@@ -9,4 +9,18 @@ num_workers=2
 
 sed -i '/set\ -xe/d' run_benchmark.sh
 bash PrepareEnv.sh;
+
+# use this script to sync between different machines.
+cat <<EOF >tmp.py
+import torch
+flag = True
+while flag:
+    try:
+        torch.distributed.init_process_group(backend="nccl")
+        flag = False
+    except:
+        pass
+EOF
+python -m torch.distributed.run --nnodes=${node_num} --node_rank=${node_rank} --master_addr=${master_addr} --master_port=${master_port} --nproc_per_node=8 tmp.py
+
 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
