fb3a1b3d5f2e0604bc6440bca14a38ef56ce4161
GhostScreaming <mofengshenjieII@163.com>
2022-09-01 06:17:58 +0000

    Merge conflicts with upstream/develop branch.

--- frame_benchmark/pytorch/dynamic/PaddleGAN/scripts/edvr/N1C1/edvr_bs64_fp32_DP.sh
+++ frame_benchmark/pytorch/dynamic/PaddleGAN/scripts/edvr/N1C1/edvr_bs64_fp32_DP.sh
@@ -7,8 +7,4 @@ device_num=N1C1
 
 sed -i '/set\ -xe/d' run_benchmark.sh
 bash PrepareEnv.sh ${model_item};
-<<<<<<< HEAD
-bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} 2>&1;
-=======
 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} 2>&1;
->>>>>>> upstream/develop
