72ad8c370b191915d255ca74dd443f7cb89bc35d
yuehuayingxueluo <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>
2022-09-01 09:50:25 +0000

    fix A100 running bug

--- frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/fastscnn/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/fastscnn/benchmark_common/prepare.sh
@@ -10,20 +10,23 @@ CUDA_VERSION=`nvcc -run detect_cuda.cu | awk -F. '{print $1}'`
 WHEEL_URL_PREFIX="https://paddle-wheel.bj.bcebos.com/benchmark"
 apt-get install -y axel
 if [ `nvidia-smi --list-gpus | grep A100 | wc -l` -ne "0" ]; then
-  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
-  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget https://paddle-wheel.bj.bcebos.com/benchmark/mmcv_full-1.3.13-cp37-cp37m-linux_x86_64_A100.whl -O mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl
   pip install torch-1.9.1+cu111-cp37-cp37m-linux_x86_64.whl
   pip install torchvision-0.10.1+cu111-cp37-cp37m-linux_x86_64.whl
 else
-  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
-  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
+  wget "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
+  wget https://paddle-wheel.bj.bcebos.com/benchmark/mmcv_full-1.3.13-cp37-cp37m-linux_x86_64_V100.whl -O mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl
   pip install torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl
   pip install torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl
 fi
 
 #pip install torch==1.9.1 -i https://mirrors.ustc.edu.cn/pypi/web/simple
 #pip install torchvision==0.10.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install mmcv-full==1.3.13 -i https://pypi.tuna.tsinghua.edu.cn/simple
+#pip install mmcv-full==1.3.13 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt  -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -e .
 pip list
--- frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/ocrnet_hrnetw48/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/ocrnet_hrnetw48/benchmark_common/prepare.sh
@@ -11,13 +11,15 @@ CUDA_VERSION=`nvcc -run detect_cuda.cu | awk -F. '{print $1}'`
 WHEEL_URL_PREFIX="https://paddle-wheel.bj.bcebos.com/benchmark"
 apt-get install -y axel
 if [ `nvidia-smi --list-gpus | grep A100 | wc -l` -ne "0" ]; then
-  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
-  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torch-1.9.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torchvision-0.10.1%2Bcu111-cp37-cp37m-linux_x86_64.whl"
+  wget https://paddle-wheel.bj.bcebos.com/benchmark/mmcv_full-1.3.13-cp37-cp37m-linux_x86_64_A100.whl -O mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl
   pip install torch-1.9.1+cu111-cp37-cp37m-linux_x86_64.whl
   pip install torchvision-0.10.1+cu111-cp37-cp37m-linux_x86_64.whl
 else
-  axel -n 16 "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
-  axel -n 16 "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
+  wget "$WHEEL_URL_PREFIX/torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl" 
+  wget "$WHEEL_URL_PREFIX/torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl"
+  wget https://paddle-wheel.bj.bcebos.com/benchmark/mmcv_full-1.3.13-cp37-cp37m-linux_x86_64_V100.whl -O mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl
   pip install torch-1.9.1-cp37-cp37m-manylinux1_x86_64.whl
   pip install torchvision-0.10.1-cp37-cp37m-manylinux1_x86_64.whl
 fi
@@ -25,7 +27,8 @@ fi
 #pip install torch==1.9.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 #pip install torchvision==0.10.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 #pip install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.9.0/index.html
-pip install mmcv-full==1.3.13 -i https://pypi.tuna.tsinghua.edu.cn/simple
+#pip install mmcv-full==1.3.13 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install mmcv_full-1.3.13-cp37-cp37m-linux_x86_64.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt  -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -e .
 pip list
