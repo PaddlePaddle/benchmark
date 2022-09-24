b62392291df0e4262aa50be1e96c9922ac01d794
WenmuZhou <572459439@qq.com>
2022-09-24 00:50:49 +0000

    update torchvision install

--- frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_res18_db/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_res18_db/benchmark_common/PrepareEnv.sh
@@ -3,7 +3,7 @@
 ################################# 安装框架 如:
 wget -nc https://paddle-wheel.bj.bcebos.com/benchmark/torch-1.8.1%2Bcu111-cp37-cp37m-linux_x86_64.whl
 python -m pip install torch-1.8.1+cu111-cp37-cp37m-linux_x86_64.whl
-python -m pip install torchvision==0.9.1 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+python -m pip install torchvision==0.9.1 torchaudio==0.8.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
 python -m pip install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 # 下载数据集并解压
 rm -rf datasets
