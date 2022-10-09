b65d9bcd2c115a53c3dfc197ac81c23a131ba228
Liu Yiqun <liuyiqun01@baidu.com>
2022-10-09 21:17:16 +0800

    Update the script of gather.

--- api/tests/gather.py
+++ api/tests/gather.py
@@ -26,7 +26,7 @@ class GatherConfig(APIConfig):
         self.feed_spec = [
             {
                 "range": [8, 10]
-            },  # input
+            },  # x
             {
                 "range": [1, self.x_shape[self.axis]]
             }  # index
@@ -58,7 +58,7 @@ class TorchGather(PytorchOpBenchmarkBase):
         x = self.variable(name='x', shape=config.x_shape, dtype=config.x_dtype)
         index = self.variable(
             name='index', shape=config.index_shape, dtype="int64")
-        result = torch.index_select(input=x, index=index, dim=config.axis)
+        result = torch.index_select(x=x, index=index, dim=config.axis)
 
         self.feed_list = [x, index]
         self.fetch_list = [result]
