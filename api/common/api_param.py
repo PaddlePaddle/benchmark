5160b03b470add1ba9cf89813d641cc72e6dfa9f
zhangbopd <1299246947@qq.com>
2022-10-26 02:51:09 +0000

    unbranch loops

--- api/common/api_param.py
+++ api/common/api_param.py
@@ -239,21 +239,15 @@ class APIConfig(object):
         Convert all variables' dtype to float16.
         """
         for var in self.variable_list:
-            for name, value in vars(self).items():
-                if name.endswith("_dtype") and value not in [
-                        "float16", "int32", "int64"
-                ]:
-                    setattr(self, name, "float16")
-            if var.type == "Variable":
-                var.dtype = "float16"
+            if var.type == "Variable" and var.dtype not in [
+                    "float16", "int32", "int64"
+            ]:
+                setattr(self, var.name + "_dtype", "float16")
             elif var.type == "list<Variable>":
+                # convert each list member in list<Variable> from fp32 into fp16 type
                 for i in range(len(var.dtype)):
-                    var.dtype[i] = "float16"
-                for name, value in vars(self).items():
-                    # convert each list member in list<Variable> from fp32 into fp16 type
-                    if name.endswith(
-                            "_dtype") and value not in ["int32", "int64"]:
-                        setattr(self, name, var.dtype)
+                    if var.dtype[i] not in ["float16", "int32", "int64"]:
+                        var.dtype[i] = "float16"
 
     def init_from_json(self, filename, config_id=0, unknown_dim=16):
         filename = self.alias_filename(filename)
