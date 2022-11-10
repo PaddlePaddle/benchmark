df980a4ec45589a9bf792254934d8511fbebdd05
201716010711 <3030364995@qq.com>
2022-11-10 02:50:25 +0000

    Add fp16 squared_l2_norm op benchmark modify7

---
+++ api/dynamic_tests_v2/squared_l2_norm.py
@@ -0,0 +1,56 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common_import import *
+
+import paddle
+from paddle import _C_ops, _legacy_C_ops
+from paddle.fluid.layer_helper import LayerHelper
+
+
+class SquaredL2NormConfig(APIConfig):
+    def __init__(self):
+        super(SquaredL2NormConfig, self).__init__("squared_l2_norm")
+        # abs belongs to activation op series which only has one parameter
+        # thus abs can reuse activation.json. 
+        self.alias_name = "squared_l2_norm"
+
+
+class PDSquaredL2Norm(PaddleDynamicAPIBenchmarkBase):
+    def build_graph(self, config):
+        x = self.variable(name="x", shape=config.x_shape, dtype=config.x_dtype)
+        result = _C_ops.squared_l2_norm(x)
+
+        self.feed_list = [x]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [x])
+
+
+class TorchSquaredL2Norm(PytorchAPIBenchmarkBase):
+    def build_graph(self, config):
+        x = self.variable(name='x', shape=config.x_shape, dtype=config.x_dtype)
+        result = torch.sum(torch.square(x=x))
+
+        self.feed_list = [x]
+        self.fetch_list = [result]
+        if config.backward:
+            self.append_gradients(result, [x])
+
+
+if __name__ == '__main__':
+    test_main(
+        pd_dy_obj=PDSquaredL2Norm(),
+        torch_obj=TorchSquaredL2Norm(),
+        config=SquaredL2NormConfig())
