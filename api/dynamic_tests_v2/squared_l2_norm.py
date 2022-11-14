28f6815c644905eb7a56401d8b2dc7597ac925ef
201716010711 <3030364995@qq.com>
2022-11-14 02:48:39 +0000

    Add fp16 squared_l2_norm op benchmark modify8

--- api/dynamic_tests_v2/squared_l2_norm.py
+++ api/dynamic_tests_v2/squared_l2_norm.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 from common_import import *
 
 import paddle
-from paddle import _C_ops, _legacy_C_ops
+from paddle import _C_ops
 from paddle.fluid.layer_helper import LayerHelper
 
 
