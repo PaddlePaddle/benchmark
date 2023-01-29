954d25c5786ba068864f1e46e2f8d145d6ccf108
gmm <38800877+mmglove@users.noreply.github.com>
2023-01-29 14:33:05 +0800

    [develop] fix MobileViT_small script (#1575)
    * docker_images.yaml
    
    * update bs
    
    * fix MobileViT_small
    
    ---------
    
    Co-authored-by: mmglove <gmm_email@baidu.com>

--- frame_benchmark/pytorch/dynamic/PaddleClas/scripts/MobileViT_small/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/MobileViT_small/benchmark_common/PrepareEnv.sh
@@ -4,6 +4,10 @@
 pip install -r requirements.txt
 pip install pyyaml
 
+# CUDA 11.1 安装高版本报cuda error
+pip install torch==1.8.2 torchvision==0.9.2 torchaudio==0.8.2 --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
+pip install -U opencv_python==3.4.10.37
+pip list
 dataset_url="https://paddle-imagenet-models-name.bj.bcebos.com/data/ImageNet1k/ILSVRC2012_val.tar"
 
 # prepare data
