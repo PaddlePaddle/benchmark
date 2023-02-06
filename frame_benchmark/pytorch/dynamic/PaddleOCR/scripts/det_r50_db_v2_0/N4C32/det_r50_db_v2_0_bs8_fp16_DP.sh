62ad62c298a8430c721b905203965bcafdb1ce41
zhoujun <572459439@qq.com>
2023-02-06 17:05:06 +0800

    add fp16 support of db (#1578)
    * add fp16 support of db
    
    * update gitmoduls
    
    * rm DBNet.pytorch
    
    * add DBNet_pytorch

---
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_r50_db_v2_0/N4C32/det_r50_db_v2_0_bs8_fp16_DP.sh
@@ -0,0 +1,13 @@
+model_item=det_r50_db_v2_0
+#max_token
+bs_item=8
+fp_item=fp16
+run_process_type=MultiP
+run_mode=DP
+device_num=N4C32
+max_iter=5
+num_workers=1
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_iter} ${num_workers} 2>&1;
