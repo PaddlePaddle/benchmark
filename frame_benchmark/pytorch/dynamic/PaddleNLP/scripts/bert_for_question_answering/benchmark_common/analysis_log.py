8fba5f3eeb95a783169cf3a7be26d7c0d2115ac6
yujun <573009727@qq.com>
2023-02-08 13:57:07 +0800

    add bert for qa benchmark | current only support eager mode

---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_for_question_answering/benchmark_common/analysis_log.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+
+import sys
+import json
+import os
+import pandas as pd
+
+def analyze(model_item, log_file, res_log_file, device_num, bs, fp_item, run_process_type):
+    seqlen = 512
+    df = pd.read_csv(log_file)
+    abs_latency = df.abs_latency.mean()
+    num_gpus = int(device_num[3:])
+    ips = num_gpus * 1000 * seqlen * bs / abs_latency
+    ips = round(ips, 2)
+    run_mode = "DP"
+    
+    model_name = model_item+"_"+"bs"+str(bs)+"_"+fp_item+"_"+run_mode
+    info = {
+                "model_branch": os.getenv('model_branch'),
+                "model_commit": os.getenv('model_commit'),
+                "model_name": model_name,
+                "batch_size": bs,
+                "fp_item": fp_item,
+                "run_process_type": run_process_type,
+                "run_mode": run_mode,
+                "convergence_value": 0,
+                "convergence_key": "",
+                "ips": ips,
+                "speed_unit": "tokens/s",
+                "device_num": device_num,
+                "model_run_time": os.getenv('model_run_time'),
+                "frame_commit": "",
+                "frame_version": os.getenv('frame_version'),
+    }
+    json_info = json.dumps(info)
+    print(json_info)
+    with open(res_log_file, "w") as of:
+        of.write(json_info)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 8:
+        print("Usage:" + sys.argv[0] + " model_item path/to/log/file path/to/res/log/file")
+        sys.exit()
+
+    model_item = sys.argv[1]
+    log_file = sys.argv[2]
+    res_log_file = sys.argv[3]
+    device_num = sys.argv[4]
+    bs  = int(sys.argv[5])
+    fp_item  = sys.argv[6]
+    run_process_type = sys.argv[7]
+    
+    analyze(model_item, log_file, res_log_file, device_num, bs, fp_item, run_process_type)
---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_for_question_answering/benchmark_common/prepare.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#set -xe
+echo "*******prepare benchmark***********"
+
+################################# 创建一些log目录,如:
+# export BENCHMARK_ROOT=/workspace   # 起容器的时候映射的目录  benchmark/OtherFrameworks/PyTorch/
+# github: https://github.com/pytorch/pytorch commit id: e525f433e15de1f16966901604a8c4c662828a8a
+run_env=$ROOT_DIR/run_env
+log_date=`date "+%Y.%m%d.%H%M%S"`
+
+unset https_proxy && unset http_proxy
+
+wget http://10.181.196.20:8000/downloads/torch_dev_whls.tar
+tar -xvf torch_dev_whls.tar
+pip install torch_dev_whls/*
+pip install transformers psutil astunparse pandas numpy scipy ninja pyyaml setuptools cmake typing_extensions six requests protobuf scikit-learn -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+mv torch torch_tmp
+sed -i '1848,1850d' benchmarks/dynamo/common.py
+sed -i '934,939d' benchmarks/dynamo/common.py
+sed -i '933 r code.txt' benchmarks/dynamo/common.py
+sed -i '330,361d' benchmarks/dynamo/huggingface.py
+
+echo "*******prepare benchmark end***********"
