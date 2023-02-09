df42a91974d2b3262c089367393b554751c9278e
root <root@bjyz-sys-gpu-kongming9.bjyz.baidu.com>
2023-02-09 07:39:46 +0000

    install torch whl according to FLAG_TORCH_WHL_URL

--- frame_benchmark/pytorch/dynamic/PaddleClas/scripts/ResNeXt50_32x4d/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/ResNeXt50_32x4d/benchmark_common/PrepareEnv.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 # install env
-pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+wget -c --no-proxy ${FLAG_TORCH_WHL_URL}
+tar_file_name=$(echo ${FLAG_TORCH_WHL_URL} | awk -F '/' '{print $NF}')
+dir_name=$(echo ${tar_file_name} | awk -F '.' '{print $1}')
+tar xf ${tar_file_name}
+rm -rf ${tar_file_name}
 
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+pip install ${dir_name}/*
 pip install -r requirements.txt
 
-dataset_url="https://paddle-wheel.bj.bcebos.com/benchmark/ILSVRC2012_w.tar"
-
 # prepare data
+dataset_url="https://paddle-wheel.bj.bcebos.com/benchmark/ILSVRC2012_w.tar"
 wget -c ${dataset_url} --no-proxy
 tar xf ILSVRC2012_w.tar
 rm -f ILSVRC2012_w.tar
