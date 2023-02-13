c2a7fa613d1aab6b4082ddab282086b774130405
yujun <573009727@qq.com>
2023-02-13 11:31:35 +0800

    add bert benchmark

---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/bert_for_question_answering/N1C8/bert_for_question_answering_bs32_fp16_DP.sh
@@ -0,0 +1,12 @@
+model_item=bert_for_question_answering
+bs_item=32
+fp_item=fp16
+run_process_type=MultiP
+run_mode=DP
+device_num=N1C8
+max_iter=-1
+num_workers=8
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash prepare.sh;
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_iter} ${num_workers} 2>&1;
