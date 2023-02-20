22db8f70bd0e7c6ff8ca868de71b144e542965b0
WenmuZhou <572459439@qq.com>
2023-02-20 11:26:56 +0000

    update prepare.sh for db

--- frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_r50_db_v2_0/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_r50_db_v2_0/benchmark_common/prepare.sh
@@ -4,6 +4,7 @@
 wget -nc ${FLAG_TORCH_WHL_URL}
 tar -xvf torch_dev_whls.tar
 pip install torch_dev_whls/*
+pip install -r requirement.txt
 # 下载数据集并解压
 rm -rf datasets
 wget -nc https://paddleocr.bj.bcebos.com/dygraph_v2.0/test/benchmark_train/datasets.tar
