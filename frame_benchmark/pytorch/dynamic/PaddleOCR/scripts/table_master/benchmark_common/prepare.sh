dcb320c70694585b44954d94205a54f68b51bf5d
WenmuZhou <572459439@qq.com>
2023-02-20 11:30:04 +0000

    update install cmd

--- frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/table_master/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/table_master/benchmark_common/prepare.sh
@@ -3,15 +3,15 @@
 ################################# 安装框架 如:
 wget -nc ${FLAG_TORCH_WHL_URL}
 tar -xvf torch_dev_whls.tar
-pip install torch_dev_whls/*
+python -m pip install torch_dev_whls/*
 
 cd ./mmdetection-2.11.0
-pip install -v -e .
+python -m pip install -v -e .
 cd ..
-pip install -v -e .
+python -m pip install -v -e .
 
 wget  -nc https://paddleocr.bj.bcebos.com/dataset/mmcv-1.3.4-py2.py3-none-any.whl
-pip install mmcv-1.3.4-py2.py3-none-any.whl
+python -m pip install mmcv-1.3.4-py2.py3-none-any.whl
 # 下载数据集并解压
 wget -nc -P ./train_data/ https://paddleocr.bj.bcebos.com/dataset/StructureLabel_val_500.tar --no-check-certificate
 cd ./train_data/ && tar xf StructureLabel_val_500.tar
