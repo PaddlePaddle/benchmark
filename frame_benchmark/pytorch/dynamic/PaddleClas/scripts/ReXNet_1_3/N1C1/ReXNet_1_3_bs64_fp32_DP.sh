39cea865a12d629b795be3a2e685315e3a736c28
zhangyubo0722 <zhangyubo0722@163.com>
2023-03-02 08:05:52 +0000

    support torch benchmark4

---
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/ReXNet_1_3/N1C1/ReXNet_1_3_bs64_fp32_DP.sh
@@ -0,0 +1,12 @@
+model_item=ReXNet_1_3
+bs_item=64
+fp_item=fp32
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C1
+max_epoch=1
+num_workers=12
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epoch} ${num_workers} 2>&1;
