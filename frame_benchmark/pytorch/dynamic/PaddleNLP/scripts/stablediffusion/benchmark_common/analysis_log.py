59aa215f3f62ada504c0f0d6dc68e0b8e8780ee1
yujun <573009727@qq.com>
2023-03-07 11:39:24 +0800

    remove run_process_type

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/stablediffusion/benchmark_common/analysis_log.py
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/stablediffusion/benchmark_common/analysis_log.py
@@ -29,7 +29,6 @@ def analyze(model_item, log_file, res_log_file, device_num, bs, fp_item, run_pro
                 "model_name": model_name,
                 "batch_size": bs,
                 "fp_item": fp_item,
-                "run_process_type": run_process_type,
                 "run_mode": run_mode,
                 "convergence_value": 0,
                 "convergence_key": "",
