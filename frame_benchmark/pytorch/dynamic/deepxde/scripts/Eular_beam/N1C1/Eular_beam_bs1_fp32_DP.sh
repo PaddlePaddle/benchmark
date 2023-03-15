74f4f8ee8c6b7fd3b1b66075a9d037628c111c40
xiaoguoguo626807 <100397923+xiaoguoguo626807@users.noreply.github.com>
2023-03-15 21:00:02 +0800

    add deepxde torch model (#1628)
    * add deepxde torch
    
    * add deepxde torch
    
    * add deepxde torch
    
    * modify path

---
+++ frame_benchmark/pytorch/dynamic/deepxde/scripts/Eular_beam/N1C1/Eular_beam_bs1_fp32_DP.sh
@@ -0,0 +1,10 @@
+model_item=Eular_beam
+bs_item=1
+fp_item=fp32
+run_mode=DP
+device_num=N1C1
+#prepare
+bash prepare.sh
+#run
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} 2>&1;
+sleep 10;
