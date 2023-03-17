697fd21ef57144dd67f682e62bc261db4d6431de
liu zhengxi <380185688@qq.com>
2023-03-17 14:17:33 +0800

    Add gpt_for_sequence_classification benchmark (#1617)
    * add submodule
    
    * add gpt and t5 competitor
    
    * update
    
    * rm t5
    
    * fix multiple cards
    
    * update
    
    * update

---
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/gpt_for_sequence_classification/N1C8/gpt_for_sequence_classification_bs8_fp32_DP.sh
@@ -0,0 +1,11 @@
+model_item=gpt_for_sequence_classification
+bs_item=8
+fp_item=fp32
+run_process_type=MultiP
+run_mode=DP
+device_num=N1C8
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+unset CUDA_VISIBLE_DEVICES
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} 2>&1;
