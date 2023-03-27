ebd4e92cd8b9c3f2f6c3e352a1055fe504b0716e
huangjun12 <2399845970@qq.com>
2023-03-27 09:50:28 +0800

    fix tsm bug (#1652)

--- frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/TSM/benchmark_common/opts.py
+++ frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/TSM/benchmark_common/opts.py
@@ -74,3 +74,4 @@ parser.add_argument('--temporal_pool', default=False, action="store_true", help=
 parser.add_argument('--non_local', default=False, action="store_true", help='add non local block')
 
 parser.add_argument('--dense_sample', default=False, action="store_true", help='use dense sample for video dataset')
+parser.add_argument('--torchcompile', default=False, action="store_true", help='use torch compile mode')
