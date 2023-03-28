85810b080dbb21c75ab13c82928c6834166e729d
huangjun12 <2399845970@qq.com>
2023-03-28 18:42:55 +0800

    add videoswin benchmark scripts (#1653)
    * add videoswin scripts
    
    * refine max epoch to 1, for time limit
    
    * add submodule
    
    * refine submodule
    
    * refine details
    
    * refine
    
    * readd submodule
    
    * re add submodule

---
+++ frame_benchmark/pytorch/dynamic/PaddleVideo/scripts/VideoSwin/N1C1/VideoSwin_bs1_fp16_DP.sh
@@ -0,0 +1,9 @@
+model_item=VideoSwin
+bs_item=1
+fp_item=fp16
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C1
+
+bash prepare.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} 2>&1;
