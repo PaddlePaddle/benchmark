a79b9aa32446688d986168efae9a571c5fd55b77
Tingquan Gao <35441050@qq.com>
2023-03-30 14:20:21 +0800

    set num_workers=24 (#1663)
    * set num_workers=24
    
    * fix batch_size to avoid oom

--- frame_benchmark/pytorch/dynamic/PaddleClas/scripts/DeiT_base_patch16_224/N1C1/DeiT_base_patch16_224_bs256_fp32_DP.sh
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/DeiT_base_patch16_224/N1C1/DeiT_base_patch16_224_bs256_fp32_DP.sh
@@ -1,11 +1,11 @@
 model_item=DeiT_base_patch16_224
-bs_item=256
+bs_item=128
 fp_item=fp32
 run_process_type=SingleP
 run_mode=DP
 device_num=N1C1
 max_epoch=1
-num_workers=12
+num_workers=24
 
 sed -i '/set\ -xe/d' run_benchmark.sh
 bash PrepareEnv.sh;
