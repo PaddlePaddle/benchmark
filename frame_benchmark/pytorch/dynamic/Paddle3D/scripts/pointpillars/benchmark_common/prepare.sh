5c1a09b561e56e18f3c05550665e317fdeeb9981
Stinky-Tofu <StinkyTofu95@gmail.com>
2023-04-06 09:11:19 +0000

    acclerate package download

--- frame_benchmark/pytorch/dynamic/Paddle3D/scripts/pointpillars/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/Paddle3D/scripts/pointpillars/benchmark_common/prepare.sh
@@ -11,14 +11,16 @@ unset https_proxy && unset http_proxy
 if [ ! -f "torch_dev_whls.tar" ];then
   wget ${FLAG_TORCH_WHL_URL}
 fi
+export https_proxy=${HTTP_PRO} && export http_proxy=${HTTPS_PRO}
 tar -xf torch_dev_whls.tar
 for whl_file in torch_dev_whls/*
 do
   pip install ${whl_file}
 done
+pip install kornia
+unset https_proxy && unset http_proxy
 pip install spconv-cu117	
 python setup.py develop
-unset https_proxy && unset http_proxy
 
 
 # 由于kitti数据集太大，为避免每次下载过于耗时，请将kitti数据集下载后，软链到/data/Dataset/kitti
