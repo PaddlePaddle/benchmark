d32a4108a7ea86b7e177917cd48f94e292afbbbb
Tingquan Gao <35441050@qq.com>
2023-05-10 21:06:44 +0800

    add r50 nhwc (#1672)
    * add r50 nhwc
    
    * fix num_workers to 16
    
    * rename to ResNet50_NHWC
    
    * rm useless field channel_last
    
    * add r50 bs256 workers16
    
    * fix run_process_type

---
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/ResNet50_NHWC/N1C1/ResNet50_NHWC_bs256_fp16_DP.sh
@@ -0,0 +1,12 @@
+model_item=ResNet50_NHWC
+bs_item=256
+fp_item=fp16
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C1
+max_epoch=1
+num_workers=16
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epoch} ${num_workers} 2>&1;
