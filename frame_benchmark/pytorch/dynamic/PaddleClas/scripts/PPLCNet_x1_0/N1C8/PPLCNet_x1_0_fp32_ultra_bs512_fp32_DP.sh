fbf7859c217bfd14ba36c311b394feb6b0540cbc
root <root@bjyz-sys-gpu-kongming9.bjyz.baidu.com>
2023-05-18 08:44:25 +0000

    add fp32 and ampo2 ultra configs

---
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/PPLCNet_x1_0/N1C8/PPLCNet_x1_0_fp32_ultra_bs512_fp32_DP.sh
@@ -0,0 +1,12 @@
+model_item=PPLCNet_x1_0_fp32_ultra
+bs_item=512
+fp_item=fp32
+run_process_type=SingleP
+run_mode=DP
+device_num=N1C8
+max_epoch=1
+num_workers=16
+
+sed -i '/set\ -xe/d' run_benchmark.sh
+bash PrepareEnv.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epoch} ${num_workers} 2>&1;
