2b948f20859f9621dd0b281fdbf5a2d4caa0acac
gaotingquan <gaotingquan@baidu.com>
2023-06-28 09:14:12 +0000

    set http proxy

--- frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/benchmark_common/PrepareEnv.sh
@@ -16,13 +16,13 @@ pip install ${dir_name}/*
 pip install -r requirements.txt
 
 rm -rf datasets
-wget -nc -P datasets/coco128/  https://paddledet.bj.bcebos.com/data/tipc/cocomini_yolov5_yolov7.tar
+wget --no-proxy -nc -P datasets/coco128/  https://paddledet.bj.bcebos.com/data/tipc/cocomini_yolov5_yolov7.tar
 cd ./datasets/coco128/ && tar -xf cocomini_yolov5_yolov7.tar
 mv cocomini_yolov5_yolov7/* ./
 cd .. && cd ..
 rm -rf ./datasets/coco128/labels/*.cache
 rm -rf ./runs
-wget https://paddledet.bj.bcebos.com/data/tipc/Arial.ttf
+wget --no-proxy https://paddledet.bj.bcebos.com/data/tipc/Arial.ttf
 mkdir -p /root/.config/Ultralytics/
 mv Arial.ttf /root/.config/Ultralytics/
 
