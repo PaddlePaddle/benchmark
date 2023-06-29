1b3d60a1bc884e74fea545d7b2f15fa0971e8d7c
Tingquan Gao <35441050@qq.com>
2023-06-29 10:08:51 +0800

    debug (#1707)
    * fix bug to adapt to torch 2.0
    
    * set http proxy
    
    * fix file name

--- frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/N1C1/yolov5_l_300e_coco_bs16_fp32_DP.sh
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/N1C1/yolov5_l_300e_coco_bs16_fp32_DP.sh
@@ -9,4 +9,8 @@ num_workers=4
 
 sed -i '/set\ -xe/d' run_benchmark.sh
 bash PrepareEnv.sh;
+export http_proxy=${HTTP_PRO}
+export https_proxy=${HTTPS_PRO}
 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
+unset http_proxy
+unset https_proxy
--- frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/yolov5_l_300e_coco/benchmark_common/PrepareEnv.sh
@@ -16,13 +16,17 @@ pip install ${dir_name}/*
 pip install -r requirements.txt
 
 rm -rf datasets
-wget -nc -P datasets/coco128/  https://paddledet.bj.bcebos.com/data/tipc/cocomini_yolov5_yolov7.tar
+wget --no-proxy -nc -P datasets/coco128/  https://paddledet.bj.bcebos.com/data/tipc/cocomini_yolov5_yolov7.tar
 cd ./datasets/coco128/ && tar -xf cocomini_yolov5_yolov7.tar
 mv cocomini_yolov5_yolov7/* ./
 cd .. && cd ..
 rm -rf ./datasets/coco128/labels/*.cache
 rm -rf ./runs
-wget https://paddledet.bj.bcebos.com/data/tipc/Arial.ttf
+wget --no-proxy https://paddledet.bj.bcebos.com/data/tipc/Arial.ttf
 mkdir -p /root/.config/Ultralytics/
 mv Arial.ttf /root/.config/Ultralytics/
+
+# fix bug to adapt to torch 2.0
+sed -i 's/local_rank/local-rank/g' train.py
+
 echo "*******prepare benchmark end***********"
