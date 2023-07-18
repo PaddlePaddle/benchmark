125010b710c30ff0b80fb2d1edbea873ca04b335
Tingquan Gao <35441050@qq.com>
2023-07-18 20:45:47 +0800

    fix workers num (#1718)

--- frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/segformer_b0/N1C8/segformer_b0_bs4_fp16_DP.sh
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/segformer_b0/N1C8/segformer_b0_bs4_fp16_DP.sh
@@ -4,7 +4,7 @@ fp_item=fp16
 run_mode=DP
 device_num=N1C8
 max_iter=500
-num_workers=5
+num_workers=8
 
 bash prepare.sh;
 bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} ${max_iter} ${num_workers} 2>&1;
