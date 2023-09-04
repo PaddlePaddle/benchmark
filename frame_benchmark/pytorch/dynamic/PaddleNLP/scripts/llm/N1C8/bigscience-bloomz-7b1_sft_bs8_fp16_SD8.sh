762e8c55561ad6e2e924e5fc5074747d00502f97
w5688414 <w5688414@gmail.com>
2023-09-05 07:31:46 +0800

    Update sft config

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C8/bigscience-bloomz-7b1_sft_bs8_fp16_SD8.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C8/bigscience-bloomz-7b1_sft_bs8_fp16_SD8.sh
@@ -1,5 +1,5 @@
 model_item="bigscience-bloomz-7b1_sft"  
-base_batch_size=8    
+base_batch_size=2    
 fp_item="fp16"    
 run_mode="SD8"    
 device_num="N1C8"
@@ -9,9 +9,9 @@ max_length=2048
 dataset_name_or_path="llm_benchmark_zh"
 learning_rate="3e-05"
 gradient_checkpointing="true"
-gradient_accumulation_steps=5
-num_train_epochs=5
+gradient_accumulation_steps=2
+num_train_epochs=2
 bash prepare.sh
 bash bloom-7b1.sh
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-CUDA_VISIBLE_DEVICES=0 bash run_benchmark.sh ${model_item} ${base_batch_size} ${fp_item} ${run_mode} ${device_num} ${model_name_or_path} ${lora} ${max_length} ${dataset_name_or_path} ${learning_rate} ${gradient_checkpointing} ${gradient_accumulation_steps} ${num_train_epochs} 2>&1;
+bash run_benchmark.sh ${model_item} ${base_batch_size} ${fp_item} ${run_mode} ${device_num} ${model_name_or_path} ${lora} ${max_length} ${dataset_name_or_path} ${learning_rate} ${gradient_checkpointing} ${gradient_accumulation_steps} ${num_train_epochs} 2>&1;
