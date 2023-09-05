519bccda89e0b6e02903f3fa4b16bd85f6ff1411
w5688414 <w5688414@gmail.com>
2023-09-05 08:34:25 +0800

    Adjust chatglm sft parameters

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C8/THUDM-chatglm-6b_sft_bs8_fp16_SD8.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C8/THUDM-chatglm-6b_sft_bs8_fp16_SD8.sh
@@ -1,5 +1,5 @@
 model_item="THUDM-chatglm-6b_sft"  
-base_batch_size=8    
+base_batch_size=2    
 fp_item="fp16"    
 run_mode="SD8"    
 device_num="N1C8"
@@ -9,8 +9,8 @@ max_length=2048
 dataset_name_or_path="llm_benchmark_zh"
 learning_rate="3e-05"
 gradient_checkpointing="true"
-gradient_accumulation_steps=5
-num_train_epochs=5
+gradient_accumulation_steps=2
+num_train_epochs=2
 bash prepare.sh
 bash chatglm-6b.sh
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
