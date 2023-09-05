837b25459e480eab3b0376f7a82879d44c8c9c71
w5688414 <w5688414@gmail.com>
2023-09-05 08:11:51 +0800

    Update chatglm config

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/benchmark_common/chatglm-6b.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/benchmark_common/chatglm-6b.sh
@@ -8,3 +8,8 @@ wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/pyt
 wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/pytorch_model-00002-of-00002.bin
 wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/pytorch_model.bin.index.json
 wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/special_tokens_map.json
+wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/tokenizer_config.json
+wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/tokenization_chatglm.py
+wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/quantization.py
+wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/modeling_chatglm.py
+wget https://paddlenlp.bj.bcebos.com/models/community/THUDM/chatglm-6b/torch/configuration_chatglm.py
