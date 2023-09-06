418798aeb182a2a32756e84ba8c59b281e453045
DesmonDay <908660116@qq.com>
2023-09-06 20:48:55 +0800

    decrease bs

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C1/facebook-llama-7b_lora_bs8_fp16_DP1.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llm/N1C1/facebook-llama-7b_lora_bs8_fp16_DP1.sh
@@ -1,17 +1,17 @@
-model_item="facebook-llama-13b_lora"  
-base_batch_size=8 
+model_item="facebook-llama-7b_lora"  
+base_batch_size=8
 fp_item="fp16"    
 run_mode="DP1"    
 device_num="N1C1"
-model_name_or_path="huggyllama/llama-13b"
+model_name_or_path="./huggyllama/llama-7b"
 lora="1"
 max_length=2048
 dataset_name_or_path="llm_benchmark_en"
 learning_rate="3e-04"
 gradient_checkpointing="1"
+gradient_accumulation_steps=4
 num_train_epochs=2
-gradient_accumulation_steps = 1
 bash prepare.sh
-bash llama-13b.sh
+bash llama-7b.sh
 export CUDA_VISIBLE_DEVICES=0
 bash run_benchmark.sh ${model_item} ${base_batch_size} ${fp_item} ${run_mode} ${device_num} ${model_name_or_path} ${lora} ${max_length} ${dataset_name_or_path} ${learning_rate} ${gradient_checkpointing} ${gradient_accumulation_steps} ${num_train_epochs} 2>&1;
