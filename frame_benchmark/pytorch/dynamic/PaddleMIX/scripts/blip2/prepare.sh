990409c8eaeebf25b3962cc0ed0d8a6aaf4eb906
wjm202 <897383984@qq.com>
2023-09-12 17:46:43 +0800

    add blip2 torch benchmark

---
+++ frame_benchmark/pytorch/dynamic/PaddleMIX/scripts/blip2/prepare.sh
@@ -0,0 +1,10 @@
+
+rm -rf /export/home/.cache/lavis/coco/
+# dataset
+wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/benchmark/blip2/coco.tar.gz
+tar -zxvf coco.tar.gz
+mv coco /export/home/.cache/lavis/
+rm -rf coco
+rm -rf coco.tar.gz
+# env
+# pip install -r scripts/blip2/requirements.txt
