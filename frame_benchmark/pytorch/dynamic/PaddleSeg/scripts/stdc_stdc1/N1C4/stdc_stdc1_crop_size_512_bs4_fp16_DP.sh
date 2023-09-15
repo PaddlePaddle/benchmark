72866867192362a60d53ee8094e1a789f9b567fd
zhouchangda@baidu.com <zhouchangda@baidu.com>
2023-09-15 14:42:57 +0800

    add crop size of 512 for seg in pytorch

---
+++ frame_benchmark/pytorch/dynamic/PaddleSeg/scripts/stdc_stdc1/N1C4/stdc_stdc1_crop_size_512_bs4_fp16_DP.sh
@@ -0,0 +1,11 @@
+model_item="stdc_stdc1"
+bs_item=4
+fp_item=fp16
+run_mode=DP
+device_num=N1C4
+max_iter=400
+num_workers=24
+crop_size=512
+
+bash prepare.sh;
+bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} ${max_iter} ${num_workers} ${crop_size} 2>&1;
