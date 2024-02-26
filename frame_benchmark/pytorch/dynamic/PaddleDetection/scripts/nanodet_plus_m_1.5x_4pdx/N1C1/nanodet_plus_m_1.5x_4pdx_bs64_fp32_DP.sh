845f9c97360a82d5c609c48a9cf3a3f8fd06a5f4
Bobholamovic <mhlin425@whu.edu.cn>
2024-02-26 15:24:05 +0800

    Add nanodet_plus_m_1.5x_4pdx

---
+++ frame_benchmark/pytorch/dynamic/PaddleDetection/scripts/nanodet_plus_m_1.5x_4pdx/N1C1/nanodet_plus_m_1.5x_4pdx_bs64_fp32_DP.sh
@@ -0,0 +1,22 @@
+model_item='nanodet_plus_m_1.5x_4pdx'
+bs_item=64
+fp_item='fp32'
+run_process_type='SingleP'
+run_mode='DP'
+device_num='N1C1'
+max_epochs=1
+num_workers=6
+repeats=24
+
+bash PrepareEnv.sh
+bash repeat_data.sh "${repeats}"
+bash run_benchmark.sh \
+    "${model_item}" \
+    "${bs_item}" \
+    "${fp_item}" \
+    "${run_process_type}" \
+    "${run_mode}" \
+    "${device_num}" \
+    "${max_epochs}" \
+    "${num_workers}" \
+    2>&1
