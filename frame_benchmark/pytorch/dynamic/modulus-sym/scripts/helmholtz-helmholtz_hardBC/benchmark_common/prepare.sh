c2f955c323c1a98cf66fe71179fe2b6f951bddd5
HydrogenSulfate <490868991@qq.com>
2024-04-13 03:46:33 +0000

    download dataset int prepare.sh

--- frame_benchmark/pytorch/dynamic/modulus-sym/scripts/helmholtz-helmholtz_hardBC/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/modulus-sym/scripts/helmholtz-helmholtz_hardBC/benchmark_common/prepare.sh
@@ -8,3 +8,11 @@ popd
 
 # install modulus-sym
 pip install -e .
+if [ ! -f './examples_sym.zip' ]; then
+    wget https://paddle-org.bj.bcebos.com/paddlescience/datasets/modulus/examples_sym.zip
+fi
+
+if [ ! -d './examples_sym' ]; then
+    unzip examples_sym.zip
+    \cp -r -f -v ./examples_sym/examples/* ./examples/
+fi
