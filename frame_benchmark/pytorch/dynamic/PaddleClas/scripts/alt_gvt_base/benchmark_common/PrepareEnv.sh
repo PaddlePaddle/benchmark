05e1c4550d5746a877cf5bd418a5951b29990f5f
gmm <38800877+mmglove@users.noreply.github.com>
2024-06-28 14:08:41 +0800

    【develop】 fix prepare.sh for clas  (#1764)
    * fix scripts
    
    * update darcy prepare;
    
    * update darcy prepare;
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * update modulus-sym
    
    * Revert "update modulus-sym"
    
    This reverts commit 74a4dc20efd0a63e7e7a3a8ee32f499fee7f4801.
    
    * update models/modulus-sym
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * fix torch23 for clas

--- frame_benchmark/pytorch/dynamic/PaddleClas/scripts/alt_gvt_base/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleClas/scripts/alt_gvt_base/benchmark_common/PrepareEnv.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # install env
-pip install torch torchvision
-pip install timm==0.4.5
+wget -c --no-proxy ${FLAG_TORCH_WHL_URL}
+tar_file_name=$(echo ${FLAG_TORCH_WHL_URL} | awk -F '/' '{print $NF}')
+dir_name=$(echo ${tar_file_name} | awk -F '.' '{print $1}')
+tar xf ${tar_file_name}
+rm -rf ${tar_file_name}
 
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+pip install ${dir_name}/*
+pip install -r requirements.txt
+pip install timm==0.4.5
+# fix bug to adapt to torch 2.0
+sed -i 's/local_rank/local-rank/g' train.py
 dataset_url="https://paddle-imagenet-models-name.bj.bcebos.com/data/ImageNet1k/ILSVRC2012_val.tar"
 
 # prepare data
