274bd7e45aa63f48500e74ba056b49a46c4f5a62
gmm <38800877+mmglove@users.noreply.github.com>
2024-07-05 18:24:54 +0800

    Update PrepareEnv.sh

--- frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_res18_db/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/det_res18_db/benchmark_common/PrepareEnv.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 # 执行路径在模型库的根目录下
 ################################# 安装框架 如:
-wget -nc https://paddle-wheel.bj.bcebos.com/benchmark/torch-1.8.1%2Bcu111-cp37-cp37m-linux_x86_64.whl
-python -m pip install torch-1.8.1+cu111-cp37-cp37m-linux_x86_64.whl
-python -m pip install torchvision==0.9.1 torchaudio==0.8.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+# install env
+wget -c -q --no-proxy ${FLAG_TORCH_WHL_URL}
+tar_file_name=$(echo ${FLAG_TORCH_WHL_URL} | awk -F '/' '{print $NF}')
+dir_name=$(echo ${tar_file_name} | awk -F '.' '{print $1}')
+tar xf ${tar_file_name}
+rm -rf ${tar_file_name}
+python -m pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+python -m pip install ${dir_name}/*
+
+python -m pip install -U openmim
+mim install mmcv
+
 python -m pip install -r requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 # 下载数据集并解压
 rm -rf datasets
