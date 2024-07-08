3e6e3023617913a684650291f58d99f10b80086f
gmm <38800877+mmglove@users.noreply.github.com>
2024-07-08 14:13:02 +0800

    【Develop】 fix crnn_4pdx_bs256_fp32_DP (#1766)
    * fix scripts
    
    * update darcy prepare;
    
    * update darcy prepare;
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * update modulus-sym
    
    * Revert "update modulus-sym"
    
    This reverts commit 74a4dc20efd0a63e7e7a3a8ee32f499fee7f4801.
    
    * update models/modulus-sym
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * Update prepare.sh
    
    * fix torch23 for clas
    
    * fix BNet_pytorch_det_r50_db_v2_0
    
    * Update PrepareEnv.sh
    
    * fix crnn_4pdx

--- frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/crnn_4pdx/benchmark_common/PrepareEnv.sh
+++ frame_benchmark/pytorch/dynamic/PaddleOCR/scripts/crnn_4pdx/benchmark_common/PrepareEnv.sh
@@ -7,8 +7,19 @@ echo "*******prepare benchmark start ***********"
 pip install -U pip
 echo `pip --version`
 
-pip install torch==2.2.0 torchvision==0.17.0
-pip install lmdb pillow torchvision nltk natsort fire
+#pip install torch==2.2.0 torchvision==0.17.0
+# install env
+wget -c --no-proxy ${FLAG_TORCH_WHL_URL}
+tar_file_name=$(echo ${FLAG_TORCH_WHL_URL} | awk -F '/' '{print $NF}')
+dir_name=$(echo ${tar_file_name} | awk -F '.' '{print $1}')
+tar xf ${tar_file_name}
+rm -rf ${tar_file_name}
+
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+pip install ${dir_name}/*
+
+pip install --upgrade setuptools  # 避免安装 fire 失败
+pip install lmdb pillow nltk natsort fire
 
 rm -rf ./datasets/ocr_rec_dataset_examples/
 wget -nc -P ./datasets/ https://paddle-model-ecology.bj.bcebos.com/uapi/data/ocr_rec_dataset_examples.tar --no-check-certificate
