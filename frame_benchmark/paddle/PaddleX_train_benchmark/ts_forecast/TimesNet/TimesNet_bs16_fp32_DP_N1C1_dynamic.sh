d3f2fb130740293059c772e90979e800ef9728c5
mmglove <guomengmeng01@baidu.com>
2024-08-08 14:16:32 +0800

    update ts_forecast script

--- frame_benchmark/paddle/PaddleX_train_benchmark/ts_forecast/TimesNet/TimesNet_bs16_fp32_DP_N1C1_dynamic.sh
+++ frame_benchmark/paddle/PaddleX_train_benchmark/ts_forecast/TimesNet/TimesNet_bs16_fp32_DP_N1C1_dynamic.sh
@@ -9,15 +9,15 @@ export run_mode=DP
 export device_num=N1C1
 export model_name=TimesNet_bs16_fp32_DP
 export job_name=PaddleX_ts_forecast_TimesNet_bs16_fp32_DP_N1C1
-export skip_steps=10
+export skip_steps=5
 export keyword="ips: "
 export convergence_key="loss: "
-export speed_unit="samples/s"
+export speed_unit="sequences/sec"
 export log_file=${TRAIN_LOG_DIR:-$PWD}/${job_name}_log
 export speed_log_file=${LOG_PATH_INDEX_DIR:-$PWD}/${job_name}_speed
 export distributed_train_logs=${MODEL_REPO_ROOT:-$PWD}/output/${job_name}/distributed_train_logs
 
-train_cmd="python main.py -c paddlex/configs/ts_forecast/TimesNet.yaml -o Global.device=gpu:${CUDA_VISIBLE_DEVICES} -o Global.mode=train -o Global.dataset_dir=dataset/ts_forecast_train_benchmark -o Global.output=output/${job_name} -o Train.batch_size=16 -o Train.epochs_iters=15"
+train_cmd="python main.py -c paddlex/configs/ts_forecast/TimesNet.yaml -o Global.device=gpu:${CUDA_VISIBLE_DEVICES} -o Global.mode=train -o Global.dataset_dir=dataset/ts_forecast_train_benchmark -o Global.output=output/${job_name} -o Train.batch_size=16 -o Train.patience=30 -o Train.epochs_iters=30"
 
 echo $train_cmd
 timeout 5m $train_cmd > $log_file 2>&1
