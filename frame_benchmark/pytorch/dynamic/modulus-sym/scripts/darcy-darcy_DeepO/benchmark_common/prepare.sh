308c8dcc3f1807168690ea6914037b6bb3b12b6d
mmglove <guomengmeng01@baidu.com>
2024-11-07 16:04:16 +0800

    update modulus-sym for torch250

--- frame_benchmark/pytorch/dynamic/modulus-sym/scripts/darcy-darcy_DeepO/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/modulus-sym/scripts/darcy-darcy_DeepO/benchmark_common/prepare.sh
@@ -1,3 +1,11 @@
+# install pytorch
+# pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/cu118
+
+wget -c --no-proxy ${FLAG_TORCH_WHL_URL}
+tar_file_name=$(echo ${FLAG_TORCH_WHL_URL} | awk -F '/' '{print $NF}')
+dir_name=$(echo ${tar_file_name} | awk -F '.' '{print $1}')
+tar xf ${tar_file_name}
+rm -rf ${tar_file_name}
 
 # install modulus
 pushd ../modulus/
@@ -6,10 +14,6 @@ popd
 
 # install modulus-sym
 pip install -e .
-
-# install pytorch
-pip install https://paddle-qa.bj.bcebos.com/benchmark/pretrained/torch-2.0.1%2Bcu118-cp310-cp310-linux_x86_64.whl
-
 if [ ! -f './examples_sym.zip' ]; then
     wget https://paddle-org.bj.bcebos.com/paddlescience/datasets/modulus/examples_sym.zip
 fi
@@ -20,9 +24,3 @@ fi
 unalias cp 2>/dev/null
 \cp -r -f -v ./examples_sym/examples/* ./examples/
 
-if [ ! -d './examples/darcy/datasets' ]; then
-    mkdir -p ./examples/darcy/datasets && cd ./examples/darcy/datasets
-    wget https://paddle-qa.bj.bcebos.com/benchmark/pretrained/Darcy_241.tar.gz
-    tar xf Darcy_241.tar.gz
-    cd -
-fi
