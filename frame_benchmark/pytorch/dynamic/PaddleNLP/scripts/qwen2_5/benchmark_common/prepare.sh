4f01fd72ad09cc1d1012b43a6aef80327d30799c
liujie44 <liujie44@baidu.com>
2024-12-11 11:22:30 +0800

    fix

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/qwen2_5/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/qwen2_5/benchmark_common/prepare.sh
@@ -6,18 +6,22 @@ echo "*******prepare benchmark***********"
 # export BENCHMARK_ROOT=/workspace   # 起容器的时候映射的目录  benchmark/OtherFrameworks/PyTorch/
 
 
-unset https_proxy && unset http_proxy
+export http_proxy=${HTTP_PRO}
+export https_proxy=${HTTPS_PRO}
 
 source /opt/torch_native_venv/bin/activate
-echo $pwd
+python -m pip install -U pip
+python -m pip install setuptools==61.0 --force-reinstall
+echo $PWD
+mv -v data data_bak
 python -m pip install -e .
-pip install deepspeed
-pip install modelscope
+python -m pip install deepspeed
+python -m pip install modelscope
 
-python -c "from modelscope import snapshot_download \
-            model_dir = snapshot_download('Qwen/Qwen2.5-7B'))"
-python -c "from modelscope import snapshot_download \
-            model_dir = snapshot_download('Qwen/Qwen2.5-14B'))"
+python -c "from modelscope import snapshot_download; \
+            model_dir = snapshot_download('Qwen/Qwen2.5-7B')"
+python -c "from modelscope import snapshot_download; \
+            model_dir = snapshot_download('Qwen/Qwen2.5-14B')"
 
 wget https://paddlenlp.bj.bcebos.com/llm_benchmark_data/qwen2_5_llamafactory_data.tar.gz
 tar zxvf qwen2_5_llamafactory_data.tar.gz && rm -rf qwen2_5_llamafactory_data.tar.gz
