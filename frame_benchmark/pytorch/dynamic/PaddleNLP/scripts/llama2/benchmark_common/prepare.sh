db1ae6bf68f22bd7f44207300bd71ee0eca9352f
liujie44 <liujie44@baidu.com>
2024-12-20 17:19:40 +0800

    update model_name_or_path

--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llama2/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/llama2/benchmark_common/prepare.sh
@@ -10,20 +10,78 @@ export PATH=/opt/torch_native_venv/bin:${PATH}
 export LD_LIBRARY_PATH=/home/opt/nvidia_lib:$LD_LIBRARY_PATH
 
 echo $PWD
-pip config set global.index-url http://pip.baidu.com/root/baidu/+simple/
-pip config set install.trusted-host  pip.baidu.com
+global.index-url='https://pip.baidu-int.com/simple/'
 python -m pip install -U pip
-python -m pip install torch==2.3.1 --extra-index https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 python -m pip install setuptools==61.0 --force-reinstall
+python -m pip install torch==2.3.1
 python -m pip install -e .
 python -m pip install deepspeed==0.14.2
 python -m pip install modelscope
 
-export http_proxy=${HTTP_PRO}
-export https_proxy=${HTTPS_PRO}
-model_name_or_path=${1:-"Qwen/Qwen2.5-1.5B"}
-python -c "from modelscope import snapshot_download;model_dir = snapshot_download('${model_name_or_path}')"
-unset http_proxy && unset https_proxy
+model_name_or_path=${1:-"meta-llama/Llama-2-7b-hf"}
+mkdir -p /opt/${model_name_or_path} && cd /opt/${model_name_or_path}
+case ${model_name_or_path} in
+meta-llama/Llama-2-7b-hf)
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/USE_POLICY.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/model-00001-of-00002.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/model-00002-of-00002.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/special_tokens_map.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/tokenizer.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/tokenizer.model
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-7b-hf/tokenizer_config.json
+    echo "download models for meta-llama/Llama-2-7b-hf done" ;;
+meta-llama/Llama-2-13b-hf)
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/LICENSE.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/Responsible-Use-Guide.pdf
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/USE_POLICY.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/model-00001-of-00003.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/model-00002-of-00003.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/model-00003-of-00003.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/special_tokens_map.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/tokenizer.model
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-13b-hf/tokenizer_config.json
+    echo "download models for meta-llama/Llama-2-13b-hf done" ;;
+meta-llama/Llama-2-70b-hf)
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/LICENSE.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/Responsible-Use-Guide.pdf
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/USE_POLICY.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/llama_updates.patch
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00001-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00002-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00003-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00004-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00005-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00006-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00007-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00008-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00009-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00010-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00011-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00012-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00013-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00014-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model-00015-of-00015.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/special_tokens_map.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/tokenizer.model
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/meta-llama/Llama-2-70b-hf/tokenizer_config.json
+    echo "download models for meta-llama/Llama-2-70b-hf done" ;;
+*) echo "no ${model_name_or_path}"; exit 1;
+esac
+
 
 mv -v data data_bak
 wget https://paddlenlp.bj.bcebos.com/llm_benchmark_data/llamafactory_data.tar.gz
--- frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/qwen2_5/benchmark_common/prepare.sh
+++ frame_benchmark/pytorch/dynamic/PaddleNLP/scripts/qwen2_5/benchmark_common/prepare.sh
@@ -10,20 +10,124 @@ export PATH=/opt/torch_native_venv/bin:${PATH}
 export LD_LIBRARY_PATH=/home/opt/nvidia_lib:$LD_LIBRARY_PATH
 
 echo $PWD
-pip config set global.index-url http://pip.baidu.com/root/baidu/+simple/
-pip config set install.trusted-host  pip.baidu.com
+global.index-url='https://pip.baidu-int.com/simple/'
 python -m pip install -U pip
-python -m pip install torch==2.3.1 --extra-index https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 python -m pip install setuptools==61.0 --force-reinstall
+python -m pip install torch==2.3.1
 python -m pip install -e .
 python -m pip install deepspeed==0.14.2
 python -m pip install modelscope
 
-export http_proxy=${HTTP_PRO}
-export https_proxy=${HTTPS_PRO}
 model_name_or_path=${1:-"Qwen/Qwen2.5-1.5B"}
-python -c "from modelscope import snapshot_download;model_dir = snapshot_download('${model_name_or_path}')"
-unset http_proxy && unset https_proxy
+mkdir -p /opt/${model_name_or_path} && cd /opt/${model_name_or_path}
+case ${model_name_or_path} in
+Qwen/Qwen2.5-1.5B)  
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/LICENSE
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/merges.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/model.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/tokenizer.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/tokenizer_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-1.5B/vocab.json
+    echo "download models for Qwen/Qwen2.5-1.5B done" ;;
+Qwen/Qwen2.5-7B) 
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/LICENSE
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/merges.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/model-00001-of-00004.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/model-00002-of-00004.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/model-00003-of-00004.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/model-00004-of-00004.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/tokenizer.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/tokenizer_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-7B/vocab.json
+    echo "download models for Qwen/Qwen2.5-7B done" ;;
+Qwen/Qwen2.5-14B)  
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/LICENSE
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/index.html
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/merges.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00001-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00001-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00002-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00002-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00003-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00003-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00004-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00004-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00005-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00005-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00006-of-00006.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00006-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00007-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model-00008-of-00008.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/tokenizer.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/tokenizer_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-14B/vocab.json
+    echo "download models for Qwen/Qwen2.5-14B done" ;;
+Qwen/Qwen2.5-72B)  
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/LICENSE
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/README.md
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/configuration.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/generation_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/merges.txt
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00001-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00002-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00003-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00004-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00005-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00006-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00007-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00008-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00009-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00010-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00011-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00012-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00013-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00014-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00015-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00016-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00017-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00018-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00019-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00020-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00021-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00022-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00023-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00024-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00025-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00026-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00027-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00028-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00029-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00030-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00031-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00032-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00033-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00034-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00035-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00036-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model-00037-of-00037.safetensors
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/model.safetensors.index.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/tokenizer.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/tokenizer_config.json
+    wget https://paddlenlp.bj.bcebos.com/models/huggingface/Qwen/Qwen2.5-72B/vocab.json
+    echo "download models for Qwen/Qwen2.5-72B done" ;;
+*) echo "no ${model_name_or_path}"; exit 1;
+esac
+
 
 mv -v data data_bak
 wget https://paddlenlp.bj.bcebos.com/llm_benchmark_data/llamafactory_data.tar.gz
