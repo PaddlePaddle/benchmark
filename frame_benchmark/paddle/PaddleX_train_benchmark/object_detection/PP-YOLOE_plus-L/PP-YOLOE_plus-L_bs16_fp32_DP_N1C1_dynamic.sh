7fc14b5cf5ab2796f2ba47374ad19f89de535846
guolixin <guolixin@baidu.com>
2025-10-23 11:53:19 +0000

    Paddle的PR#75907默认关闭NVIDIA_TF32_OVERRIDE，影响性能，benchmark监控打开此flag

--- frame_benchmark/paddle/PaddleX_train_benchmark/object_detection/PP-YOLOE_plus-L/PP-YOLOE_plus-L_bs16_fp32_DP_N1C1_dynamic.sh
+++ frame_benchmark/paddle/PaddleX_train_benchmark/object_detection/PP-YOLOE_plus-L/PP-YOLOE_plus-L_bs16_fp32_DP_N1C1_dynamic.sh
@@ -16,6 +16,7 @@ export speed_unit="samples/s"
 export log_file=${TRAIN_LOG_DIR:-$PWD}/${job_name}_log
 export speed_log_file=${LOG_PATH_INDEX_DIR:-$PWD}/${job_name}_speed
 export distributed_train_logs=${MODEL_REPO_ROOT:-$PWD}/output/${job_name}/distributed_train_logs
+export NVIDIA_TF32_OVERRIDE=1 #Paddle的PR#75907默认关闭此flag，影响性能，benchmark监控打开此flag
 
 train_cmd="python main.py -c paddlex/configs/modules/object_detection/PP-YOLOE_plus-L.yaml -o Global.mode=train -o Global.dataset_dir=dataset/coco_train_benchmark -o Train.epochs_iters=1 -o Train.log_interval=1 -o Train.num_classes=80 -o Benchmark.shuffle=False -o Benchmark.shared_memory=True -o Benchmark.print_mem_info=True -o Benchmark.do_eval=False -o Benchmark.disable_deamon=True -o Global.model=PP-YOLOE_plus-L -o Train.batch_size=16 -o Benchmark.num_workers=4 -o Global.device=gpu:${CUDA_VISIBLE_DEVICES} -o Global.output=output/${job_name} -o Train.uniform_output_enabled=False"
 
