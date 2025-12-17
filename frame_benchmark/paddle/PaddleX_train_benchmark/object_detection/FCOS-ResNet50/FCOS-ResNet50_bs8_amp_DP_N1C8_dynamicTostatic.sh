750d4469741a1e63336c109134ab8a2e2099f344
fightfat <18510030324@126.com>
2025-12-17 10:52:29 +0000

    add timeout

--- frame_benchmark/paddle/PaddleX_train_benchmark/object_detection/FCOS-ResNet50/FCOS-ResNet50_bs8_amp_DP_N1C8_dynamicTostatic.sh
+++ frame_benchmark/paddle/PaddleX_train_benchmark/object_detection/FCOS-ResNet50/FCOS-ResNet50_bs8_amp_DP_N1C8_dynamicTostatic.sh
@@ -20,5 +20,5 @@ export distributed_train_logs=${MODEL_REPO_ROOT:-$PWD}/output/${job_name}/distri
 train_cmd="python main.py -c paddlex/configs/modules/object_detection/FCOS-ResNet50.yaml -o Global.mode=train -o Global.dataset_dir=dataset/coco_train_benchmark -o Train.epochs_iters=1 -o Train.log_interval=1 -o Train.num_classes=80 -o Benchmark.shuffle=False -o Benchmark.shared_memory=True -o Benchmark.print_mem_info=True -o Benchmark.do_eval=False -o Benchmark.disable_deamon=True -o Global.model=FCOS-ResNet50 -o Train.batch_size=8 -o Train.learning_rate=0.00001 -o Benchmark.num_workers=16 -o Benchmark.amp=O1 -o Global.device=gpu:${CUDA_VISIBLE_DEVICES} -o Global.output=output/${job_name} -o Train.uniform_output_enabled=False -o Train.dy2st=True"
 
 echo $train_cmd
-timeout 5m $train_cmd > $log_file 2>&1
+timeout 15m $train_cmd > $log_file 2>&1
 echo $train_cmd >> $log_file
